e99b718d78fad37e501585b4b1d66eb2ac2ad14d
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-02-06 08:48:46 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@91 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ tag/SpagoBI-1.7(20060201)/Exo SpagoBI Portal/portal/WEB-INF/groovy/org/exoplatform/container/groovy/test/OtherGroovyObject.groovy
@@ -0,0 +1,16 @@
+/***************************************************************************
+ * Copyright 2001-2003 The eXo Platform SARL         All rights reserved.  *
+ * Please look at license.txt in info directory for more license detail.   *
+ **************************************************************************/
+package org.exoplatform.container.groovy.test ;
+
+/**
+ * @author Tuan Nguyen (tuan08@users.sourceforge.net)
+ * @since Oct 2, 2004
+ * @version $Id: UserSuites.java,v 1.4 2004/10/21 15:21:50 tuan08 Exp $
+ */
+public class OtherGroovyObject {
+  public OtherGroovyObject() {
+  	println 'hello , this is OtherGroovyObject' 
+  }
+}

77c75b9ff3cbfbd32f693a0f9dc3c20776a99d15
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-03-09 10:54:42 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@302 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIDemoInstaller/spagobi_demo_contents/patchbin/catalina.sh
+++ trunk/SpagoBIDemoInstaller/spagobi_demo_contents/patchbin/catalina.sh
@@ -67,7 +67,8 @@ PRGDIR=`dirname "$PRG"`
 #####################################################################################
 LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
 SECURITY_OPTS="-Djava.security.auth.login.config=$PRGDIR/../conf/jaas.conf"
-JAVA_OPTS="$LOG_OPTS $SECURITY_OPTS $JAVA_OPTS"
+MEMORY_OPTS="-Xmx256m"
+JAVA_OPTS="$LOG_OPTS $SECURITY_OPTS $MEMORY_OPTS $JAVA_OPTS"
 ####################################################################################
 #                     END  EXO CONFIGURATION                                        #
 #####################################################################################

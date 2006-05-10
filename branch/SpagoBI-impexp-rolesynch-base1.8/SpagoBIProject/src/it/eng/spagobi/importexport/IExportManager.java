a39fecf2edce27dd98c3a5337053ed1bcabcbf03
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-05-10 12:31:35 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@449 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- branch/SpagoBI-impexp-rolesynch-base1.8/SpagoBIProject/src/it/eng/spagobi/importexport/IExportManager.java
+++ branch/SpagoBI-impexp-rolesynch-base1.8/SpagoBIProject/src/it/eng/spagobi/importexport/IExportManager.java
@@ -1,13 +1,15 @@
 package it.eng.spagobi.importexport;
 
-import java.util.List;
 import it.eng.spago.error.EMFInternalError;
 import it.eng.spago.error.EMFUserError;
 
+import java.util.List;
+
 public interface IExportManager {
 
-	public void prepareExport(String pathExpFold, String nameExpFile) throws EMFUserError, EMFInternalError;
+	public void prepareExport(String pathExpFold, String nameExpFile, boolean expSubObj) throws EMFUserError;
 	
-	public void exportObjects(List objPaths) throws EMFUserError, EMFInternalError;
+	public String exportObjects(List objPaths) throws EMFUserError;
 	
+	public void cleanExportEnvironment();
 }

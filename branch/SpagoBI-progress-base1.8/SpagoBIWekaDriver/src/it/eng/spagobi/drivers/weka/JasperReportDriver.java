e41433d9ed8fc7782723718633de5cf647612507
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-05-11 08:49:32 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@482 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ branch/SpagoBI-progress-base1.8/SpagoBIWekaDriver/src/it/eng/spagobi/drivers/weka/JasperReportDriver.java
@@ -0,0 +1,254 @@
+/**
+ * Copyright (c) 2005, Engineering Ingegneria Informatica s.p.a.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of 
+      conditions and the following disclaimer.
+      
+    * Redistributions in binary form must reproduce the above copyright notice, this list of 
+      conditions and the following disclaimer in the documentation and/or other materials 
+      provided with the distribution.
+      
+    * Neither the name of the Engineering Ingegneria Informatica s.p.a. nor the names of its contributors may
+      be used to endorse or promote products derived from this software without specific
+      prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+
+ */
+
+/*
+ * Created on 3-mag-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package it.eng.spagobi.drivers.weka;
+
+import it.eng.spago.error.EMFUserError;
+import it.eng.spago.security.IEngUserProfile;
+import it.eng.spagobi.bo.BIObject;
+import it.eng.spagobi.bo.BIObjectParameter;
+import it.eng.spagobi.bo.Subreport;
+import it.eng.spagobi.bo.dao.DAOFactory;
+import it.eng.spagobi.bo.dao.IBIObjectDAO;
+import it.eng.spagobi.bo.dao.ISubreportDAO;
+import it.eng.spagobi.drivers.IEngineDriver;
+import it.eng.spagobi.utilities.GeneralUtilities;
+import it.eng.spagobi.utilities.SpagoBITracer;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+
+
+/**
+ * Driver Implementation (IEngineDriver Interface) for Jasper Report Engine. 
+ */
+public class JasperReportDriver implements IEngineDriver {
+
+	
+    /**
+	 * Return a map of parameters which will be sended in the request to the 
+	 * engine application.
+	 * @param biObject Object to execute
+	 * @return Map The map of the execution call parameters
+  	*/
+	public Map getParameterMap(Object biobject){
+		Map map = new Hashtable();
+		try{
+			BIObject biobj = (BIObject)biobject;
+			map = getMap(biobj);
+		} catch (ClassCastException cce) {
+			SpagoBITracer.major("ENGINES",
+					this.getClass().getName(),
+					"getParameterMap(Object)",
+					"The parameter is not a BIObject type",
+					cce);
+		} 
+		return map;
+	}			
+	/**
+	 * Return a map of parameters which will be sended in the request to the 
+	 * engine application.
+	 * @param biObject Object to execute
+	 * @param profile Profile of the user 
+	 * @return Map The map of the execution call parameters
+	 */
+	public Map getParameterMap(Object object, IEngUserProfile profile){
+		return getParameterMap(object);
+	}
+	/**
+	 * Return a map of parameters which will be sended in the request to the 
+	 * engine application.
+	 * @param biObject Object container of the subObject
+	 * @param subObject SubObject to execute
+	 * @return Map The map of the execution call parameters
+  	 */
+	public Map getParameterMap(Object object, Object subObject){
+		return getParameterMap(object);
+	}
+    /**
+	 * Return a map of parameters which will be sended in the request to the 
+	 * engine application.
+	 * @param biObject Object container of the subObject
+	 * @param subObject SubObject to execute
+	 * @param profile Profile of the user 
+	 * @return Map The map of the execution call parameters
+  	 */
+    public Map getParameterMap(Object object, Object subObject, IEngUserProfile profile){
+		return getParameterMap(object);
+	}
+
+    
+        
+        
+    /**
+     * Starting from a BIObject extracts from it the map of the paramaeters for the
+     * execution call
+     * @param biobj BIObject to execute
+     * @return Map The map of the execution call parameters
+     */    
+	private Map getMap(BIObject biobj) {
+		Map pars = new Hashtable();
+		pars.put("templatePath",biobj.getPath() + "/template");
+        pars.put("spagobiurl", GeneralUtilities.getSpagoBiContentRepositoryServlet());
+        pars = addBISubreports(biobj, pars);
+        pars = addBIParameters(biobj, pars);
+        return pars;
+	} 
+ 
+	// TODO check all the subreport's hierarchy recursively and not only the first level
+	private Map addBISubreports(BIObject biobj, Map pars) {
+		Integer masterReportId = biobj.getId();
+				 
+		try {
+			ISubreportDAO subrptdao = DAOFactory.getSubreportDAO();
+			IBIObjectDAO biobjectdao = DAOFactory.getBIObjectDAO();
+			
+			List subreportList =  subrptdao.loadSubreportsByMasterRptId(masterReportId);
+			for(int i = 0; i < subreportList.size(); i++) {
+				Subreport subreport = (Subreport)subreportList.get(i);
+				BIObject subrptbiobj = biobjectdao.loadBIObjectForDetail(subreport.getSub_rpt_id());
+				
+				String path = subrptbiobj.getPath();
+				SpagoBITracer.debug("JasperReportDriver", "JasperReportDriver","addBISubreports", " PATH: " + path);
+				
+				pars.put("subrpt." + (i+1) + ".path", path);
+			}
+			pars.put("srptnum", "" + subreportList.size());
+			
+		} catch (EMFUserError e) {
+			SpagoBITracer.warning("ENGINES",
+					  this.getClass().getName(),
+					  "addBISubreports",
+					  "Error while reading subreports",
+					  e);
+		}
+		
+		
+		/*
+		DataConnection dataConnection = null;
+		SQLCommand sqlCommand = null;
+		DataResult dataResult = null;
+		SourceBean subreports = null;
+		try {
+			DataConnectionManager dataConnectionManager = DataConnectionManager.getInstance();
+			dataConnection = dataConnectionManager.getConnection("spagobi");
+			String statement = null;
+						
+			statement = SQLStatements.getStatement("SELECT_SUBREPORTS_LIST");
+			statement = statement.replaceFirst("\\?", masterReportId.toString());
+			SpagoBITracer.debug("JasperReportDriver", "JasperReportDriver","addBISubreports", " statement: " + statement);
+			sqlCommand = dataConnection.createSelectCommand(statement);
+			dataResult = sqlCommand.execute();
+			ScrollableDataResult scrollableDataResult = (ScrollableDataResult)dataResult.getDataObject();
+			subreports = scrollableDataResult.getSourceBean();
+			SpagoBITracer.debug("JasperReportDriver", "JasperReportDriver","addBISubreports", " results: " + subreports.toXML());
+		
+		
+			List subreportsList = subreports.getAttributeAsList("ROW");			
+			for(int i = 0; i < subreportsList.size(); i++) {
+				SourceBean subreport = (SourceBean)subreportsList.get(i);
+				Integer id = (Integer)subreport.getAttribute("SUBREPORT_ID");	
+				
+				statement = SQLStatements.getStatement("SELECT_SUBREPORT_PATH");
+				statement = statement.replaceFirst("\\?", id.toString());
+				SpagoBITracer.debug("JasperReportDriver", "JasperReportDriver","addBISubreports", " statement: " + statement);
+				sqlCommand = dataConnection.createSelectCommand(statement);
+				dataResult = sqlCommand.execute();
+				scrollableDataResult = (ScrollableDataResult)dataResult.getDataObject();
+				subreport = (SourceBean)scrollableDataResult.getSourceBean().getAttribute("ROW");
+				SpagoBITracer.debug("JasperReportDriver", "JasperReportDriver","addBISubreports", " results: " + subreport.toXML());
+				
+				String path = (String)subreport.getAttribute("PATH");
+				SpagoBITracer.debug("JasperReportDriver", "JasperReportDriver","addBISubreports", " PATH: " + path);
+				
+				pars.put("subrpt." + (i+1) + ".path", path);
+			}
+			
+			pars.put("srptnum", "" + subreportsList.size());
+		} catch (Exception ex) {
+			TracerSingleton.log(Constants.NOME_MODULO, TracerSingleton.CRITICAL, "execQuery::executeQuery:", ex);
+		} finally {
+			Utils.releaseResources(dataConnection, sqlCommand, dataResult);
+		} 		
+		*/
+		
+		
+		
+		return pars;
+	}
+	
+    /**
+     * Add into the parameters map the BIObject's BIParameter names and values
+     * @param biobj BIOBject to execute
+     * @param pars Map of the parameters for the execution call  
+     * @return Map The map of the execution call parameters
+     */
+	private Map addBIParameters(BIObject biobj, Map pars) {
+		if(biobj==null) {
+			SpagoBITracer.warning("ENGINES",
+								  this.getClass().getName(),
+								  "addBIParameters",
+								  "BIObject parameter null");
+			return pars;
+		}
+		if(biobj.getBiObjectParameters() != null){
+			BIObjectParameter biobjPar = null;
+			String value = null;
+			for(Iterator it = biobj.getBiObjectParameters().iterator(); it.hasNext();){
+				try {
+					biobjPar = (BIObjectParameter)it.next();
+					value = (String)biobjPar.getParameterValues().get(0);
+					pars.put(biobjPar.getParameterUrlName(), value);
+				} catch (Exception e) {
+					SpagoBITracer.warning("ENGINES",
+										  this.getClass().getName(),
+										  "addBIParameters",
+										  "Error while processing a BIParameter",
+										  e);
+				}
+			}
+		}
+  		return pars;
+	}
+
+}
+

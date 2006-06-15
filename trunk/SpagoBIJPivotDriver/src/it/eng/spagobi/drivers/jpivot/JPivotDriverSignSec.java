2390bd5fc44080a7ca952a05757189760647a4d3
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-06-15 10:25:14 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@618 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIJPivotDriver/src/it/eng/spagobi/drivers/jpivot/JPivotDriverSignSec.java
+++ trunk/SpagoBIJPivotDriver/src/it/eng/spagobi/drivers/jpivot/JPivotDriverSignSec.java
@@ -45,11 +45,14 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import it.eng.spago.base.SourceBean;
+import it.eng.spago.configuration.ConfigSingleton;
 import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.bo.BIObject;
 import it.eng.spagobi.bo.BIObjectParameter;
 import it.eng.spagobi.bo.BIObject.SubObjectDetail;
 import it.eng.spagobi.drivers.IEngineDriver;
+import it.eng.spagobi.services.modules.DetailBIObjectModule;
 import it.eng.spagobi.utilities.GeneralUtilities;
 import it.eng.spagobi.utilities.SecurityUtilities;
 import it.eng.spagobi.utilities.SpagoBITracer;
@@ -166,8 +169,12 @@ public class JPivotDriverSignSec implements IEngineDriver {
      * @return Map The map of the execution call parameters
      */    
 	private Map getMap(BIObject biobj) {
-		Map pars = new Hashtable();
-		pars.put("templatePath",biobj.getPath() + "/template");
+   		Map pars = new Hashtable();
+		ConfigSingleton config = ConfigSingleton.getInstance();
+		SourceBean biobjectsPathSB = (SourceBean) config.getAttribute(DetailBIObjectModule.CMS_BIOBJECTS_PATH);
+		String biobjectsPath = (String) biobjectsPathSB.getAttribute("path");
+		String path = biobjectsPath + "/" + biobj.getUuid() + "/template";
+		pars.put("templatePath", path);
         pars.put("spagobiurl", GeneralUtilities.getSpagoBiContentRepositoryServlet());
 		pars.put("query", "dynamicOlap");
         pars = addBIParameters(biobj, pars);

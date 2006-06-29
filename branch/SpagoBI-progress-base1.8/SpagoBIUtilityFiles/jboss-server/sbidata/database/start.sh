0b6554718c5510213a4b9bdfae76d3de80d47b89
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-06-29 11:39:48 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@932 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- branch/SpagoBI-progress-base1.8/SpagoBIUtilityFiles/jboss-server/sbidata/database/start.sh
+++ branch/SpagoBI-progress-base1.8/SpagoBIUtilityFiles/jboss-server/sbidata/database/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp ./hsqldb1_8_0_2.jar org.hsqldb.Server -database.0 ./spagobi -dbname.0 spagobi
+java -cp ./hsqldb1_8_0_2.jar org.hsqldb.Server -database.0 ./spagobi -database.1 ./spagobigeo -dbname.0 spagobi -dbname.1 spagobigeo

074f7e69ff42d40481d7d2fc1f56297f9347aaa8
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-06-29 08:02:08 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@927 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- branch/SpagoBI-progress-base1.8/SpagoBIUtilityFiles/jonas-server/sbidata/database/start.bat
+++ branch/SpagoBI-progress-base1.8/SpagoBIUtilityFiles/jonas-server/sbidata/database/start.bat
@@ -1,1 +1,1 @@
-java -cp ./hsqldb1_8_0_2.jar org.hsqldb.Server -database.0 ./spagobi -dbname.0 spagobi
+java -cp ./hsqldb1_8_0_2.jar org.hsqldb.Server -database.0 ./spagobi -database.1 ./spagobigeo -dbname.0 spagobi -dbname.1 spagobigeo

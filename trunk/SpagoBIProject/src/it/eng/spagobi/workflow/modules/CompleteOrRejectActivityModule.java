3dc379fa22a9223fb322010b78b0f884ca5e53ac
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-09-07 13:57:26 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@1323 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIProject/src/it/eng/spagobi/workflow/modules/CompleteOrRejectActivityModule.java
+++ trunk/SpagoBIProject/src/it/eng/spagobi/workflow/modules/CompleteOrRejectActivityModule.java
@@ -2,12 +2,32 @@ package it.eng.spagobi.workflow.modules;
 
 import it.eng.spago.base.SourceBean;
 import it.eng.spago.dispatching.module.AbstractModule;
+import it.eng.spagobi.utilities.SpagoBITracer;
+
+import org.jbpm.JbpmConfiguration;
+import org.jbpm.JbpmContext;
+import org.jbpm.taskmgmt.exe.TaskInstance;
 
 public class CompleteOrRejectActivityModule extends AbstractModule {
 
-	public void service(SourceBean arg0, SourceBean arg1) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void service(SourceBean request, SourceBean response) throws Exception {
+    	// This action handle both activity completion and activity reject 
+		
+		JbpmConfiguration jbpmConfiguration = JbpmConfiguration.getInstance();
+    	JbpmContext jbpmContext = jbpmConfiguration.createJbpmContext();
+    	String activityKeyIdStr = (String) request.getAttribute("ActivityKey");
+		long activityKeyId = Long.valueOf(activityKeyIdStr).longValue();
+		TaskInstance taskInstance = jbpmContext.getTaskInstance(activityKeyId);
+    	
+    	if (request.getAttribute("CompletedActivity") != null){
+    		// Submit buttin named CompleteActivity is pressed
+    		SpagoBITracer.info("Workflow", "CompleteOrRejectActivityModule", "service", "Completing Activity ["+ activityKeyId + "]");
+    		taskInstance.end();
+    	} else {
+    		//  Submit buttin named RejectActivity is pressed
+    		SpagoBITracer.info("Workflow", "CompleteOrRejectActivityModule", "service", "Completing Activity ["+ activityKeyId + "]");
+    		taskInstance.cancel();
+    	}
 	}
 
 }

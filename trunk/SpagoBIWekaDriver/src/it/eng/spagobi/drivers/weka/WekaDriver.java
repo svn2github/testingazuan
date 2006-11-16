612badb7ec5fa8c8f071f90f90db068b89a6fcba
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-11-16 09:55:35 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@1684 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIWekaDriver/src/it/eng/spagobi/drivers/weka/WekaDriver.java
+++ trunk/SpagoBIWekaDriver/src/it/eng/spagobi/drivers/weka/WekaDriver.java
@@ -8,7 +8,9 @@ package it.eng.spagobi.drivers.weka;
 import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.bo.BIObject;
 import it.eng.spagobi.bo.BIObjectParameter;
+import it.eng.spagobi.drivers.EngineURL;
 import it.eng.spagobi.drivers.IEngineDriver;
+import it.eng.spagobi.drivers.exceptions.InvalidOperationRequest;
 import it.eng.spagobi.utilities.GeneralUtilities;
 import it.eng.spagobi.utilities.ParameterValuesEncoder;
 import it.eng.spagobi.utilities.SpagoBITracer;
@@ -163,7 +165,34 @@ public class WekaDriver implements IEngineDriver {
 	protected Map applySecurity(Map pars) {
 		return pars;
 	}
-       
+	
+	/**
+	 * Function not implemented. Thid method should not be called
+	 * 
+	 * @param biobject The BIOBject to edit
+	 * @throws InvalidOperationRequest
+	 */
+	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject) throws InvalidOperationRequest {
+		SpagoBITracer.major("ENGINES",
+				  this.getClass().getName(),
+				  "getEditDocumentTemplateBuildUrl",
+				  "Function not implemented");
+		throw new InvalidOperationRequest();
+	}
+
+	/**
+	 * Function not implemented. Thid method should not be called
+	 * 
+	 * @param biobject The BIOBject to edit
+	 * @throws InvalidOperationRequest
+	 */
+	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject) throws InvalidOperationRequest {
+		SpagoBITracer.major("ENGINES",
+				  this.getClass().getName(),
+				  "getNewDocumentTemplateBuildUrl",
+				  "Function not implemented");
+		throw new InvalidOperationRequest();
+	}
 
 }
 

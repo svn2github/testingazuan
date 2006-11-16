07ca4f52ae6beaaa3657c8b99d7376ae3be33413
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-11-16 11:02:54 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@1693 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIBODriver/src/it/eng/spagobi/drivers/bo/BODriver.java
+++ trunk/SpagoBIBODriver/src/it/eng/spagobi/drivers/bo/BODriver.java
@@ -35,7 +35,9 @@ package it.eng.spagobi.drivers.bo;
 import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.bo.BIObject;
 import it.eng.spagobi.bo.BIObjectParameter;
+import it.eng.spagobi.drivers.EngineURL;
 import it.eng.spagobi.drivers.IEngineDriver;
+import it.eng.spagobi.drivers.exceptions.InvalidOperationRequest;
 import it.eng.spagobi.utilities.SpagoBITracer;
 import it.eng.spagobi.utilities.UploadedFile;
 
@@ -249,6 +251,35 @@ public class BODriver implements IEngineDriver {
 					  "HTML|PDF|XSL");
 	    }
 	}
+
+	/**
+	 * Function not implemented. Thid method should not be called
+	 * 
+	 * @param biobject The BIOBject to edit
+	 * @throws InvalidOperationRequest
+	 */
+	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject) throws InvalidOperationRequest {
+		SpagoBITracer.major("ENGINES",
+				  this.getClass().getName(),
+				  "getEditDocumentTemplateBuildUrl",
+				  "Function not implemented");
+		throw new InvalidOperationRequest();
+	}
+
+	/**
+	 * Function not implemented. Thid method should not be called
+	 * 
+	 * @param biobject The BIOBject to edit
+	 * @throws InvalidOperationRequest
+	 */
+	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject) throws InvalidOperationRequest {
+		SpagoBITracer.major("ENGINES",
+				  this.getClass().getName(),
+				  "getNewDocumentTemplateBuildUrl",
+				  "Function not implemented");
+		throw new InvalidOperationRequest();
+	}
+	
 	
 }
 

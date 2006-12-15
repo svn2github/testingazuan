00f1d05aad2a592f45268d0afcc8bf79a5de0ad4
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2006-12-15 17:49:37 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@1774 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ trunk/QbeWeb/WebContent/tmp/groovy/customerlnk.groovy
@@ -0,0 +1,8 @@
+def foo(pValue) {
+    return "http://boccalon.eng.it/CUST_DETAIL_ACTION?id=${pValue}\"> Detail ${pValue}"
+ 
+}
+return foo("${input1}");
+
+
+
---
+++ trunk/QbeWeb/WebContent/tmp/groovy/customername.groovy
@@ -0,0 +1,1 @@
+return  "http://andrea.eng.it/NAME_DETAIL_ACTION?lname=${input1}&fname=${input2}"

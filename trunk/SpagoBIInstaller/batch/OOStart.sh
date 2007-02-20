1e3f9418026005d77168d4a9dbeb746650a81239
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-02-20 17:10:59 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@2111 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ trunk/SpagoBIInstaller/batch/OOStart.sh
@@ -0,0 +1,1 @@
+exec "$OPENOFFICE_HOME"/program/soffice -quickstart -invisible -accept="socket,host=0,port=9005;urp;StarOffice.ServiceManager"
---
+++ trunk/SpagoBIInstaller/batch/StartSpagoBI.sh
@@ -0,0 +1,12 @@
+if [ -n "$OPENOFFICE_HOME" ]; then
+  ./OOStart.sh &
+else
+  echo "OPENOFFICE_HOME is not defined ,  SpagoBIBooklets component for booklets creation will not work!!"
+  echo "Control that OpenOffice 2.0.x is installed and set OPENOFFICE_HOME variable to the installation folder."
+sleep 3
+fi
+
+BASEDIR=./bin
+exec ./bin/exo-run.sh "$@" start
+
+exit 1

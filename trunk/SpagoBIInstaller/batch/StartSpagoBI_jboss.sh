829cd8e973bc4b0375905b6a703c9831ee17fc79
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-03-07 10:07:55 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@2170 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIInstaller/batch/StartSpagoBI_jboss.sh
+++ trunk/SpagoBIInstaller/batch/StartSpagoBI_jboss.sh
@@ -6,7 +6,25 @@ else
 sleep 3
 fi
 
-cd ./bin
+# start database/databases
+# controls if database is alive
+cd ./sbidata/database
+java -cp hsqldb1_8_0_2.jar:testSpagobiHsqldbAlive.jar it.eng.spagobi.testhsqldb.TestSpagobiHsqldbAlive
+
+RESULT=$?
+# if database is not alive starts it
+if [ $RESULT = 1 ]; then 
+	./start.sh &
+	sleep 3
+fi
+
+while [ $RESULT = 1 ]
+do
+	java -cp hsqldb1_8_0_2.jar:testSpagobiHsqldbAlive.jar it.eng.spagobi.testhsqldb.TestSpagobiHsqldbAlive
+	RESULT=$?
+done
+
+cd ../../bin
 exec ./run.sh "$@" start
 
 exit 1
--- trunk/SpagoBIInstaller/batch/StartSpagoBI_jonas.sh
+++ trunk/SpagoBIInstaller/batch/StartSpagoBI_jonas.sh
@@ -6,7 +6,25 @@ else
 sleep 3
 fi
 
-BASEDIR=./bin
-exec ./bin/unix/jonas start
+# start database/databases
+# controls if database is alive
+cd ./sbidata/database
+java -cp hsqldb1_8_0_2.jar:testSpagobiHsqldbAlive.jar it.eng.spagobi.testhsqldb.TestSpagobiHsqldbAlive
+
+RESULT=$?
+# if database is not alive starts it
+if [ $RESULT = 1 ]; then 
+	./start.sh &
+	sleep 3
+fi
+
+while [ $RESULT = 1 ]
+do
+	java -cp hsqldb1_8_0_2.jar:testSpagobiHsqldbAlive.jar it.eng.spagobi.testhsqldb.TestSpagobiHsqldbAlive
+	RESULT=$?
+done
+
+cd ../../bin/unix
+exec ./jonas start
 
 exit 1

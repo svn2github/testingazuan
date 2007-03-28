064ddbccc19cf11678766c648ab7cd859ce8d846
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-03-28 13:22:47 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@2263 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIInstaller/IzPackSource/com/izforge/izpack/installer/InstallSpagoBIPlatform.java
+++ trunk/SpagoBIInstaller/IzPackSource/com/izforge/izpack/installer/InstallSpagoBIPlatform.java
@@ -10,13 +10,13 @@ import java.util.Set;
 
 public class InstallSpagoBIPlatform {
 
-	private static String SPAGOBI_ZIP_FILE = "SpagoBI-bin-1.9.2-installer-03272007.zip";
-	private static String BIRT_ZIP_FILE = "SpagoBIBirtReportEngine-bin-1.9.2-installer-03272007.zip";
-	private static String GEO_ZIP_FILE = "SpagoBIGeoEngine-bin-1.9.2-Beta-installer-03272007.zip";
-	private static String JASPER_ZIP_FILE = "SpagoBIJasperReportEngine-bin-1.9.2-installer-03272007.zip";
-	private static String JPIVOT_ZIP_FILE = "SpagoBIJPivotEngine-bin-1.9.2-installer-03272007.zip";
-	private static String QBE_ZIP_FILE = "SpagoBIQbeEngine-bin-1.9.2-installer-03272007.zip";
-	private static String WEKA_ZIP_FILE = "SpagoBIWekaEngine-bin-1.9.2-Beta-installer-03272007.zip";
+	private static String SPAGOBI_ZIP_FILE = "SpagoBI-bin-1.9.2-03272007.zip";
+	private static String BIRT_ZIP_FILE = "SpagoBIBirtReportEngine-bin-1.9.2-03272007.zip";
+	private static String GEO_ZIP_FILE = "SpagoBIGeoEngine-bin-1.9.2-Beta-03272007.zip";
+	private static String JASPER_ZIP_FILE = "SpagoBIJasperReportEngine-bin-1.9.2-03272007.zip";
+	private static String JPIVOT_ZIP_FILE = "SpagoBIJPivotEngine-bin-1.9.2-03272007.zip";
+	private static String QBE_ZIP_FILE = "SpagoBIQbeEngine-bin-1.9.2-03272007.zip";
+	private static String WEKA_ZIP_FILE = "SpagoBIWekaEngine-bin-1.9.2-Beta-03272007.zip";
 	
 	private static String _pathdest;
 	private static String _spagobi_plaftorm_source_dir;

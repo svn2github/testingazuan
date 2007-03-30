0cee7b6a9757b0137b6bd9dfda4533783471d252
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-03-30 11:44:15 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@2275 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIInstaller/batch/StartSpagoBI.sh
+++ trunk/SpagoBIInstaller/batch/StartSpagoBI.sh
@@ -1,3 +1,20 @@
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+ 
+PRGDIR=`dirname "$PRG"`
+
+cd $PRGDIR
+
 ./OOStart.sh &
 sleep 3
 
--- trunk/SpagoBIInstaller/batch/StopSpagoBI.sh
+++ trunk/SpagoBIInstaller/batch/StopSpagoBI.sh
@@ -1,3 +1,20 @@
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+ 
+PRGDIR=`dirname "$PRG"`
+
+cd $PRGDIR
+
 # stop exo
 cd ./${BIN_DIR}
 exec ./${STOP_COMMAND} &

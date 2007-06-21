3ebb31f4a88fe5a4001636b65800fbbc6114bc6c
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-06-21 16:42:25 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@2535 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIProject/web-content/js/spagobi.js
+++ trunk/SpagoBIProject/web-content/js/spagobi.js
@@ -55,7 +55,31 @@ function findPos(obj) {
  }
  
 
+function getCookie(NameOfCookie){
+    if (document.cookie.length > 0) {              
+   		 begin = document.cookie.indexOf(NameOfCookie+"=");       
+    	if (begin != -1) {           
+     		 begin += NameOfCookie.length+1;       
+    		 end = document.cookie.indexOf(";", begin);
+     		 if (end == -1) 
+     		 		end = document.cookie.length;
+       		return unescape(document.cookie.substring(begin, end));
+   		 } 
+ 	 }
+ 	 return null;
+}
 
+function setCookie(NameOfCookie, value, expiredays) {
+		var ExpireDate = new Date();
+		ExpireDate.setTime(ExpireDate.getTime() + (expiredays * 24 * 3600 * 1000));
+		 document.cookie = NameOfCookie + "=" + escape(value) +   ((expiredays == null) ? "" : "; expires=" + ExpireDate.toGMTString());
+}
+
+function delCookie (NameOfCookie) {
+ 	 if (getCookie(NameOfCookie)) {
+   		 document.cookie = NameOfCookie + "=" +  "; expires=Thu, 01-Jan-70 00:00:01 GMT";
+ 	 }
+}
 
 
 // START PORLTET JAVASCRIPT INTIIALIZER

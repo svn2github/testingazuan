348ecb896da57bdc5e620d032c03a569f958d956
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-08-28 15:10:58 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@2903 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/SpagoBIProject/src/it/eng/spagobi/utilities/ParameterValuesEncoder.java
+++ trunk/SpagoBIProject/src/it/eng/spagobi/utilities/ParameterValuesEncoder.java
@@ -21,12 +21,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.utilities;
 
+import it.eng.spago.error.EMFUserError;
 import it.eng.spagobi.bo.BIObjectParameter;
 import it.eng.spagobi.bo.ModalitiesValue;
 import it.eng.spagobi.bo.Parameter;
+import it.eng.spagobi.bo.dao.DAOFactory;
 import it.eng.spagobi.constants.SpagoBIConstants;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -95,6 +96,8 @@ public class ParameterValuesEncoder {
 		
 		Parameter parameter = biobjPar.getParameter();
 		if (parameter != null) {
+			String type = parameter.getType();
+			boolean isString = "STRING".equalsIgnoreCase(type);
 			ModalitiesValue modValue = parameter.getModalityValue();
 			if (modValue != null) {
 				boolean mult = biobjPar.getParameter().getModalityValue().isMultivalue();
@@ -107,41 +110,62 @@ public class ParameterValuesEncoder {
 				if(!mult) {
 					return (String)biobjPar.getParameterValues().get(0);
 				} else {
-					return encodeMultivaluesParam(biobjPar.getParameterValues());
+					return encodeMultivaluesParam(biobjPar.getParameterValues(), isString);
 				}
 			} else {
 				List values = biobjPar.getParameterValues();
 				if (values != null && values.size() > 0) {
 					if (values.size() == 1) return (String)biobjPar.getParameterValues().get(0);
-					else return encodeMultivaluesParam(biobjPar.getParameterValues());
+					else return encodeMultivaluesParam(biobjPar.getParameterValues(), isString);
 				} else return "";
 			}
 		} else {
+			Integer parId = biobjPar.getParID();
+			boolean isString = false;
+			if (parId == null) {
+				SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), "encode",
+						  "Parameter object nor parameter id are set into BiObjectPrameter with label = " 
+						+ biobjPar.getLabel() + " of document with id = " + biobjPar.getBiObjectID());
+			} else {
+				try {
+					Parameter aParameter = DAOFactory.getParameterDAO().loadForDetailByParameterID(parId);
+					String type = aParameter.getType();
+					isString = "STRING".equalsIgnoreCase(type);
+				} catch (EMFUserError e) {
+					SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), "encode",
+							  "Error loading parameter with id = " + biobjPar.getParID());
+				}
+			}
 			List values = biobjPar.getParameterValues();
 			if (values != null && values.size() > 0) {
 				if (values.size() == 1) return (String)biobjPar.getParameterValues().get(0);
-				else return encodeMultivaluesParam(biobjPar.getParameterValues());
+				else return encodeMultivaluesParam(biobjPar.getParameterValues(), isString);
 			} else return "";
 		}
 
 	}
 	
-	private String encode(Object values) {
-		if(values instanceof String)
-			return (String)values;
-		else if(values instanceof List) {
-			return encodeMultivaluesParam((List)values);
-		} else {
-			return null;
-		}
-	}
+//	private String encode(Object values) {
+//		if(values instanceof String)
+//			return (String)values;
+//		else if(values instanceof List) {
+//			return encodeMultivaluesParam((List)values);
+//		} else {
+//			return null;
+//		}
+//	}
 		
 	
 	/////////////////////////////////////////////////////////////
 	//	UTILITY METHODS
 	/////////////////////////////////////////////////////////////
 	
-	private String encodeMultivaluesParam(List values) {
+	/*
+	 * isString: it is a flag: if it is true, the values are strings: in this 
+	 * case, if the strings are not surrounded by ' character, they are surrounded by ' character.
+	 * This is a work-around: generally speaking, a list of values should declare the type of contained objects.
+	 */
+	private String encodeMultivaluesParam(List values, boolean isString) {
 		String value = "";
 		
 		if(values == null || values.size() == 0) return value;
@@ -151,6 +175,14 @@ public class ParameterValuesEncoder {
 		value += openBlockMarker;
 		for(int i = 0; i < values.size(); i++) {
 			String valueToBeAppended = (values.get(i) == null)? "": (String)values.get(i);
+			if (isString) {
+				if (valueToBeAppended.equals("")) valueToBeAppended = "''";
+				else {
+					if (!valueToBeAppended.startsWith("'") && !valueToBeAppended.endsWith("'")) {
+						valueToBeAppended = "'" + valueToBeAppended + "'";
+					}
+				}
+			}
 			value += (i>0)? separator: "";
 			value += valueToBeAppended;
 		}

5ef1b46b0c36bfd73b03db684d9f83c3399df12b
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-11-20 12:25:47 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3304 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/services/datasource/service/DataSourceSupplier.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/services/datasource/service/DataSourceSupplier.java
@@ -1,14 +1,40 @@
 package it.eng.spagobi.services.datasource.service;
 
+
+import it.eng.spagobi.commons.dao.DAOFactory;
 import it.eng.spagobi.services.datasource.bo.SpagoBiDataSource;
+import it.eng.spagobi.tools.datasource.bo.DataSource;
+import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
+import org.apache.log4j.Logger;
 
 public class DataSourceSupplier {
-
+	static private Logger logger = Logger.getLogger(DataSourceSupplier.class);
+	
     public SpagoBiDataSource getDataSource( String documentLabel,String engineLabel) {
-	SpagoBiDataSource tmp=new SpagoBiDataSource();
-	tmp.setDriver("org.driver");
-	tmp.setJndiName("jndi/pippo");
-	return tmp;
+        
+		SpagoBiDataSource sbds = new SpagoBiDataSource();  
+    		
+    	//gets data source data from database
+		try{
+			it.eng.spagobi.analiticalmodel.document.bo.BIObject obj = DAOFactory.getBIObjectDAO().loadBIObjectById(Integer.valueOf(documentLabel));
+			if (obj == null){
+				logger.warn("The object with id " + documentLabel + " is not found on the database.");
+				return null;
+			}
+			DataSource ds = DAOFactory.getDataSourceDAO().loadDataSourceByID(obj.getDataSourceId());
+			if (ds == null){
+				logger.warn("The data source with id " + documentLabel + " is not found on the database.");
+				return null;
+			}
+			sbds.setJndiName(ds.getJndi());
+			sbds.setUrl(ds.getUrlConnection());
+			sbds.setUser(ds.getUser());
+			sbds.setPassword(ds.getPwd());
+			sbds.setDriver(ds.getDriver());
+		}catch (Exception e){
+			logger.error("The data source is not correctly returned", e);
+		}
+    	return sbds;
     }
     
 }

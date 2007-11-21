9ec150a240f021106dfe2010c2fe681418f0769f
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-11-21 11:20:13 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3332 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/config/exceptions/NoEngineSuitableException.java
@@ -0,0 +1,9 @@
+package it.eng.spagobi.engines.config.exceptions;
+
+public class NoEngineSuitableException extends Exception{
+
+	public NoEngineSuitableException(String message) {
+		super(message);
+	}
+	
+}

07adac5ccce21cb14fb90612ff69f2eb5374b681
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-11-21 14:32:42 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3376 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/services/event/stub/EventServiceSoapBindingImpl.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/services/event/stub/EventServiceSoapBindingImpl.java
@@ -7,9 +7,12 @@
 
 package it.eng.spagobi.services.event.stub;
 
+import it.eng.spagobi.services.event.service.EventServiceImpl;
+
 public class EventServiceSoapBindingImpl implements it.eng.spagobi.services.event.stub.EventService{
     public java.lang.String fireEvent(java.lang.String in0, java.lang.String in1, java.lang.String in2, java.lang.String in3, java.lang.String in4, java.lang.String in5) throws java.rmi.RemoteException {
-        return null;
+	EventServiceImpl service=new EventServiceImpl();
+	return service.fireEvent(in0, in1, in2, in3, in4, in5);
     }
 
 }

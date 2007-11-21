54f41177eff99a44fe25c968b271f809e5fb2fe8
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-11-21 11:02:52 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3325 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/services/audit/stub/AuditServiceService.java
@@ -0,0 +1,16 @@
+/**
+ * AuditServiceService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.3 Oct 05, 2005 (05:23:37 EDT) WSDL2Java emitter.
+ */
+
+package it.eng.spagobi.services.audit.stub;
+
+public interface AuditServiceService extends javax.xml.rpc.Service {
+    public java.lang.String getAuditServiceAddress();
+
+    public it.eng.spagobi.services.audit.stub.AuditService getAuditService() throws javax.xml.rpc.ServiceException;
+
+    public it.eng.spagobi.services.audit.stub.AuditService getAuditService(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}

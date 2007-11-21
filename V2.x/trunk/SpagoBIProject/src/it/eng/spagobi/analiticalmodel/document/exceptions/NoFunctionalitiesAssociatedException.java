d1d16c4427fdf7d7fa46e4689a25e0c9bef7dc42
fiscato <fiscato@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-11-21 13:11:50 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3355 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/analiticalmodel/document/exceptions/NoFunctionalitiesAssociatedException.java
@@ -0,0 +1,9 @@
+package it.eng.spagobi.analiticalmodel.document.exceptions;
+
+public class NoFunctionalitiesAssociatedException extends Exception {
+
+	public NoFunctionalitiesAssociatedException(String message) {
+		super(message);
+	}
+	
+}

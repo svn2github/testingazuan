661ccd452f50474279ce317693f548f19d8dbc78
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-11-26 10:28:51 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3415 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/utilities/callbacks/mapcatalogue/MapCatalogueAccessUtils.java
+++ V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/utilities/callbacks/mapcatalogue/MapCatalogueAccessUtils.java
@@ -21,15 +21,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.utilities.callbacks.mapcatalogue;
 
-import java.io.IOException;
+import it.eng.spagobi.services.proxy.ContentServiceProxy;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.commons.httpclient.DefaultMethodRetryHandler;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.PostMethod;
 
 /**
  * A proxy class used by clients to remotly access the spagoBI EventHandler 
@@ -39,7 +34,6 @@ import org.apache.commons.httpclient.methods.PostMethod;
  */
 public class MapCatalogueAccessUtils {
 	
-	private String mapCatalogueManagerServletUrl;
 	
 	public static final String GET_STANDARD_HIERARCHY = "getStandardHierarchy";
 	public static final String GET_MAPS_BY_FEATURE = "getMapsByFeature";
@@ -53,254 +47,114 @@ public class MapCatalogueAccessUtils {
 	public static final String ERROR_FEATURE_NOT_FOUND = ERROR_PREFIX + "03";
 	public static final String ERROR_MAP_URL_NOT_FOUND = ERROR_PREFIX + "04";
 	
-	public MapCatalogueAccessUtils(String eventsManagerServletUrl) {
-		this.mapCatalogueManagerServletUrl = eventsManagerServletUrl;
+	public MapCatalogueAccessUtils() {
 	}
 	
-	public String getStandardHierarchy() throws Exception {
-		HttpClient client = new HttpClient();
-	    PostMethod httppost = new PostMethod(mapCatalogueManagerServletUrl);
-	    NameValuePair[] parameters = {  new NameValuePair("operation", GET_STANDARD_HIERARCHY) };
-	    
-	    //	  Provide custom retry handler is necessary
-	    DefaultMethodRetryHandler retryhandler = new DefaultMethodRetryHandler();
-	    retryhandler.setRequestSentRetryEnabled(false);
-	    retryhandler.setRetryCount(3);
-	    httppost.setMethodRetryHandler(retryhandler);
-	    httppost.setRequestBody(parameters);
-        byte[] responseBody = null; 
-        try {
-            // Execute the method.        	
-            int statusCode = client.executeMethod(httppost);
-            if (statusCode != HttpStatus.SC_OK) {
-              System.err.println("Method failed: " + httppost.getStatusLine());
-            }
-            // Read the response body.
-             responseBody  = httppost.getResponseBody();
-        } catch (IOException e) {
-        	throw new Exception("Error while getting default hierarchy");
-        } finally {
-            // Release the connection.
-          	httppost.releaseConnection();
-        }
-        
-        if (responseBody == null) throw new Exception("Error while getting default hierarchy");
-        
-        String result = new String(responseBody);
-        if(result.equalsIgnoreCase(ERROR_HIERARCHY_NOT_FOUND)) {        
-        	throw new Exception("Default Hierarchy not found. ");
-        }
-       
-        
-        return result;
+	public String getStandardHierarchy(String userId) throws Exception {
+
+	ContentServiceProxy proxy = new ContentServiceProxy();
+	String ris = proxy.mapCatalogue(userId, GET_STANDARD_HIERARCHY, null, null, null);
+	if (ris == null)
+	    throw new Exception("Error while getting default hierarchy");
+	if (ris.equalsIgnoreCase(ERROR_HIERARCHY_NOT_FOUND)) {
+	    throw new Exception("Default Hierarchy not found. ");
 	}
+	return ris;
+    }
 	
-	public List getMapNamesByFeature(String featureName) throws Exception {
-		HttpClient client = new HttpClient();
-	    PostMethod httppost = new PostMethod(mapCatalogueManagerServletUrl);
-	    NameValuePair[] parameters = {  new NameValuePair("operation", GET_MAPS_BY_FEATURE), 
-	    								new NameValuePair("featureName", featureName) };
-	    
-	    //	  Provide custom retry handler is necessary
-	    DefaultMethodRetryHandler retryhandler = new DefaultMethodRetryHandler();
-	    retryhandler.setRequestSentRetryEnabled(false);
-	    retryhandler.setRetryCount(3);
-	    httppost.setMethodRetryHandler(retryhandler);
-	    httppost.setRequestBody(parameters);
-        byte[] responseBody = null; 
-        try {
-            // Execute the method.        	
-            int statusCode = client.executeMethod(httppost);
-            if (statusCode != HttpStatus.SC_OK) {
-              System.err.println("Method failed: " + httppost.getStatusLine());
-            }
-            // Read the response body.
-             responseBody  = httppost.getResponseBody();
-        } catch (IOException e) {
-        	throw new Exception("Error while reading maps about feature " + featureName);
-        } finally {
-            // Release the connection.
-          	httppost.releaseConnection();
-        }
-        
-        if (responseBody == null) throw new Exception("Error while reading maps about feature " + featureName);
-        
-        String result = new String(responseBody);
-        if(result.equalsIgnoreCase(ERROR_MAP_NOT_FOUND)) {        
-        	throw new Exception("Maps about " + featureName + " not found. ");
-        }
-        
-        String[] maps = result.split(",");
-        List mapList = new ArrayList();
-        for(int i = 0; i < maps.length; i++) {
-        	mapList.add(maps[i]);        	
-        }
-        
-        return mapList;
+	public List getMapNamesByFeature(String userId,String featureName) throws Exception {
+
+	ContentServiceProxy proxy = new ContentServiceProxy();
+	String ris = proxy.mapCatalogue(userId, GET_MAPS_BY_FEATURE, null, featureName, null);
+	if (ris == null)
+	    throw new Exception("Error while reading maps about feature " + featureName);
+	if (ris.equalsIgnoreCase(ERROR_FEATURE_NOT_FOUND)) {
+	    throw new Exception("Error while reading maps about feature " + featureName);
 	}
 
-	public List getFeatureNamesInMap(String mapName) throws Exception {
-		HttpClient client = new HttpClient();
-	    PostMethod httppost = new PostMethod(mapCatalogueManagerServletUrl);
-	    NameValuePair[] parameters = {  new NameValuePair("operation", GET_FEATURES_IN_MAP), 
-	    								new NameValuePair("mapName", mapName) };
-	    
-	    //	  Provide custom retry handler is necessary
-	    DefaultMethodRetryHandler retryhandler = new DefaultMethodRetryHandler();
-	    retryhandler.setRequestSentRetryEnabled(false);
-	    retryhandler.setRetryCount(3);
-	    httppost.setMethodRetryHandler(retryhandler);
-	    httppost.setRequestBody(parameters);
-        byte[] responseBody = null; 
-        try {
-            // Execute the method.        	
-            int statusCode = client.executeMethod(httppost);
-            if (statusCode != HttpStatus.SC_OK) {
-              System.err.println("Method failed: " + httppost.getStatusLine());
-            }
-            // Read the response body.
-             responseBody  = httppost.getResponseBody();
-        } catch (IOException e) {
-        	throw new Exception("Error while reading features about map " + mapName);
-        } finally {
-            // Release the connection.
-          	httppost.releaseConnection();
-        }
-        
-        if (responseBody == null) throw new Exception("Error while reading features about map " + mapName);
-        
-        String result = new String(responseBody);
-        if(result.startsWith(ERROR_FEATURE_NOT_FOUND)) {        	
-        	throw new Exception("Features about " + mapName + " not found. ");
-        }
-        
-        String[] features = result.split(",");
-        List featureList = new ArrayList();
-        for(int i = 0; i < features.length; i++) {
-        	featureList.add(features[i]);        	
-        }
-        
-        return featureList;
+	if (ris.equalsIgnoreCase(ERROR_MAP_NOT_FOUND)) {
+	    throw new Exception("Maps about " + featureName + " not found. ");
 	}
+
+	String[] maps = ris.split(",");
+	List mapList = new ArrayList();
+	for (int i = 0; i < maps.length; i++) {
+	    mapList.add(maps[i]);
+	}
+
+	return mapList;
+    }
+
+	public List getFeatureNamesInMap(String userId,String mapName) throws Exception {
+
+	ContentServiceProxy proxy = new ContentServiceProxy();
+	String ris = proxy.mapCatalogue(userId, GET_FEATURES_IN_MAP, null, null, mapName);
+
+	if (ris == null)
+	    throw new Exception("Error while reading features about map " + mapName);
+
+	if (ris.startsWith(ERROR_FEATURE_NOT_FOUND)) {
+	    throw new Exception("Features about " + mapName + " not found. ");
+	}
+
+	String[] features = ris.split(",");
+	List featureList = new ArrayList();
+	for (int i = 0; i < features.length; i++) {
+	    featureList.add(features[i]);
+	}
+
+	return featureList;
+    }
 	
-	public String getMapUrl(String mapName)throws Exception {
-		HttpClient client = new HttpClient();
-	    PostMethod httppost = new PostMethod(mapCatalogueManagerServletUrl);
-	    NameValuePair[] parameters = {  new NameValuePair("operation", GET_MAP_URL), 
-	    								new NameValuePair("mapName", mapName) };
-	    
-	    //	  Provide custom retry handler is necessary
-	    DefaultMethodRetryHandler retryhandler = new DefaultMethodRetryHandler();
-	    retryhandler.setRequestSentRetryEnabled(false);
-	    retryhandler.setRetryCount(3);
-	    httppost.setMethodRetryHandler(retryhandler);
-	    httppost.setRequestBody(parameters);
-        byte[] responseBody = null; 
-        try {
-            // Execute the method.        	
-            int statusCode = client.executeMethod(httppost);
-            if (statusCode != HttpStatus.SC_OK) {
-              System.err.println("Method failed: " + httppost.getStatusLine());
-            }
-            // Read the response body.
-             responseBody  = httppost.getResponseBody();
-        } catch (IOException e) {
-        	throw new Exception("Error while reading map url " + mapName);
-        } finally {
-            // Release the connection.
-          	httppost.releaseConnection();
-        }
-        
-        if (responseBody == null) throw new Exception("Error while reading map url " + mapName);
-        
-        String result = new String(responseBody);
-        if(result.startsWith(ERROR_FEATURE_NOT_FOUND)) {        	
-        	throw new Exception("Map about " + mapName + " not found. ");
-        }
-        return result;
+	public String getMapUrl(String userId,String mapName) throws Exception {
+
+	ContentServiceProxy proxy = new ContentServiceProxy();
+	String ris = proxy.mapCatalogue(userId, GET_MAP_URL, null, null, mapName);
+
+	if (ris == null)
+	    throw new Exception("Error while reading map url " + mapName);
+
+	if (ris.startsWith(ERROR_FEATURE_NOT_FOUND)) {
+	    throw new Exception("Map about " + mapName + " not found. ");
 	}
+	return ris;
+    }
 	
-	public List getAllMapNames() throws Exception {
-		HttpClient client = new HttpClient();
-	    PostMethod httppost = new PostMethod(mapCatalogueManagerServletUrl);
-	    NameValuePair[] parameters = {  new NameValuePair("operation", GET_ALL_MAP_NAMES)};
-	    
-	    //	  Provide custom retry handler is necessary
-	    DefaultMethodRetryHandler retryhandler = new DefaultMethodRetryHandler();
-	    retryhandler.setRequestSentRetryEnabled(false);
-	    retryhandler.setRetryCount(3);
-	    httppost.setMethodRetryHandler(retryhandler);
-	    httppost.setRequestBody(parameters);
-        byte[] responseBody = null; 
-        try {
-            // Execute the method.        	
-            int statusCode = client.executeMethod(httppost);
-            if (statusCode != HttpStatus.SC_OK) {
-              System.err.println("Method failed: " + httppost.getStatusLine());
-            }
-            // Read the response body.
-             responseBody  = httppost.getResponseBody();
-        } catch (IOException e) {
-        	throw new Exception("Error while reading maps");
-        } finally {
-            // Release the connection.
-          	httppost.releaseConnection();
-        }
-        
-        if (responseBody == null) throw new Exception("Error while reading maps ");
-        
-        String result = new String(responseBody);
-        if(result.equalsIgnoreCase(ERROR_MAP_NOT_FOUND)) {        
-        	throw new Exception("Maps not found. ");
-        }        
-        String[] maps = result.split(",");
-        List mapList = new ArrayList();
-        for(int i = 0; i < maps.length; i++) {
-        	mapList.add(maps[i]);        	
-        }
-        
-        return mapList;
+	public List getAllMapNames(String userId) throws Exception {
+
+	ContentServiceProxy proxy = new ContentServiceProxy();
+	String ris = proxy.mapCatalogue(userId, GET_ALL_MAP_NAMES, null, null, null);
+
+	if (ris == null)
+	    throw new Exception("Error while reading maps ");
+
+	if (ris.equalsIgnoreCase(ERROR_MAP_NOT_FOUND)) {
+	    throw new Exception("Maps not found. ");
 	}
+	String[] maps = ris.split(",");
+	List mapList = new ArrayList();
+	for (int i = 0; i < maps.length; i++) {
+	    mapList.add(maps[i]);
+	}
+
+	return mapList;
+    }
 	
-	public List getAllFeatureNames() throws Exception {
-		HttpClient client = new HttpClient();
-	    PostMethod httppost = new PostMethod(mapCatalogueManagerServletUrl);
-	    NameValuePair[] parameters = {  new NameValuePair("operation", GET_ALL_FEATURE_NAMES)};
-	    
-	    //	  Provide custom retry handler is necessary
-	    DefaultMethodRetryHandler retryhandler = new DefaultMethodRetryHandler();
-	    retryhandler.setRequestSentRetryEnabled(false);
-	    retryhandler.setRetryCount(3);
-	    httppost.setMethodRetryHandler(retryhandler);
-	    httppost.setRequestBody(parameters);
-        byte[] responseBody = null; 
-        try {
-            // Execute the method.        	
-            int statusCode = client.executeMethod(httppost);
-            if (statusCode != HttpStatus.SC_OK) {
-              System.err.println("Method failed: " + httppost.getStatusLine());
-            }
-            // Read the response body.
-             responseBody  = httppost.getResponseBody();
-        } catch (IOException e) {
-        	throw new Exception("Error while reading Features");
-        } finally {
-            // Release the connection.
-          	httppost.releaseConnection();
-        }
-        
-        if (responseBody == null) throw new Exception("Error while reading Features ");
-        
-        String result = new String(responseBody);
-        if(result.equalsIgnoreCase(ERROR_FEATURE_NOT_FOUND)) {        
-        	throw new Exception("Feature not found. ");
-        }        
-        String[] maps = result.split(",");
-        List mapList = new ArrayList();
-        for(int i = 0; i < maps.length; i++) {
-        	mapList.add(maps[i]);        	
-        }
-        
-        return mapList;
+	public List getAllFeatureNames(String userId) throws Exception {
+	ContentServiceProxy proxy = new ContentServiceProxy();
+	String ris = proxy.mapCatalogue(userId, GET_ALL_FEATURE_NAMES, null, null, null);
+	if (ris == null)
+	    throw new Exception("Error while reading Features ");
+
+	if (ris.equalsIgnoreCase(ERROR_FEATURE_NOT_FOUND)) {
+	    throw new Exception("Feature not found. ");
 	}
+	String[] maps = ris.split(",");
+	List mapList = new ArrayList();
+	for (int i = 0; i < maps.length; i++) {
+	    mapList.add(maps[i]);
+	}
+
+	return mapList;
+    }
 }

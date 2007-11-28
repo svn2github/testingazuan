ba22ab98f182a235356cdc47635d2df9715d9dfe
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-11-28 13:07:30 +0000

    Initial import.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3485 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ SpagoBIBirtReportEngine/src/it/eng/spagobi/engines/birt/exceptions/ConnectionParameterNotValidException.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * LICENSE: see BIRT.LICENSE.txt file
+ * 
+ */
+package it.eng.spagobi.engines.birt.exceptions;
+
+public class ConnectionParameterNotValidException extends ConnectionDefinitionException {
+	
+	public ConnectionParameterNotValidException(String msg) {
+		super(msg);
+	}
+	
+}

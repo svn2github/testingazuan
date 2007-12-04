6c9a47b44819024fa0fbe29349346413119a3037
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-12-04 10:30:11 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3501 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/services/audit/AuditService.java
+++
@@ -1,6 +0,0 @@
-package it.eng.spagobi.services.audit;
-
-public interface AuditService {
-
-    String log(String token,String user,String id,String start,String end,String state,String message,String errorCode);
-}

fe55aa6d0cbf46968f486a59d0d97baf2beecf13
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2007-12-04 09:00:41 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3500 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIBirtReportEngine/src/it/eng/spagobi/engines/birt/BirtReportServlet.java
+++ V2.x/trunk/SpagoBIBirtReportEngine/src/it/eng/spagobi/engines/birt/BirtReportServlet.java
@@ -18,20 +18,10 @@ import it.eng.spagobi.services.security.exceptions.SecurityException;
 import it.eng.spagobi.utilities.ParametersDecoder;
 import it.eng.spagobi.utilities.callbacks.audit.AuditAccessUtils;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.security.InvalidKeyException;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.Signature;
-import java.security.SignatureException;
-import java.security.spec.EncodedKeySpec;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -54,10 +44,6 @@ import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Node;
-import org.dom4j.io.SAXReader;
 import org.eclipse.birt.report.IBirtConstants;
 import org.eclipse.birt.report.engine.api.HTMLActionHandler;
 import org.eclipse.birt.report.engine.api.HTMLRenderOption;
@@ -78,120 +64,22 @@ public class BirtReportServlet extends HttpServlet {
 	private IReportEngine birtReportEngine = null;
 	protected static Logger logger = Logger.getLogger(BirtReportServlet.class);
 	
-	protected transient PublicKey publicKeyDSASbi = null;
-	protected transient boolean securityAble = true;
+
 	protected String documentId = null;
 	protected String userId = null;
 	
 	public void init(ServletConfig servletConfig) throws ServletException {
         super.init(servletConfig);
-        logger.debug(this.getClass().getName() +":init:Initializing SpagoBI BirtReport Engine...");
-        String secAblePar = servletConfig.getInitParameter("SECURITY_ABLE");
-        if (!secAblePar.equalsIgnoreCase("true")) {
-        	securityAble = false;
-        }
-        if (securityAble) {
-        	// get spagobi public key
-        	publicKeyDSASbi = getPublicKey();        	
-        	
-        	// Identity hashMap
-        	Map identities = new HashMap();
-        	// set into the context
-        	ServletContext context = this.getServletContext();
-        	context.setAttribute("IDENTITIES", identities);
-        }
+        logger.debug("Initializing SpagoBI BirtReport Engine...");
         BirtEngine.initBirtConfig();
-        logger.debug(this.getClass().getName() +":init:Inizialization " +
-	      "of SpagoBI BirtReport Engine ended succesfully");
-     } // public void init(ServletConfig config) throws ServletException
+        logger.debug(":init:Inizialization of SpagoBI BirtReport Engine ended succesfully");
+     } 
 
 	public void destroy() {
 		super.destroy();
 		BirtEngine.destroyBirtEngine();
 	}
 	
-	/**
-	 * Get the SpagoBI Public Key for a DSA alghoritm
-	 * @return Public Key for SpagoBI (DSA alghoritm)
-	 */
-	private PublicKey getPublicKey() {
-		PublicKey pubKey = null;
-		SAXReader reader = new SAXReader();
-		Document document = null;
-		try {
-			document = reader.read(getClass().getResourceAsStream("/security-config.xml"));
-			Node publicKeyNode = document.selectSingleNode( "//SECURITY-CONFIGURATION/KEYS/SPAGOBI_PUBLIC_KEY_DSA");
-			String namePubKey =  publicKeyNode.valueOf("@keyname");
-			InputStream publicKeyIs = this.getClass().getClassLoader().getResourceAsStream(namePubKey);	    
-		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		    byte[] buffer = new byte[1024];
-		    int len;
-		    while ((len = publicKeyIs.read(buffer)) >= 0)
-		    	baos.write(buffer, 0, len);
-		    publicKeyIs.close();
-		    baos.close();
-		    byte[] pubKeyByte = baos.toByteArray();
-		    // get the public key from bytes  
-		    KeyFactory keyFactory = KeyFactory.getInstance("DSA");
-		    EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(pubKeyByte);
-		    pubKey = keyFactory.generatePublic(publicKeySpec);
-		} catch (DocumentException de) {
-			logger.error(this.getClass().getName()+ ":getPublicKey:" +
-					 "Error during parsing of the security configuration file", de);
-		} catch (IOException e) {
-			logger.error(this.getClass().getName()+ ":getPublicKey:" +
-					"Error retriving the key file", e);
-		} catch (NoSuchAlgorithmException e) {
-			logger.error(this.getClass().getName()+ ":getPublicKey:" +
-					 "DSA Alghoritm not avaiable", e);
-		} catch (InvalidKeySpecException e) {
-			logger.error(this.getClass().getName()+ ":getPublicKey:" +
-					 "Invalid Key", e);
-		}
-		return pubKey;
-	}
-	
-	/**
-	 * Authenticate the caller (must be SpagoBI)
-	 * @param request HttpRequest
-	 * @param response HttpResponse
-	 * @return boolean, true if autheticated false otherwise
-	 */
-	protected boolean authenticate(HttpServletRequest request, HttpServletResponse response) {
-		String tokenClearStr = (String)request.getParameter("TOKEN_CLEAR");
-	    if (tokenClearStr == null || tokenClearStr.trim().equals("")) {
-	    	logger.error(this.getClass().getName()+ ":authenticate:" +
-	                 "Token clear null or empty");
-	        return false;
-	    }
-		String tokenSign64 = (String)request.getParameter("TOKEN_SIGN");
-		if (tokenSign64 == null || tokenSign64.trim().equals("")) {
-			logger.error(this.getClass().getName()+ ":authenticate:" +
-		                 "Token signed null or empty");
-		    return false;
-		}
-	    String identity = (String)request.getParameter("IDENTITY");
-	    if (identity == null || identity.trim().equals("")) {
-	    	logger.error(this.getClass().getName()+ ":authenticate:" +
-		                 "Identity null or empty");
-		    return false;
-		}	
-		byte[] tokenClear = tokenClearStr.getBytes();
-		if (tokenClear == null) {
-			logger.error(this.getClass().getName()+ ":authenticate:" +
-		                 "Token clear after base 64 decoding null");
-		    return false;
-		}
-		byte[] tokenSign = decodeBase64(tokenSign64); 
-		if (tokenClear == null) {
-			logger.error(this.getClass().getName()+ ":authenticate:" +
-		                 "Token clear after base 64 decoding null");
-		    return false;
-		}
-		// verify the signature
-		boolean sign = verifySignature(tokenClear, tokenSign);
-		return sign;
-	}
 	
 	public void service(
 	        HttpServletRequest request,
@@ -217,18 +105,7 @@ public class BirtReportServlet extends HttpServlet {
 		    throw new ServletException();
 		}
 		
-	 	if (securityAble && !authenticate(request, response)) {
-	 		logger.error(this.getClass().getName()+ ":service:" + "Authentication failed");
-	 		PrintWriter writer = response.getWriter();
-	 		String resp = "<html><body><center><h2>Unauthorized</h2></center></body></html>";
-	 		writer.write(resp);
-	 		writer.flush();
-	 		writer.close();
-	 		return;
-	 	} else {
-	 		if (securityAble) 
-				logger.info(this.getClass().getName() +":service: Caller authenticated succesfully");
-	 		
+	 	
 			// AUDIT UPDATE
 			String auditId = request.getParameter("SPAGOBI_AUDIT_ID");
 			AuditAccessUtils auditAccessUtils = 
@@ -266,57 +143,14 @@ public class BirtReportServlet extends HttpServlet {
 		 					"EXECUTION_FAILED", e.getMessage(), null);
 		 		}	
 	 		}
-	 	}
+	 	
 	 	
 	 	logger.info(" Request processed");
 	 	
 	}
 	
-	/**
-	 * Decode a Base64 String into a byte array
-	 * @param encoded String encoded with Base64 algorithm
-	 * @return byte array decoded
-	 */
-	private byte[] decodeBase64(String encoded) {
-		byte[] clear = null;
-		try{
-			BASE64Decoder decoder = new BASE64Decoder();
-			clear = decoder.decodeBuffer(encoded);
-			return clear;
-		} catch (IOException ioe) {
-			logger.error(this.getClass().getName()+ ":decodeBase64:" +
-					     "Error during base64 decoding", ioe);
-		}
-		return clear;
-	}
 	
 	
-	/**
-	 * Verify the signature 
-	 * @param tokenclear Clear data
-	 * @param tokensign Signed data
-	 * @return
-	 */
-	private boolean verifySignature(byte[] tokenclear, byte[] tokensign) {
-		try {
-			Signature sign = Signature.getInstance("DSA");
-			sign.initVerify(publicKeyDSASbi);
-			sign.update(tokenclear);
-			return sign.verify(tokensign);
-		} catch (NoSuchAlgorithmException e) {
-			logger.error(this.getClass().getName()+ ":verifySignature:" + 
-				     "DSA Algorithm not avaiable", e);
-			return false;
-		} catch (InvalidKeyException e) {
-			logger.error(this.getClass().getName()+ ":verifySignature:" +  
-				     "Invalid Key", e);
-			return false;
-		} catch (SignatureException e) {
-			logger.error(this.getClass().getName()+ ":verifySignature:" + 
-				     "Error while verifing the exception", e);
-			return false;
-		} 
-	}
 	
 	private String decodeParameter(Object paramaterValue) {
 		if (paramaterValue == null) return null;
@@ -357,27 +191,7 @@ public class BirtReportServlet extends HttpServlet {
 		this.birtReportEngine.getConfig().getEmitterConfigs().put("html", renderOption);
 		
 		return renderOption;
-		
-		/*
-		HTMLEmitterConfig emitterConfig = new HTMLEmitterConfig();
-		emitterConfig.setActionHandler(new HTMLActionHandler());
-		HTMLServerImageHandler imageHandler = new HTMLServerImageHandler();
-		emitterConfig.setImageHandler(imageHandler);
-		this.birtReportEngine.getConfig().getEmitterConfigs().put("html", emitterConfig);
-		
-		HTMLRenderContext renderContext = new HTMLRenderContext();
-		renderContext.setImageDirectory(imageDirectory);
-		renderContext.setBaseImageURL(contextPath + imageBaseUrl);
-		renderContext.setBaseURL(contextPath + "/frameset");
-		//renderContext.setSupportedImageFormats( svgFlag ? "PNG;GIF;JPG;BMP;SVG" : "PNG;GIF;JPG;BMP" ); //$NON-NLS-1$ //$NON-NLS-2$
-		
-		HashMap appContext = new HashMap();
-		//appContext.put(DataEngine.DATASET_CACHE_OPTION, Boolean.TRUE);
-		appContext.put(EngineConstants.APPCONTEXT_BIRT_VIEWER_HTTPSERVET_REQUEST, servletRequest);
-		appContext.put(EngineConstants.APPCONTEXT_CLASSLOADER_KEY, BirtReportServlet.class.getClassLoader());
-		//appContext.put(EngineConstants.APPCONTEXT_HTML_RENDER_CONTEXT, renderContext);
-		appContext.put(EngineConstants.APPCONTEXT_HTML_RENDER_CONTEXT, renderOption);
-		*/
+
 
 	}
 	
@@ -614,23 +428,6 @@ public class BirtReportServlet extends HttpServlet {
 			renderOption = prepareHtmlRenderOption(servletContext, request);
 			renderOption.setOutputFormat(IBirtConstants.HTML_RENDER_FORMAT);
 		}
-		
-		//test --------------------
-		/*
-		SpagoBiDataSource ds = findDataSource(documentId);
-		if (ds != null){
-			logger.debug("DataSource founded.");
-			if(ds.getJndiName() != null && ds.getJndiName() != ""){
-				reportParams.put("connectionName", ds.getJndiName());
-			}
-			else{
-				BirtJDBCDataSource jdbc = new BirtJDBCDataSource(ds.getDriver(), ds.getUrl(), ds.getUser(), ds.getPassword());
-				DataEngine de = DataEngine.newDataEngine( DataEngineContext.newInstance( DataEngineContext.DIRECT_PRESENTATION, null,null,null ) );
-				de.defineDataSource(jdbc.getOdaDataSourceDesign());
-			}
-		}
-		*/
-		//fine test --------------------
 		
 		Map context = BirtUtility.getAppContext(request, BirtReportServlet.class.getClassLoader());
 		task.setAppContext(context);

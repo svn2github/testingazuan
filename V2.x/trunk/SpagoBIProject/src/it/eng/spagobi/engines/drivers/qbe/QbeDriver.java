f9d06fb17232169f496f66dce8781a5f8a01dbc0
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-01-10 09:05:32 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3627 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/drivers/qbe/QbeDriver.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/drivers/qbe/QbeDriver.java
@@ -9,7 +9,7 @@ import it.eng.spago.base.SourceBean;
 import it.eng.spago.configuration.ConfigSingleton;
 import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
-import it.eng.spagobi.analiticalmodel.document.bo.BIObject.SubObjectDetail;
+import it.eng.spagobi.analiticalmodel.document.bo.SubObject;
 import it.eng.spagobi.behaviouralmodel.analyticaldriver.bo.BIObjectParameter;
 import it.eng.spagobi.commons.utilities.GeneralUtilities;
 import it.eng.spagobi.commons.utilities.ParameterValuesEncoder;
@@ -25,6 +25,8 @@ import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import sun.misc.BASE64Encoder;
 
 
@@ -33,44 +35,10 @@ import sun.misc.BASE64Encoder;
  * Driver Implementation (IEngineDriver Interface) for Qbe External Engine. 
  */
 public class QbeDriver implements IEngineDriver {
-
-	private void addLocale(Map map) {
-		ConfigSingleton config = ConfigSingleton.getInstance();
-		Locale portalLocale = null;
-		try {
-			portalLocale =  PortletUtilities.getPortalLocale();
-		} catch (Exception e) {
-			SpagoBITracer.major("ENGINES",
-					this.getClass().getName(),
-					"addLocale(Map)",
-					"Error while getting portal locale.");
-			portalLocale = new Locale("en", "US");
-		}
-		SpagoBITracer.debug("ENGINES", this.getClass().getName(), "addLocale(Map)", 
-				"Portal locale: " + portalLocale);
-		
-		
-		SourceBean languageSB = null;
-		if(portalLocale != null && portalLocale.getLanguage() != null) {
-			languageSB = (SourceBean)config.getFilteredSourceBeanAttribute("SPAGOBI.LANGUAGE_SUPPORTED.LANGUAGE", 
-					"language", portalLocale.getLanguage());
-			SpagoBITracer.debug("ENGINES", this.getClass().getName(), "addLocale(Map)", 
-					"Found language configuration for portal locale [" + portalLocale + "]: " + languageSB);
-		}
-		if(languageSB != null) {
-			map.put("country", (String)languageSB.getAttribute("country"));
-			map.put("language", (String)languageSB.getAttribute("language"));
-			SpagoBITracer.debug("ENGINES", this.getClass().getName(), "addLocale(Map)", 
-					"Adding [" + languageSB.getAttribute("country") + "," +
-							languageSB.getAttribute("language") + "] locale.");
-		} else {
-			map.put("country", "US");
-			map.put("language", "en");
-			SpagoBITracer.debug("ENGINES", this.getClass().getName(), "addLocale(Map)", 
-					"Adding [en,US] locale as default.");
-		}			
-	}
 	
+	static private Logger logger = Logger.getLogger(QbeDriver.class);
+	 
+		
 	/**
 	 * Returns a map of parameters which will be send in the request to the 
 	 * engine application.
@@ -80,20 +48,22 @@ public class QbeDriver implements IEngineDriver {
 	 * @return Map The map of the execution call parameters
 	 */
 	public Map getParameterMap(Object biobject, IEngUserProfile profile, String roleName) {
+		logger.debug("IN");
+		
 		Map map = new Hashtable();
 		try{
 			BIObject biobj = (BIObject)biobject;
 			map = getMap(biobj);
 			map.put("query", "#");
-			map.put("user", profile.getUserUniqueIdentifier());
+			//map.put("user", profile.getUserUniqueIdentifier());
 		} catch (ClassCastException cce) {
-			SpagoBITracer.major("ENGINES",
-					this.getClass().getName(),
-					"getParameterMap(Object)",
-					"The parameter is not a BIObject type",
-					cce);
+			logger.error("The parameter is not a BIObject type", cce);
 		} 
-		map = applySecurity(map);
+		
+		map = applySecurity(map, profile);
+		map = applyLocale(map);
+		
+		logger.debug("OUT");
 		return map;
 	}
 	
@@ -107,35 +77,31 @@ public class QbeDriver implements IEngineDriver {
 	 * @return Map The map of the execution call parameters
   	 */
 	public Map getParameterMap(Object object, Object subObject, IEngUserProfile profile, String roleName) {
-		if(subObject == null) return getParameterMap(object, profile, roleName);
+		logger.debug("IN");			
+			
+		if(subObject == null) {
+			return getParameterMap(object, profile, roleName);
+		}
 		
 		Map map = new Hashtable();
 		try{
 			BIObject biobj = (BIObject)object;
 			map = getMap(biobj);
-			SubObjectDetail subObjectDetail = (SubObjectDetail) subObject;
+			SubObject subObjectDetail = (SubObject) subObject;
 			map.put("query", subObjectDetail.getName());
-			map.put("user", profile.getUserUniqueIdentifier());
+			//map.put("user", profile.getUserUniqueIdentifier());
 		} catch (ClassCastException cce) {
-			SpagoBITracer.major("ENGINES",
-					this.getClass().getName(),
-					"getParameterMap(Object, SubObject)",
-					"The parameter is not a BIObject type",
-					cce);
+			logger.error("The parameter is not a BIObject type", cce);
 		} 
-		map = applySecurity(map);
+		map = applySecurity(map, profile);
+		map = applyLocale(map);
+		
+		logger.debug("OUT");
 		return map;
 	}
 	
 	
-	/**
-	 * Applys changes for security reason if necessary
-	 * @param pars The map of parameters
-	 * @return the map of parameters to send to the engine 
-	 */
-	protected Map applySecurity(Map pars) {
-		return pars;
-	}
+	
 	     
     /**
      * Starting from a BIObject extracts from it the map of the paramaeters for the
@@ -144,23 +110,21 @@ public class QbeDriver implements IEngineDriver {
      * @return Map The map of the execution call parameters
      */    
 	private Map getMap(BIObject biobj) {
-		Map pars = new Hashtable();
+		logger.debug("IN");
 		
-		biobj.loadTemplate();
-		UploadedFile uploadedFile =  biobj.getTemplate();
-		byte[] template = uploadedFile.getFileContent();
-		BASE64Encoder bASE64Encoder = new BASE64Encoder();
-		pars.put("template", bASE64Encoder.encode(template));
-		pars.put("templatePath",biobj.getPath() + "/template");
-        pars.put("spagobiurl", GeneralUtilities.getSpagoBiContentRepositoryServlet());
-        //String mapCatalogueManagerUrl;
-        //mapCatalogueManagerUrl = "http://localhost:8080/spagobi/MapCatalogueManagerServlet";
-        //pars.put("mapCatalogueManagerUrl", mapCatalogueManagerUrl);
-        pars.put("mapCatalogueManagerUrl", GeneralUtilities.getMapCatalogueManagerServlet());
+		Map pars = new Hashtable();
+		String documentId=biobj.getId().toString();
+		pars.put("document", documentId);
+		logger.debug("Add document parameter:"+documentId);
+		
+		//pars.put("templatePath",biobj.getPath() + "/template");
+        // pars.put("spagobiurl", GeneralUtilities.getSpagoBiContentRepositoryServlet());
+        // pars.put("mapCatalogueManagerUrl", GeneralUtilities.getMapCatalogueManagerServlet());
         
-        addLocale(pars);
+        
         pars = addBIParameters(biobj, pars);
         
+        logger.debug("OUT");
         return pars;
 	} 
 	
@@ -171,12 +135,11 @@ public class QbeDriver implements IEngineDriver {
      * @return Map The map of the execution call parameters
      */
 	private Map addBIParameters(BIObject biobj, Map pars) {
+		logger.debug("IN");
+		
 		if(biobj==null) {
-			SpagoBITracer.warning("ENGINES",
-								  this.getClass().getName(),
-								  "addBIParameters",
-								  "BIObject parameter null");
-			return pars;
+			logger.warn("BIObject parameter null");	    
+		    return pars;
 		}
 		
 		ParameterValuesEncoder parValuesEncoder = new ParameterValuesEncoder();
@@ -184,50 +147,94 @@ public class QbeDriver implements IEngineDriver {
 			BIObjectParameter biobjPar = null;
 			for(Iterator it = biobj.getBiObjectParameters().iterator(); it.hasNext();){
 				try {
-					biobjPar = (BIObjectParameter)it.next();
-					
-										
+					biobjPar = (BIObjectParameter)it.next();									
 					String value = parValuesEncoder.encode(biobjPar);
 					pars.put(biobjPar.getParameterUrlName(), value);
+					logger.debug("Add parameter:"+biobjPar.getParameterUrlName()+"/"+value);
 				} catch (Exception e) {
-					SpagoBITracer.warning("ENGINES",
-										  this.getClass().getName(),
-										  "addBIParameters",
-										  "Error while processing a BIParameter",
-										  e);
+					logger.error("Error while processing a BIParameter",e);
 				}
 			}
 		}
+		
+		logger.debug("OUT");
   		return pars;
 	}
 	
-	/**
-	 * Function not implemented. Thid method should not be called
-	 * 
-	 * @param biobject The BIOBject to edit
-	 * @throws InvalidOperationRequest
-	 */
-	public EngineURL getEditDocumentTemplateBuildUrl(Object biobject) throws InvalidOperationRequest {
-		SpagoBITracer.major("ENGINES",
-				  this.getClass().getName(),
-				  "getEditDocumentTemplateBuildUrl",
-				  "Function not implemented");
-		throw new InvalidOperationRequest();
-	}
+	 /**
+     * Function not implemented. Thid method should not be called
+     * 
+     * @param biobject The BIOBject to edit
+     * @throws InvalidOperationRequest
+     */
+    public EngineURL getEditDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile)
+	throws InvalidOperationRequest {
+    	logger.warn("Function not implemented");
+    	throw new InvalidOperationRequest();
+    }
 
-	/**
-	 * Function not implemented. Thid method should not be called
-	 * 
-	 * @param biobject The BIOBject to edit
-	 * @throws InvalidOperationRequest
+    /**
+     * Function not implemented. Thid method should not be called
+     * 
+     * @param biobject  The BIOBject to edit
+     * @throws InvalidOperationRequest
+     */
+    public EngineURL getNewDocumentTemplateBuildUrl(Object biobject, IEngUserProfile profile)
+	throws InvalidOperationRequest {
+    	logger.warn("Function not implemented");
+    	throw new InvalidOperationRequest();
+    }
+
+    
+        
+    /**
+	 * Applys changes for security reason if necessary
+	 * @param pars The map of parameters
+	 * @return the map of parameters to send to the engine 
 	 */
-	public EngineURL getNewDocumentTemplateBuildUrl(Object biobject) throws InvalidOperationRequest {
-		SpagoBITracer.major("ENGINES",
-				  this.getClass().getName(),
-				  "getNewDocumentTemplateBuildUrl",
-				  "Function not implemented");
-		throw new InvalidOperationRequest();
+	protected Map applySecurity(Map pars, IEngUserProfile profile) {
+		logger.debug("IN");
+		pars.put("userId", profile.getUserUniqueIdentifier());
+		logger.debug("Add parameter: userId/"+ profile.getUserUniqueIdentifier());
+		logger.debug("OUT");
+		return pars;
 	}
-	
+    
+    private Map applyLocale(Map map) {
+    	logger.debug("IN");
+    	
+		ConfigSingleton config = ConfigSingleton.getInstance();
+		Locale portalLocale = null;
+		try {
+			portalLocale =  PortletUtilities.getPortalLocale();
+		} catch (Exception e) {
+			logger.warn("Impossible to get portal locale.");
+			logger.warn("Default lacale will be used (country: US; language: en).");
+			portalLocale = new Locale("en", "US");
+		}
+		
+		SourceBean languageSB = null;
+		if(portalLocale != null && portalLocale.getLanguage() != null) {
+			languageSB = (SourceBean)config.getFilteredSourceBeanAttribute("SPAGOBI.LANGUAGE_SUPPORTED.LANGUAGE", 
+					"language", portalLocale.getLanguage());
+		}
+		
+		if(languageSB != null) {
+			map.put("country", (String)languageSB.getAttribute("country"));
+			map.put("language", (String)languageSB.getAttribute("language"));
+			logger.debug("Added parameter: country/" + (String)languageSB.getAttribute("country"));
+			logger.debug("Added parameter: language/" + (String)languageSB.getAttribute("language"));
+		} else {
+			logger.warn("Language " + portalLocale.getLanguage() + " is not supported by SpagoBI");
+			logger.warn("Portal locale will be replaced with the default lacale (country: US; language: en).");
+			map.put("country", "US");
+			map.put("language", "en");
+			logger.debug("Added parameter: country/US");
+			logger.debug("Added parameter: language/en");
+		}			
+		
+		logger.debug("OUT");
+		return map;
+	}
 }
 

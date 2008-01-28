df139d92464e6059faef0cfb3e38f9896d315d0b
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-01-28 17:03:42 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3683 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/QbeCore/src/it/eng/qbe/query/Field.java
@@ -0,0 +1,76 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.qbe.query;
+
+
+public class Field implements IField {
+	private String id;
+	private String fieldName = null;
+	
+	private String type = null;
+	private Integer scale = null;
+	private Integer precision = null;
+	
+	public Field(String id) {
+		setId(id);
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}	
+	
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Integer getScale() {
+		return scale;
+	}
+
+	public void setScale(Integer scale) {
+		this.scale = scale;
+	}
+
+	public Integer getPrecision() {
+		return precision;
+	}
+
+	public void setPrecision(Integer precision) {
+		this.precision = precision;
+	}
+}

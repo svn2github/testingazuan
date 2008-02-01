7a0ebf800503eec2397408c3a8c405559647af09
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-02-01 11:54:56 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3729 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/officedocument/SpagoBIOfficeDocumentInternalEngine.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/officedocument/SpagoBIOfficeDocumentInternalEngine.java
@@ -23,11 +23,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 package it.eng.spagobi.engines.officedocument;
 
 import it.eng.spago.base.RequestContainer;
-import it.eng.spago.base.SessionContainer;
+
 import it.eng.spago.base.SourceBean;
 import it.eng.spago.error.EMFErrorSeverity;
 import it.eng.spago.error.EMFUserError;
-import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.analiticalmodel.document.bo.BIObject;
 import it.eng.spagobi.analiticalmodel.document.bo.ObjTemplate;
 import it.eng.spagobi.commons.constants.SpagoBIConstants;

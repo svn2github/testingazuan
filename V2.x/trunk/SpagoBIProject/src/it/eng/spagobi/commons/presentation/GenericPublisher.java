968ce6ca5451d0ce454f995145e941b91ca1143d
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-02-19 15:22:56 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3892 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/commons/presentation/GenericPublisher.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/commons/presentation/GenericPublisher.java
@@ -18,7 +18,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-**/
+ **/
 package it.eng.spagobi.commons.presentation;
 
 import it.eng.spago.base.RequestContainer;
@@ -33,36 +33,39 @@ import it.eng.spagobi.commons.utilities.GeneralUtilities;
 import it.eng.spagobi.commons.utilities.SpagoBITracer;
 
 public abstract class GenericPublisher implements PublisherDispatcherIFace {
+
+    protected String getPublisherName(RequestContainer requestContainer, ResponseContainer responseContainer,SourceBean moduleResponse) {
+	// get error handler
+	EMFErrorHandler errorHandler = responseContainer.getErrorHandler();
 	
-	protected String getPublisherName(RequestContainer requestContainer, ResponseContainer responseContainer, 
-			                          SourceBean moduleResponse) {
-		// get error handler
-		EMFErrorHandler errorHandler = responseContainer.getErrorHandler();
-		// if there are some errors (not validation error) into the errorHandler  return the name for the errors publisher
-		if(!errorHandler.isOKBySeverity(EMFErrorSeverity.ERROR)) {
-			if(!GeneralUtilities.isErrorHandlerContainingOnlyValidationError(errorHandler)) {
-				return "error";
-			}
-		}
-		// if the module response is null throws an error and return the name of the errors publisher
-		if (moduleResponse == null) {
-			SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), 
-					            "getPublisherName", "Module response null");
-			EMFUserError error = new EMFUserError(EMFErrorSeverity.ERROR, 10 );
-			errorHandler.addError(error);
-			return "error";
-		}
-		// get the value of the publisher name attribute
-	    String publisherName = (String) moduleResponse.getAttribute(SpagoBIConstants.PUBLISHER_NAME );
-	    if (publisherName != null &&  !publisherName.trim().equals("")) {
-	       	return publisherName;
-		} else {
-			SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), 
-		            			"getPublisherName", "Publisher name attribute not found");
-			EMFUserError error = new EMFUserError(EMFErrorSeverity.ERROR, 10 );
-			errorHandler.addError(error);
-			return "error";
-		}
+	// if there are some errors (not validation error) into the errorHandler
+	// return the name for the errors publisher
+	if (!errorHandler.isOKBySeverity(EMFErrorSeverity.ERROR)) {
+	    if (!GeneralUtilities.isErrorHandlerContainingOnlyValidationError(errorHandler)) {
+		return "error";
+	    }
 	}
+	
+	// if the module response is null throws an error and return the name of
+	// the errors publisher
+	if (moduleResponse == null) {
+	    SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), "getPublisherName",
+		    "Module response null");
+	    EMFUserError error = new EMFUserError(EMFErrorSeverity.ERROR, 10);
+	    errorHandler.addError(error);
+	    return "error";
+	}
+	// get the value of the publisher name attribute
+	String publisherName = (String) moduleResponse.getAttribute(SpagoBIConstants.PUBLISHER_NAME);
+	if (publisherName != null && !publisherName.trim().equals("")) {
+	    return publisherName;
+	} else {
+	    SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), "getPublisherName",
+		    "Publisher name attribute not found");
+	    EMFUserError error = new EMFUserError(EMFErrorSeverity.ERROR, 10);
+	    errorHandler.addError(error);
+	    return "error";
+	}
+    }
 
 }

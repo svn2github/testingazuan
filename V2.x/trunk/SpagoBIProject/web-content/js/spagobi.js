b8785a79441cacbb083612a470cc027e2c4675c8
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-02-27 09:02:06 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@3995 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/web-content/js/spagobi.js
+++ V2.x/trunk/SpagoBIProject/web-content/js/spagobi.js
@@ -247,10 +247,43 @@ function toggle(elementId, togglerId, toggled) {
 	Ext.onReady(function() {
 		var element = Ext.get(elementId);
 	    element.enableDisplayMode("block");
-	    if (toggled) element.toggle(true);
-	
-	    Ext.get(togglerId).on('click', function(){
+	    var value = getCookie(elementId);
+	    if (value == '') {
+	    	if (toggled) {
+	    		element.toggle(true);
+	    		setCookie(elementId, 'open');
+	    	}
+	    } else {
+	    	if (value == 'open') element.toggle(true);
+	    }
+	    
+	    Ext.get(togglerId).on('click', function() {
+	    	if (value == 'open') setCookie(elementId, 'closed');
+	    	else setCookie(elementId, 'open');
 	        element.toggle(true);
 	    });
 	});
 }
+
+// [Cookie] Gets a value from a cookie
+function getCookie(cookieName) {
+	var cookieValue = '';
+	var posName = document.cookie.indexOf(escape(cookieName) + '=');
+	if (posName != -1) {
+		var posValue = posName + (escape(cookieName) + '=').length;
+		var endPos = document.cookie.indexOf(';', posValue);
+		if (endPos != -1) cookieValue = unescape(document.cookie.substring(posValue, endPos));
+		else cookieValue = unescape(document.cookie.substring(posValue));
+	}
+	return (cookieValue);
+};
+
+// [Cookie] Sets value in a cookie
+function setCookie(cookieName, cookieValue, expires, path, domain, secure) {
+	document.cookie =
+		escape(cookieName) + '=' + escape(cookieValue)
+		+ (expires ? '; expires=' + expires.toGMTString() : '')
+		+ (path ? '; path=' + path : '')
+		+ (domain ? '; domain=' + domain : '')
+		+ (secure ? '; secure' : '');
+};

e5f6f16df4c0c933b747a7c1d4d143d2bf4ed79d
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-03-06 15:05:47 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@4089 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/behaviouralmodel/lov/service/GetLovResultAction.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/behaviouralmodel/lov/service/GetLovResultAction.java
@@ -1,5 +1,7 @@
 package it.eng.spagobi.behaviouralmodel.lov.service;
 
+import it.eng.spago.base.RequestContainer;
+import it.eng.spago.base.SessionContainer;
 import it.eng.spago.base.SourceBean;
 import it.eng.spago.configuration.ConfigSingleton;
 import it.eng.spago.dispatching.action.AbstractHttpAction;
@@ -30,173 +32,180 @@ import org.apache.log4j.Logger;
 
 public class GetLovResultAction extends AbstractHttpAction {
 
-    private static transient Logger logger=Logger.getLogger(GetLovResultAction.class);
-    
-	public void service(SourceBean requestSB, SourceBean responseSB) throws Exception {
-		logger.debug("IN");
-		
-		freezeHttpResponse();
-		HttpServletRequest request = getHttpRequest();
-		HttpServletResponse response = getHttpResponse();
-		
-		// AUDIT UPDATE
-		Integer auditId = null;
-		String auditIdStr = request.getParameter("SPAGOBI_AUDIT_ID");
-		if (auditIdStr == null) {
-			logger.warn("Audit record id not specified! No operations will be performed");
+    private static transient Logger logger = Logger.getLogger(GetLovResultAction.class);
+
+    public void service(SourceBean requestSB, SourceBean responseSB) throws Exception {
+	logger.debug("IN");
+
+	freezeHttpResponse();
+	HttpServletRequest request = getHttpRequest();
+	HttpServletResponse response = getHttpResponse();
+
+	// AUDIT UPDATE
+	Integer auditId = null;
+	String auditIdStr = request.getParameter("SPAGOBI_AUDIT_ID");
+	if (auditIdStr == null) {
+	    logger.warn("Audit record id not specified! No operations will be performed");
+	} else {
+	    logger.debug("Audit id = [" + auditIdStr + "]");
+	    auditId = new Integer(auditIdStr);
+	}
+	AuditManager auditManager = AuditManager.getInstance();
+	if (auditId != null) {
+	    auditManager.updateAudit(auditId, new Long(System.currentTimeMillis()), null, "EXECUTION_STARTED", null,
+		    null);
+	}
+	IEngUserProfile profile = null;
+	String userId = null;
+	try {
+	    RequestContainer reqCont = RequestContainer.getRequestContainer();
+	    SessionContainer sessCont = reqCont.getSessionContainer();
+	    SessionContainer permSess = sessCont.getPermanentContainer();
+	    profile = (IEngUserProfile) permSess.getAttribute(IEngUserProfile.ENG_USER_PROFILE);
+	    if (profile == null) {
+		ConfigSingleton config = ConfigSingleton.getInstance();
+		SourceBean validateSB = (SourceBean) config.getAttribute("SPAGOBI_SSO.ACTIVE");
+		String active = (String) validateSB.getCharacters();
+		if (active != null && active.equals("true")) {
+		    IProxyService proxy = IProxyServiceFactory.createProxyService();
+		    userId = proxy.readUserId(request.getSession());
+		    logger.debug("got userId from IProxyService=" + userId);
 		} else {
-			logger.debug("Audit id = [" + auditIdStr + "]");
-			auditId = new Integer(auditIdStr);
+		    userId = request.getParameter("userId");
+		    logger.debug("got userId from Request=" + userId);
 		}
-		AuditManager auditManager = AuditManager.getInstance();
-		if (auditId != null) {
-			auditManager.updateAudit(auditId, new Long(System.currentTimeMillis()), null, 
-					"EXECUTION_STARTED", null, null);
-		}
-		
-	 	try {
-	 		
-			String userId = null;
-			ConfigSingleton config = ConfigSingleton.getInstance();
-			SourceBean validateSB =(SourceBean) config.getAttribute("SPAGOBI_SSO.ACTIVE");
-			String active = (String) validateSB.getCharacters();
-			if (active != null && active.equals("true")){
-				IProxyService proxy=IProxyServiceFactory.createProxyService();
-				userId=proxy.readUserId(request.getSession());
-				logger.debug("got userId from IProxyService="+userId);
-			} else {
-		    		userId = request.getParameter("userId");
-		    		logger.debug("got userId from Request="+userId);
-			}
-	 		
-			IEngUserProfile profile = null;
-			ISecurityServiceSupplier supplier=SecurityServiceSupplierFactory.createISecurityServiceSupplier();
-	        try {
-	            SpagoBIUserProfile user= supplier.createUserProfile(userId);
-	            profile=new UserProfile(user);
-	        } catch (Exception e) {
-	            throw new SecurityException();
-	        }			
 
-			String documentId = request.getParameter("documentId");
-			logger.debug("got parameter documentId="+documentId);
-			
-			// TODO control that template contains the reference to the lov to be executed
-			//ContentServiceImplSupplier c = new ContentServiceImplSupplier();
-			//Content template = c.readTemplate(userId, documentId);
-			//parse template ....
-	        
-	        
-	 		// get the mode (mode=single --> only one lov to execute, mode=list --> more than one lov to execute)
-	 		// if the parameter mode is not present the single mode is the default
-	 		String mode = request.getParameter("mode");
-	 		
-	 		String result = "";
-	 		
-	 		if (mode == null || !mode.equalsIgnoreCase("list")) {
-		 		// ge the lov name
-	 			String dataName = request.getParameter("dataname");
-	 			// if lov name is not present send an error
-	 			if((dataName==null) || dataName.trim().equals("")) {
-	 				response.getOutputStream().write(createErrorMsg(10, "Param dataname not found"));
-	 				response.getOutputStream().flush();
-			 		return;
-	 			}
-	 			// check if the lov is supported
-	 			if(!isLovSupported(dataName, response)) {
-	 				logger.debug("Lov is not supported.");
-	 				return;
-	 			}
-	 			// get the lov type
-	 			String type = getLovType(dataName);
-	 			// get the result
-	            if (profile == null) {
-	            	result = GeneralUtilities.getLovResult(dataName);
-	            } else {
-	            	result = GeneralUtilities.getLovResult(dataName, profile);
-	            }
-	            // if the lov is a query trasform the result to lower case (for flash dashboard)
-	            if (type.equalsIgnoreCase("QUERY")){
-	            	result = result.toLowerCase();
-	            }
-	            
-	 		} else {
-	 			Map lovMap =new HashMap();
-	 			Enumeration paramNames = request.getParameterNames();
-	 			while(paramNames.hasMoreElements()){
-	 				String paramKey = (String)paramNames.nextElement();
-	 				if(paramKey.startsWith("LovResLogName_")) {
-	 					String logicalName = paramKey.substring(14);
-	 					String paramValue = request.getParameter(paramKey);
-	 					if( !(paramValue==null) && !(paramValue.trim().equals("")) ) {
-	 						lovMap.put(logicalName, paramValue);
-	 					}
-	 				}
-	 			}
-	 			result = GeneralUtilities.getLovMapResult(lovMap);
-	 		}
-	 		
-	 		
-	 		// replace special characters
-            result = result.replaceAll("&lt;", "<");
-            result = result.replaceAll("&gt;", ">");
-            // write the result into response
-            response.getOutputStream().write(result.getBytes());
-            response.getOutputStream().flush();
-    		// AUDIT UPDATE
-    		auditManager.updateAudit(auditId, null, new Long(System.currentTimeMillis()), 
-    								"EXECUTION_PERFORMED", null, null);	
-		 	
-		 	
-	 	} catch(Exception e){
-	 		logger.error("Exception", e);
-	 		// AUDIT UPDATE
-	 		auditManager.updateAudit(auditId, null, new Long(System.currentTimeMillis()), 
-					"EXECUTION_FAILED", e.getMessage(), null);
-	 	} finally {
-	 		logger.debug("OUT");
-	 	}
-	}
-
-	private String getLovType(String lovName) {
-		String toReturn = "";
-		try{
-			IModalitiesValueDAO lovDAO = DAOFactory.getModalitiesValueDAO();
-			ModalitiesValue lov = lovDAO.loadModalitiesValueByLabel(lovName);
-		    String type = lov.getITypeCd();
-		    toReturn = type;
-		} catch (Exception e) {
-			logger.error("Error while recovering type of lov " + lovName);
-		}
-	    return toReturn;
-	}
-
-	
-	private boolean isLovSupported(String lovName, HttpServletResponse response) {
-		boolean toReturn = true;
+		ISecurityServiceSupplier supplier = SecurityServiceSupplierFactory.createISecurityServiceSupplier();
 		try {
-			IModalitiesValueDAO lovDAO = DAOFactory.getModalitiesValueDAO();
-	 		ModalitiesValue lov = lovDAO.loadModalitiesValueByLabel(lovName);
-	        String type = lov.getITypeCd();
-	        if(!type.equalsIgnoreCase("QUERY") && !type.equalsIgnoreCase("SCRIPT") && !type.equalsIgnoreCase("JAVA_CLASS")) {
-	        	SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(),
-			                        "isLovSupported", "Dashboard "+type+" lov Not yet Supported");
-	        	response.getOutputStream().write(createErrorMsg(12, "Dashboard  "+type+" lov not yet supported"));
-	        	response.getOutputStream().flush();
-	        	toReturn = false;
-	        }
+		    SpagoBIUserProfile user = supplier.createUserProfile(userId);
+		    profile = new UserProfile(user);
 		} catch (Exception e) {
-			toReturn = false;
-			logger.error("Error while checkin if lov " + lovName + " is supported");
+		    logger.error("Exception while creating user profile",e);
+		    throw new SecurityException();
 		}
-        return toReturn;
+
+	    }
+	    userId = (String) profile.getUserUniqueIdentifier();
+
+	    String documentId = request.getParameter("documentId");
+	    logger.debug("got parameter documentId=" + documentId);
+
+	    // TODO control that template contains the reference to the lov to
+	    // be executed
+	    // ContentServiceImplSupplier c = new ContentServiceImplSupplier();
+	    // Content template = c.readTemplate(userId, documentId);
+	    // parse template ....
+
+	    // get the mode (mode=single --> only one lov to execute, mode=list
+	    // --> more than one lov to execute)
+	    // if the parameter mode is not present the single mode is the
+	    // default
+	    String mode = request.getParameter("mode");
+
+	    String result = "";
+
+	    if (mode == null || !mode.equalsIgnoreCase("list")) {
+		// ge the lov name
+		String dataName = request.getParameter("dataname");
+		// if lov name is not present send an error
+		if ((dataName == null) || dataName.trim().equals("")) {
+		    response.getOutputStream().write(createErrorMsg(10, "Param dataname not found"));
+		    response.getOutputStream().flush();
+		    return;
+		}
+		// check if the lov is supported
+		if (!isLovSupported(dataName, response)) {
+		    logger.debug("Lov is not supported.");
+		    return;
+		}
+		// get the lov type
+		String type = getLovType(dataName);
+		// get the result
+		if (profile == null) {
+		    result = GeneralUtilities.getLovResult(dataName);
+		} else {
+		    result = GeneralUtilities.getLovResult(dataName, profile);
+		}
+		// if the lov is a query trasform the result to lower case (for
+		// flash dashboard)
+		if (type.equalsIgnoreCase("QUERY")) {
+		    result = result.toLowerCase();
+		}
+
+	    } else {
+		Map lovMap = new HashMap();
+		Enumeration paramNames = request.getParameterNames();
+		while (paramNames.hasMoreElements()) {
+		    String paramKey = (String) paramNames.nextElement();
+		    if (paramKey.startsWith("LovResLogName_")) {
+			String logicalName = paramKey.substring(14);
+			String paramValue = request.getParameter(paramKey);
+			if (!(paramValue == null) && !(paramValue.trim().equals(""))) {
+			    lovMap.put(logicalName, paramValue);
+			}
+		    }
+		}
+		result = GeneralUtilities.getLovMapResult(lovMap);
+	    }
+
+	    // replace special characters
+	    result = result.replaceAll("&lt;", "<");
+	    result = result.replaceAll("&gt;", ">");
+	    // write the result into response
+	    response.getOutputStream().write(result.getBytes());
+	    response.getOutputStream().flush();
+	    // AUDIT UPDATE
+	    auditManager.updateAudit(auditId, null, new Long(System.currentTimeMillis()), "EXECUTION_PERFORMED", null,
+		    null);
+
+	} catch (Exception e) {
+	    logger.error("Exception", e);
+	    // AUDIT UPDATE
+	    auditManager.updateAudit(auditId, null, new Long(System.currentTimeMillis()), "EXECUTION_FAILED", e
+		    .getMessage(), null);
+	} finally {
+	    logger.debug("OUT");
 	}
-	
-	
-	private byte[] createErrorMsg(int code, String message) {
-		String response = "<response><error><code>"+code+"</code>" +
-				          "<message>"+message+"</message></error></response>";
-		return response.getBytes();
+    }
+
+    private String getLovType(String lovName) {
+	String toReturn = "";
+	try {
+	    IModalitiesValueDAO lovDAO = DAOFactory.getModalitiesValueDAO();
+	    ModalitiesValue lov = lovDAO.loadModalitiesValueByLabel(lovName);
+	    String type = lov.getITypeCd();
+	    toReturn = type;
+	} catch (Exception e) {
+	    logger.error("Error while recovering type of lov " + lovName);
 	}
-	
+	return toReturn;
+    }
+
+    private boolean isLovSupported(String lovName, HttpServletResponse response) {
+	boolean toReturn = true;
+	try {
+	    IModalitiesValueDAO lovDAO = DAOFactory.getModalitiesValueDAO();
+	    ModalitiesValue lov = lovDAO.loadModalitiesValueByLabel(lovName);
+	    String type = lov.getITypeCd();
+	    if (!type.equalsIgnoreCase("QUERY") && !type.equalsIgnoreCase("SCRIPT")
+		    && !type.equalsIgnoreCase("JAVA_CLASS")) {
+		SpagoBITracer.major(SpagoBIConstants.NAME_MODULE, this.getClass().getName(), "isLovSupported",
+			"Dashboard " + type + " lov Not yet Supported");
+		response.getOutputStream().write(createErrorMsg(12, "Dashboard  " + type + " lov not yet supported"));
+		response.getOutputStream().flush();
+		toReturn = false;
+	    }
+	} catch (Exception e) {
+	    toReturn = false;
+	    logger.error("Error while checkin if lov " + lovName + " is supported");
+	}
+	return toReturn;
+    }
+
+    private byte[] createErrorMsg(int code, String message) {
+	String response = "<response><error><code>" + code + "</code>" + "<message>" + message
+		+ "</message></error></response>";
+	return response.getBytes();
+    }
+
 }

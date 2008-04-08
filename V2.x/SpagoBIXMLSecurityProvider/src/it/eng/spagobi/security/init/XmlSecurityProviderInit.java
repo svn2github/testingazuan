0a26abb46d900c26718820e65927693ba0255f85
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-04-08 09:07:52 +0000

    Initial import.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@4468 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/SpagoBIXMLSecurityProvider/src/it/eng/spagobi/security/init/XmlSecurityProviderInit.java
@@ -0,0 +1,17 @@
+package it.eng.spagobi.security.init;
+
+import it.eng.spago.base.SourceBean;
+import it.eng.spago.init.InitializerIFace;
+
+public class XmlSecurityProviderInit implements InitializerIFace {
+	
+
+	public void init(SourceBean config) {
+	}
+
+	public SourceBean getConfig() {
+		return null;
+	}
+		
+
+}

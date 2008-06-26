e092446b1a94412c4f45dd246dcff9a2555e9510
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-06-26 22:27:44 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@5105 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ branch/Chiron/WebContent/js/spagobi/source/class/spagobi/source/class/spagobi/commons/CoreUtils.js
@@ -0,0 +1,40 @@
+/**
+ *
+ 
+ */
+
+
+qx.Class.define("spagobi.commons.CoreUtils", {
+  type : "static",
+  statics : {
+  	
+  	/**
+     *  Copies all the properties of config to obj.
+     *
+     * @param {Object} obj The receiver of the properties
+ 	 * @param {Object} config The source of the properties
+ 	 * @param {Object} defaults A different object that will also be applied for default values
+ 	 * @return {Object} returns obj
+     */
+    apply : function(o, c, defaults){
+	    if(defaults){
+	        // no "this" reference for friendly out of scope calls
+	        apply(o, defaults);
+	    }
+	    if(o && c && typeof c == 'object'){
+	        for(var p in c){
+	            o[p] = c[p];
+	        }
+	    }
+	    return o;
+  	},
+  	
+  	toStr : function(o) {
+  		var s = '';
+    	for(p in o) {
+    		s += p + ': ' + o[p] + ";\n";
+    	}
+    	return s;
+  	}
+  }
+});

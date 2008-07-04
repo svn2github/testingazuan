84fa45babee6560993eb0602556b0c2bc6e30388
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-07-04 08:41:44 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@5150 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/analiticalmodel/document/dao/ObjTemplateDAOHibImpl.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/analiticalmodel/document/dao/ObjTemplateDAOHibImpl.java
@@ -115,11 +115,11 @@ public class ObjTemplateDAOHibImpl extends AbstractHibernateDAO implements IObjT
 		try {
 			aSession = getSession();
 			tx = aSession.beginTransaction();
-			String hql = "from SbiObjTemplates sot where sot.sbiObject.biobjId="+biobjId;
+			String hql = "from SbiObjTemplates sot where sot.sbiObject.biobjId="+biobjId+" order by sot.prog desc";
 			Query query = aSession.createQuery(hql);
 			List result = query.list();
 			Iterator it = result.iterator();
-			while (it.hasNext()){
+ 			while (it.hasNext()){
 				templates.add(toObjTemplate((SbiObjTemplates)it.next()));
 			}
 			tx.commit();

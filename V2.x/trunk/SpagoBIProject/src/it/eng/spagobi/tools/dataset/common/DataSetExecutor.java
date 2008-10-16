173fb0d92aa8306eb0ea4cbce8d7b25ac6e739b7
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-10-16 14:52:01 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@5624 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/dataset/common/DataSetExecutor.java
@@ -0,0 +1,14 @@
+package it.eng.spagobi.tools.dataset.common;
+
+import it.eng.spago.security.IEngUserProfile;
+
+import java.util.HashMap;
+
+public class DataSetExecutor implements IDataSetExecutor {
+
+	public String execute(IEngUserProfile profile,String dataSetLabel, HashMap parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

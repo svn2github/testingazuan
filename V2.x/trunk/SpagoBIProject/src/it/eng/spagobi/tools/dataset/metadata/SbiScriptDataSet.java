d3700343bf21f79f7e8e854f1241904af1d5005d
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-10-20 15:21:22 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@5633 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/dataset/metadata/SbiScriptDataSet.java
@@ -0,0 +1,14 @@
+package it.eng.spagobi.tools.dataset.metadata;
+
+public class SbiScriptDataSet extends SbiDataSet {
+
+	 private String script =null;
+
+	public String getScript() {
+		return script;
+	}
+
+	public void setScript(String script) {
+		this.script = script;
+	}
+}

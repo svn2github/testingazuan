e6b84d03b7b9a0bc5f846ea0a5259d3a0ce96761
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-11-05 09:22:43 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@5782 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/kpi/dao/IKpiDAO.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/kpi/dao/IKpiDAO.java
@@ -1,6 +1,97 @@
 package it.eng.spagobi.kpi.dao;
 
+import it.eng.spago.error.EMFUserError;
+import it.eng.spagobi.kpi.bo.Kpi;
+import it.eng.spagobi.kpi.bo.KpiInstance;
+import it.eng.spagobi.kpi.bo.KpiValue;
+
+import java.util.Date;
+import java.util.List;
+
 public interface IKpiDAO {
+	
+	/**
+	 * Inserts a new KPI Value with its date, value period and thresholds
+	 * 
+	 * @param KpiValue to insert for the specific Kpi
+	 * @throws EMFUserError If an Exception occurred
+	 */	
+	public void insertKpiValue(KpiValue value, Kpi k) throws EMFUserError;
+
+	/**
+	 * Returns true if the value is not in the correct threshold, false if it's fine
+	 * 
+	 * @param KpiValue 
+	 * 
+	 * @return Returns true if the value is not in the correct threshold, false if it's fine
+	 * 
+	 * @throws EMFUserError if an Exception occurs
+	 */
+	public boolean isAlarmingValue(KpiValue value)	throws EMFUserError;
+	
+	/**
+	 * Returns true if the value is the actual one, false if it has to be recalculated
+	 * 
+	 * @param KpiValue 
+	 * 
+	 * @return Returns true if the value is the actual one, false if it has to be recalculated
+	 * 
+	 * @throws EMFUserError if an Exception occurs
+	 */
+	public  boolean isActualValue(KpiValue value)  throws EMFUserError;
+	
+	
+	/**
+	 * Deletes the KPI Value 
+	 * 
+	 * @param KpiValue to delete
+	 * @throws EMFUserError If an Exception occurred
+	 */	
+	public void deleteKpiValue(KpiValue value) throws EMFUserError;
+	
+	/**
+	 * Gets a Kpi Value of the past
+	 * 
+	 * @param Kpi , Date of when the KpiValue has to valid
+	 * 
+	 * @return Returns the correct KpiValue for that Date
+	 * 
+	 * @throws EMFUserError If an Exception occurred
+	 */	
+	public KpiValue getKpiValue(Kpi kpi, Date d) throws EMFUserError;
+	
+	/**
+	 * Lists all Kpi Values of a specific KPI
+	 * 
+	 * @param Kpi 
+	 * 
+	 * @return Returns a list of all the Kpi Value of the specific KPI
+	 * 
+	 * @throws EMFUserError If an Exception occurred
+	 */	
+	public KpiValue getKpiActualValue(Kpi kpi) throws EMFUserError;
+	
+	/**
+	 * Lists all Kpi Values of a specific KPIInstance
+	 * 
+	 * @param Kpi 
+	 * 
+	 * @return Returns a list of all the Kpi Value of the specific KPI Instance
+	 * 
+	 * @throws EMFUserError If an Exception occurred
+	 */	
+	public List listKpiValues(KpiInstance kpi) throws EMFUserError;
+	
+	/**
+	 * Gets a Kpi Instance of the specific KPI
+	 * 
+	 * @param Kpi 
+	 * 
+	 * @return Returns the Kpi Instance of the specific KPI
+	 * 
+	 * @throws EMFUserError If an Exception occurred
+	 */	
+	public KpiInstance getKpiInstance(Kpi kpi) throws EMFUserError;
 	
 	
 
--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/kpi/dao/KpiDAOImpl.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/kpi/dao/KpiDAOImpl.java
@@ -1,5 +1,53 @@
 package it.eng.spagobi.kpi.dao;
 
+import it.eng.spago.error.EMFUserError;
+import it.eng.spagobi.kpi.bo.Kpi;
+import it.eng.spagobi.kpi.bo.KpiInstance;
+import it.eng.spagobi.kpi.bo.KpiValue;
+
+import java.util.Date;
+import java.util.List;
+
 public class KpiDAOImpl implements IKpiDAO {
+
+	public void deleteKpiValue(KpiValue value) throws EMFUserError {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public KpiValue getKpiActualValue(Kpi kpi) throws EMFUserError {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public KpiInstance getKpiInstance(Kpi kpi) throws EMFUserError {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public KpiValue getKpiValue(Kpi kpi, Date d) throws EMFUserError {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void insertKpiValue(KpiValue value, Kpi k) throws EMFUserError {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isActualValue(KpiValue value) throws EMFUserError {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isAlarmingValue(KpiValue value) throws EMFUserError {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List listKpiValues(KpiInstance kpi) throws EMFUserError {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

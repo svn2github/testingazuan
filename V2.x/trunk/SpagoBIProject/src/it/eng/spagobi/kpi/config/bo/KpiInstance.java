a385d2e66c267fa4a99185bffb65599b453789eb
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-11-06 08:51:13 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@5800 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/kpi/config/bo/KpiInstance.java
@@ -0,0 +1,52 @@
+package it.eng.spagobi.kpi.config.bo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KpiInstance {
+	
+	Integer kpiId = null;
+	List kpiHistoryValues = null;//a List of all past values with relative thresholds	
+	KpiValue lastValue = null;//last calculated value with relative thresholds
+	Kpi kpi = null ;// type of the kpi instance
+	
+	
+	public KpiInstance() {
+		super();
+		this.kpiHistoryValues = new ArrayList();
+	}
+
+	public Integer getKpiId() {
+		return kpiId;
+	}
+
+	public void setKpiId(Integer kpiId) {
+		this.kpiId = kpiId;
+	}
+
+	public List getKpiHistoryValues() {
+		return kpiHistoryValues;
+	}
+
+	public void setKpiHistoryValues(List kpiHistoryValues) {
+		this.kpiHistoryValues = kpiHistoryValues;
+	}
+
+	public KpiValue getValue() {
+		return lastValue;
+	}
+
+	public void setValue(KpiValue value) {
+		this.lastValue = value;
+	}
+
+	public Kpi getKpi() {
+		return kpi;
+	}
+
+	public void setKpi(Kpi kpi) {
+		this.kpi = kpi;
+	}
+	
+
+}
---
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/kpi/config/bo/KpiValue.java
@@ -0,0 +1,107 @@
+package it.eng.spagobi.kpi.config.bo;
+
+import it.eng.spagobi.kpi.model.bo.Resource;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class KpiValue {
+	
+	Integer kpiInstanceId = null;
+	String value = null;		
+	List thresholds = null;
+	Double weight = null;
+	Date beginDate = null;
+	Date endDate = null;//null beginDate + num_validity_days
+	Integer num_validity_days = null;// number of days from begindate for wich the value is valid
+	String scaleCode = null;
+	String scaleName = null;
+	Resource r = null;//Resource (project/process) to which refers the value
+	
+	public KpiValue() {
+		super();
+		this.thresholds = new ArrayList();
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getScaleCode() {
+		return scaleCode;
+	}
+
+	public void setScaleCode(String scaleCode) {
+		this.scaleCode = scaleCode;
+	}
+
+	public String getScaleName() {
+		return scaleName;
+	}
+
+	public void setScaleName(String scaleName) {
+		this.scaleName = scaleName;
+	}
+
+	public List getThresholds() {
+		return thresholds;
+	}
+
+	public void setThresholds(List thresholds) {
+		this.thresholds = thresholds;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Date getBeginDate() {
+		return beginDate;
+	}
+
+	public void setBeginDate(Date beginDate) {
+		this.beginDate = beginDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Integer getNum_validity_days() {
+		return num_validity_days;
+	}
+
+	public void setNum_validity_days(Integer num_validity_days) {
+		this.num_validity_days = num_validity_days;
+	}
+
+	public Resource getR() {
+		return r;
+	}
+
+	public void setR(Resource r) {
+		this.r = r;
+	}
+
+	public Integer getKpiInstanceId() {
+		return kpiInstanceId;
+	}
+
+	public void setKpiInstanceId(Integer kpiInstanceId) {
+		this.kpiInstanceId = kpiInstanceId;
+	}
+
+}

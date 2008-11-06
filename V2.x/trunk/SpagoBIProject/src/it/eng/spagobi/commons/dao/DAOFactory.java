2eb69e55a846e5357bb01d79c0c20aad2e20800d
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-11-06 08:53:49 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@5810 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/commons/dao/DAOFactory.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/commons/dao/DAOFactory.java
@@ -52,7 +52,7 @@ import it.eng.spagobi.engines.dossier.dao.IDossierDAO;
 import it.eng.spagobi.engines.dossier.dao.IDossierPartsTempDAO;
 import it.eng.spagobi.engines.dossier.dao.IDossierPresentationsDAO;
 import it.eng.spagobi.hotlink.rememberme.dao.IRememberMeDAO;
-import it.eng.spagobi.kpi.dao.IKpiDAO;
+import it.eng.spagobi.kpi.config.dao.IKpiDAO;
 import it.eng.spagobi.mapcatalogue.bo.dao.ISbiGeoFeaturesDAO;
 import it.eng.spagobi.mapcatalogue.bo.dao.ISbiGeoMapFeaturesDAO;
 import it.eng.spagobi.mapcatalogue.bo.dao.ISbiGeoMapsDAO;

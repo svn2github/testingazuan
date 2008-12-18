42e45bdc82a99d70a82ca901a5056a274fb819f5
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2008-12-18 13:02:42 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@6206 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/commons/filters/ProfileFilter.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/commons/filters/ProfileFilter.java
@@ -18,7 +18,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-**/
+ **/
 package it.eng.spagobi.commons.filters;
 
 import it.eng.spago.base.Constants;
@@ -44,71 +44,78 @@ import org.apache.log4j.Logger;
 /**
  * @author Zerbetto (davide.zerbetto@eng.it)
  * 
- * This filter tries to build the user profile object, using the user identifier returned by 
+ * This filter tries to build the user profile object, using the user identifier
+ * returned by
  * <code>GeneralUtilities.findUserId((HttpServletRequest) request);</code>
  */
 
 public class ProfileFilter implements Filter {
 
-	private static transient Logger logger = Logger.getLogger(ProfileFilter.class);
-	
-	public void destroy() {
-		// do nothing
-	}
+    private static transient Logger logger = Logger.getLogger(ProfileFilter.class);
 
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
-		logger.debug("IN");
-		try {
-			if (request instanceof HttpServletRequest) {
-				HttpServletRequest httpRequest = (HttpServletRequest) request;
-				HttpSession session = httpRequest.getSession();
-				String userId = GeneralUtilities.findUserId(httpRequest);
-				logger.debug("User id = " + userId);
-				// in case the user is not specified, does nothing 
-				if (userId == null || userId.trim().equals("")) {
-					logger.warn("User identifier not found.");
-				} else {
-					// looking if a RequestContainer already exists in session
-					RequestContainer requestContainer = (RequestContainer) session.getAttribute(Constants.REQUEST_CONTAINER);
-					if (requestContainer == null) {
-						// RequestContainer does not exists yet (maybe it is the first call to Spago)
-						// initializing Spago objects (user profile object must be put into PermanentContainer)
-			            requestContainer = new RequestContainer();
-			            SessionContainer sessionContainer = new SessionContainer(true);
-			            requestContainer.setSessionContainer(sessionContainer);
-			            session.setAttribute(Constants.REQUEST_CONTAINER, requestContainer);
-					}
-					SessionContainer sessionContainer = requestContainer.getSessionContainer();
-					SessionContainer permanentSession = sessionContainer.getPermanentContainer();
-					IEngUserProfile profile = (IEngUserProfile) permanentSession.getAttribute(IEngUserProfile.ENG_USER_PROFILE);
-					if (profile == null) {
-						logger.debug("User profile not found in session, creating a new one and putting in session....");
-						// in case the profile does not exist, creates a new one
-						profile = GeneralUtilities.createNewUserProfile(userId);
-						permanentSession.setAttribute(IEngUserProfile.ENG_USER_PROFILE, profile);
-					} else {
-						// in case the profile is different, creates a new one and overwrites the existing
-						if (!((UserProfile)profile).getUserUniqueIdentifier().toString().equals(userId)) {
-							logger.debug("Different user profile found in session, creating a new one and replacing in session....");
-							profile = GeneralUtilities.createNewUserProfile(userId);
-							permanentSession.setAttribute(IEngUserProfile.ENG_USER_PROFILE, profile);
-						} else {
-							logger.debug("User profile object for user [" + userId + "] already existing in session, ok");
-						}
-					}
-				}
+    public void destroy() {
+	// do nothing
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+	    ServletException {
+	logger.debug("IN");
+	try {
+	    if (request instanceof HttpServletRequest) {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession session = httpRequest.getSession();
+		String userId = GeneralUtilities.findUserId(httpRequest);
+		logger.debug("User id = " + userId);
+		// in case the user is not specified, does nothing
+		if (userId == null || userId.trim().equals("")) {
+		    logger.warn("User identifier not found.");
+		} else {
+		    // looking if a RequestContainer already exists in session
+		    RequestContainer requestContainer = (RequestContainer) session
+			    .getAttribute(Constants.REQUEST_CONTAINER);
+		    if (requestContainer == null) {
+			// RequestContainer does not exists yet (maybe it is the
+			// first call to Spago)
+			// initializing Spago objects (user profile object must
+			// be put into PermanentContainer)
+			requestContainer = new RequestContainer();
+			SessionContainer sessionContainer = new SessionContainer(true);
+			requestContainer.setSessionContainer(sessionContainer);
+			session.setAttribute(Constants.REQUEST_CONTAINER, requestContainer);
+		    }
+		    SessionContainer sessionContainer = requestContainer.getSessionContainer();
+		    SessionContainer permanentSession = sessionContainer.getPermanentContainer();
+		    IEngUserProfile profile = (IEngUserProfile) permanentSession
+			    .getAttribute(IEngUserProfile.ENG_USER_PROFILE);
+		    if (profile == null) {
+			logger.debug("User profile not found in session, creating a new one and putting in session....");
+			// in case the profile does not exist, creates a new one
+			profile = GeneralUtilities.createNewUserProfile(userId);
+			permanentSession.setAttribute(IEngUserProfile.ENG_USER_PROFILE, profile);
+		    } else {
+			// in case the profile is different, creates a new one
+			// and overwrites the existing
+			if (!((UserProfile) profile).getUserUniqueIdentifier().toString().equals(userId)) {
+			    logger.debug("Different user profile found in session, creating a new one and replacing in session....");
+			    profile = GeneralUtilities.createNewUserProfile(userId);
+			    permanentSession.setAttribute(IEngUserProfile.ENG_USER_PROFILE, profile);
+			} else {
+			    logger.debug("User profile object for user [" + userId
+				    + "] already existing in session, ok");
 			}
-		} catch (Exception e) {
-			logger.error(e);
-		} finally {
-			logger.debug("OUT");
-			chain.doFilter(request, response);
+		    }
 		}
+	    }
+	} catch (Exception e) {
+	    logger.error(e);
+	} finally {
+	    logger.debug("OUT");
+	    chain.doFilter(request, response);
 	}
+    }
 
-	public void init(FilterConfig config) throws ServletException {
-		// do nothing
-	}
+    public void init(FilterConfig config) throws ServletException {
+	// do nothing
+    }
 
 }

8e8946a90350124f963d2b573aac99812437ef79
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-01-09 11:02:04 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@6363 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/dataset/common/transformer/PivotDataSetTransformer.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/dataset/common/transformer/PivotDataSetTransformer.java
@@ -18,9 +18,8 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-**/
+ **/
 package it.eng.spagobi.tools.dataset.common.transformer;
-
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -29,35 +28,60 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import it.eng.spago.base.SourceBeanAttribute;
+import it.eng.spagobi.tools.dataset.common.datastore.DataStore;
 import it.eng.spagobi.tools.dataset.common.datastore.Field;
 import it.eng.spagobi.tools.dataset.common.datastore.FieldMetadata;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStore;
 import it.eng.spagobi.tools.dataset.common.datastore.IDataStoreMetaData;
 import it.eng.spagobi.tools.dataset.common.datastore.IField;
+import it.eng.spagobi.tools.dataset.common.datastore.IFieldMetaData;
 import it.eng.spagobi.tools.dataset.common.datastore.IRecord;
 import it.eng.spagobi.tools.dataset.common.datastore.Record;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
- *
+ * 
  */
 public class PivotDataSetTransformer extends AbstractDataStoreTransformer {
-	
+
 	String pivotFieldName; // pivotColumn
 	String valueFieldName; // pivotValue
 	String groupFieldName; // pivotRow
 	
-	private static transient Logger logger = Logger.getLogger(PivotDataSetTransformer.class);
-    
+	List pivotedFieldNames;
 
-	public PivotDataSetTransformer(String pivotFieldName, String valueFieldName, String groupFieldName) {
+	private static transient Logger logger = Logger
+			.getLogger(PivotDataSetTransformer.class);
+
+	public PivotDataSetTransformer(String pivotFieldName,
+			String valueFieldName, String groupFieldName) {
 		this.setPivotFieldName(pivotFieldName);
 		this.setValueFieldName(valueFieldName);
 		this.setGroupFieldName(groupFieldName);
+		pivotedFieldNames = new ArrayList();
 	}
 
-	void transformDataSetMetaData(IDataStore dataStore) {		
+	void transformDataSetMetaData(IDataStore dataStore) {
+		IDataStoreMetaData dataStoreMeta;
+		int pivotFieldIndex;
+		int valueFieldIndex;
 		
+		dataStoreMeta = dataStore.getMetaData();
+		
+		pivotFieldIndex = dataStoreMeta.getFieldIndex(getPivotFieldName());
+		valueFieldIndex = dataStoreMeta.getFieldIndex(getValueFieldName());
+		
+		Class pivotedFieldType = dataStoreMeta.getFieldMeta( valueFieldIndex ).getType();
+		
+		dataStoreMeta = dataStore.getMetaData();
+		dataStoreMeta.deleteFieldMetaDataAt( pivotFieldIndex );
+		dataStoreMeta.deleteFieldMetaDataAt( valueFieldIndex );
+		
+		for(int i = 0; i < pivotedFieldNames.size(); i++) {
+			IFieldMetaData fieldMeta;			
+			fieldMeta = new FieldMetadata((String)pivotedFieldNames.get(i), pivotedFieldType);			
+			dataStoreMeta.addFiedMeta( fieldMeta );
+		}
 	}
 
 	void transformDataSetRecords(IDataStore dataStore) {
@@ -65,78 +89,54 @@ public class PivotDataSetTransformer extends AbstractDataStoreTransformer {
 		int pivotFieldIndex;
 		int valueFieldIndex;
 		int groupFieldIndex;
-		List pivotedFieldNames = new ArrayList();;
-		
+
 		List newRecords = new ArrayList();
-	    IRecord newRecord = null;
-		String rowValue = "";
-		
-			
+		IRecord newRecord = null;
+		Object selectedGroupValue = null;
+
 		dataStoreMeta = dataStore.getMetaData();
-		pivotFieldIndex = dataStoreMeta.getFieldIndex( getPivotFieldName() );
-		valueFieldIndex = dataStoreMeta.getFieldIndex( getValueFieldName() );
-		groupFieldIndex = dataStoreMeta.getFieldIndex( getGroupFieldName() );
-		
-		Iterator it = dataStore.getFieldDistinctValues( pivotFieldIndex ).iterator();
-		while(it.hasNext()) pivotedFieldNames.add( it.next() );
-					
-		
-		it = dataStore.iterator();
-		while(it.hasNext()) {
-			IRecord record = (IRecord)it.next();
-			
+		pivotFieldIndex = dataStoreMeta.getFieldIndex(getPivotFieldName());
+		valueFieldIndex = dataStoreMeta.getFieldIndex(getValueFieldName());
+		groupFieldIndex = dataStoreMeta.getFieldIndex(getGroupFieldName());
+
+		Iterator pivotedFieldNamesIterator = dataStore.getFieldDistinctValues(pivotFieldIndex).iterator();		
+		while ( pivotedFieldNamesIterator.hasNext() ) {
+			pivotedFieldNames.add( pivotedFieldNamesIterator.next() );
+		}
+
+		Iterator it = dataStore.iterator();
+		while (it.hasNext()) {
+			IRecord record = (IRecord) it.next();
+
 			IField pivotField = record.getFieldAt(pivotFieldIndex);
 			IField valueField = record.getFieldAt(valueFieldIndex);
 			IField groupField = record.getFieldAt(groupFieldIndex);
-			
-			for(int i  = 0; i < pivotedFieldNames.size(); i++) {
-				if( pivotField.getValue().equals( pivotedFieldNames.get(i)) ) {
-					newRecord.appendField( new Field( valueField.getValue() ) );
-				} else {
-					newRecord.appendField( new Field(new Double(0)) );
-				}						
-			}	
-			
-			
-			
-			
-			List fields = record.getFields();
-			for(int j = 0; j < fields.size(); j++) {		
-				IField field = (IField)fields.get(j);
-				String fieldName = dataStoreMeta.getFieldName(j);
-				String fieldValue = "" + field.getValue();
+
+			if(selectedGroupValue == null || !selectedGroupValue.equals( groupField.getValue() )) {
+				selectedGroupValue = groupField.getValue();
+				if(newRecord != null) {
+					newRecord.getFields().remove(pivotFieldIndex);
+					newRecord.getFields().remove(valueFieldIndex);
+					newRecords.add( newRecord );
+				}
+				newRecord = record;
+								
+				for (int i = 0; i < pivotedFieldNames.size(); i++) {					
+					newRecord.appendField(new Field(null));
+				}
 				
-				if(j == pivotFieldIndex) {
-					//pivot column
-					
-									
-				} else if (j == valueFieldIndex) {
-					//pivotValue
-					//skip field
-					
-				} else if (j == groupFieldIndex) {
-					//pivotRow
-					if (rowValue.equals("")) rowValue = fieldValue;
-					if (!(rowValue.trim()).equalsIgnoreCase(fieldValue.trim())){
-						rowValue = fieldValue;
-						newRecords.add(newRecord);
-						newRecord = new Record();
-					}
-					if (newRecord.getFieldAt(dataStoreMeta.getFieldIndex(fieldName) ) == null)
-						newRecord.appendField(field);
-					
-				} else {
-					
-					//if the field isn't into record comes added
-					if (newRecord.getFieldAt(dataStoreMeta.getFieldIndex(fieldName)) == null)
-						newRecord.appendField(field);
-				}				
-			}	//for	
-		} //while
-    	//Adds the last record
-		//if (newRecord != null) newRecords.add(newRecord);
+				
+			}
+			
+			int pivotedFieldIndex = dataStoreMeta.getFieldCount() + pivotedFieldNames.indexOf( pivotField.getValue() );
+			newRecord.getFieldAt( pivotedFieldIndex ).setValue( valueField.getValue() );
+		}
 		
+		newRecord.getFields().remove(pivotFieldIndex);
+		newRecord.getFields().remove(valueFieldIndex);
+		newRecords.add( newRecord );
 		
+		((DataStore) dataStore).setRecords(newRecords);
 	}
 
 	public String getPivotFieldName() {
@@ -162,6 +162,93 @@ public class PivotDataSetTransformer extends AbstractDataStoreTransformer {
 	public void setGroupFieldName(String groupFieldName) {
 		this.groupFieldName = groupFieldName;
 	}
-
 	
+	
+	/**
+	 X                      SER           VAL         IDX   ....
+Gennaio            SER1           33            1
+Gennaio            SER2           12            1
+Gennaio            SER3           64            1
+Gennaio            SER4           21            1
+Febbraio           SER1           56            2
+Febbraio           SER2           35            2
+Febbraio           SER3           13            2
+Febbraio           SER4           75            2
+	 */
+	public static void main(String[] args) {
+		IDataStore dataStore;
+		IRecord record;
+		
+		dataStore = new DataStore();
+		
+		dataStore.getMetaData().addFiedMeta(new FieldMetadata("X", String.class ));
+		dataStore.getMetaData().addFiedMeta(new FieldMetadata("SER", String.class));
+		dataStore.getMetaData().addFiedMeta(new FieldMetadata("VAL", Double.class));
+		dataStore.getMetaData().addFiedMeta(new FieldMetadata("IDX", Integer.class));
+		
+		// ----------------------------------------------------
+		
+		record = new Record();
+		record.appendField(new Field("Gennaio"));
+		record.appendField(new Field("SER1"));
+		record.appendField(new Field(new Double(33)));
+		record.appendField(new Field(new Integer(1)));
+		dataStore.appendRecord(record);
+		
+		record = new Record();
+		record.appendField(new Field("Gennaio"));
+		record.appendField(new Field("SER2"));
+		record.appendField(new Field(new Field(new Double(12))));
+		record.appendField(new Field(new Integer(1)));
+		dataStore.appendRecord(record);
+		
+		record = new Record();
+		record.appendField(new Field("Gennaio"));
+		record.appendField(new Field("SER3"));
+		record.appendField(new Field(new Field(new Double(64))));
+		record.appendField(new Field(new Integer(1)));
+		dataStore.appendRecord(record);
+		
+		record = new Record();
+		record.appendField(new Field("Gennaio"));
+		record.appendField(new Field("SER4"));
+		record.appendField(new Field(new Field(new Double(21))));
+		record.appendField(new Field(new Integer(1)));
+		dataStore.appendRecord(record);
+		
+		// ----------------------------------------------------
+		
+		record = new Record();
+		record.appendField(new Field("Febbraio"));
+		record.appendField(new Field("SER1"));
+		record.appendField(new Field("56"));
+		record.appendField(new Field(new Integer(2)));
+		dataStore.appendRecord(record);
+		
+		record = new Record();
+		record.appendField(new Field("Febbraio"));
+		record.appendField(new Field("SER2"));
+		record.appendField(new Field(new Field(new Double(35))));
+		record.appendField(new Field(new Integer(2)));
+		dataStore.appendRecord(record);
+		
+		record = new Record();
+		record.appendField(new Field("Febbraio"));
+		record.appendField(new Field("SER3"));
+		record.appendField(new Field(new Field(new Double(13))));
+		record.appendField(new Field(new Integer(2)));
+		dataStore.appendRecord(record);
+		
+		record = new Record();
+		record.appendField(new Field("Febbraio"));
+		record.appendField(new Field("SER4"));
+		record.appendField(new Field(new Field(new Double(75))));
+		record.appendField(new Field(new Integer(2)));
+		dataStore.appendRecord(record);
+		
+		IDataStoreTransformer transformer = new PivotDataSetTransformer("SER", "VAL", "X");
+		transformer.transform(dataStore);
+		System.out.println(dataStore.toXml());
+	}
+
 }

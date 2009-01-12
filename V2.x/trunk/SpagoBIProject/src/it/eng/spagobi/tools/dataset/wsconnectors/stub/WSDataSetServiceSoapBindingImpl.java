04c2dfc496d6b5f8dff5908ba39155d75a92af3a
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-01-12 09:52:50 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@6384 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/dataset/wsconnectors/stub/WSDataSetServiceSoapBindingImpl.java
@@ -0,0 +1,15 @@
+/**
+ * WSDataSetServiceSoapBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.3 Oct 05, 2005 (05:23:37 EDT) WSDL2Java emitter.
+ */
+
+package it.eng.spagobi.tools.dataset.wsconnectors.stub;
+
+public class WSDataSetServiceSoapBindingImpl implements it.eng.spagobi.tools.dataset.wsconnectors.stub.IWsConnector{
+    public java.lang.String readDataSet(java.lang.String in0, java.util.HashMap in1, java.lang.String in2) throws java.rmi.RemoteException {
+        return "da definire un xml di test";
+    }
+
+}

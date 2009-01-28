96a8853151811eb58801121acdc1b869b01155e0
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-01-28 15:56:22 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@6683 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/tag/SpagoBI-2.0.0(20092701)/SpagoBIJPaloEngine/src/com/tensegrity/palowebviewer/modules/engine/client/IElementPathCallback.java
+++
@@ -1,9 +0,0 @@
-package com.tensegrity.palowebviewer.modules.engine.client;
-
-import com.tensegrity.palowebviewer.modules.paloclient.client.XElement;
-
-public interface IElementPathCallback {
-	
-	public void onSuccess(XElement[] path);
-
-}
--- V2.x/tag/SpagoBI-2.0.0(20092701)/SpagoBIJPaloEngine/src/com/tensegrity/palowebviewer/modules/engine/client/usermessage/IUserMessageQueueListener.java
+++
@@ -1,9 +0,0 @@
-package com.tensegrity.palowebviewer.modules.engine.client.usermessage;
-
-public interface IUserMessageQueueListener {
-	
-	public void onMessagePush(IUserMessage msg);
-	
-	public void onMessagePop(IUserMessage msg);
-	
-}

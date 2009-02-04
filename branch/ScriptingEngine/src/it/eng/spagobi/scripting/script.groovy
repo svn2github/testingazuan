79e5c5bd46c757bc99b745fb24727e641c4d4bc3
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-02-04 16:53:29 +0000

    Test project. To be deleted once the code produced will be integrated into the core
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@6733 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ branch/ScriptingEngine/src/it/eng/spagobi/scripting/script.groovy
@@ -0,0 +1,7 @@
+import it.eng.spagobi.scripting.ITestInterface;
+
+public class Tester implements ITestInterface {
+    public String getMessage() {
+       return "Can you fill the groove?";
+    }
+}

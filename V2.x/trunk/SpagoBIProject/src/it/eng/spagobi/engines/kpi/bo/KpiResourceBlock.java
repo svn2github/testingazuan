c4251b24abf420227eff9ce828afe6b7fe9cbe66
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-03-10 08:49:01 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@7140 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/kpi/bo/KpiResourceBlock.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/kpi/bo/KpiResourceBlock.java
@@ -176,7 +176,7 @@ public class KpiResourceBlock {
 			_htmlStream.append("	<tr class='kpi_line_section_odd' style='border-bottom: 1px solid #DDDDDD !important;' id='"+id+"' onclick=\"toggleHideChild(this,'"+tab_name+"');\" >\n");
 		}
 		if (display_semaphore && semaphorColor!= null){
-			String semaphorHex = Integer.toHexString( semaphorColor.getRGB() & 0x00ffffff ) ;		
+			String semaphorHex ="rgb("+semaphorColor.getRed()+", "+semaphorColor.getGreen()+", "+semaphorColor.getBlue()+")" ;	
 			_htmlStream.append("		<td width='53%' class='kpi_td' ><div style=\"margin-left: "+20*recursionLev+"px;margin-top:5px;margin-right:5px;float:left;width:9px;height:9px;border: 1px solid #5B6B7C;background-color:#"+semaphorHex+"\"></div><div  class='kpi_div'>"+modelName+"</div></td>\n");
 		}else{
 			_htmlStream.append("		<td width='53%'  class='kpi_td' ><div class='kpi_div'><div style='MARGIN-LEFT: "+20*recursionLev+"px;text-align:left;' class='kpi_div'>"+modelName+"</div></div></td>\n");

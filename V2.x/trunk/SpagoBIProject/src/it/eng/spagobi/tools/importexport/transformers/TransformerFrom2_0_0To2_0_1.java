36bdd624f32dec10d31cbc91b3c2a010e713a33a
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-03-12 16:45:51 +0000

    Fix hibernate dialect domains (from 'HSQL' to 'org.hibernate.dialect.HSQLDialect', from 'ORACLE' to ...)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@7195 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/importexport/transformers/TransformerFrom2_0_0To2_0_1.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/importexport/transformers/TransformerFrom2_0_0To2_0_1.java
@@ -26,6 +26,7 @@ import it.eng.spagobi.tools.importexport.ITransformer;
 
 import java.io.File;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.apache.log4j.Logger;
@@ -59,28 +60,74 @@ public class TransformerFrom2_0_0To2_0_1 implements ITransformer {
 	private void changeDatabase(String pathImpTmpFolder, String archiveName) {
 		logger.debug("IN");
 		Connection conn = null;
-		try{
+		try {
 			conn = TransformersUtilities.getConnectionToDatabase(pathImpTmpFolder, archiveName);
-			String sql = "";
-			Statement stmt = conn.createStatement();
-			// avoid HQL --> HSQL hibernate dialect problem
-			sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'HSQL', VALUE_NM = 'HSQL' WHERE VALUE_CD = 'HQL' AND DOMAIN_CD = 'DIALECT_HIB'";
-			stmt.execute(sql);
-			sql =  "ALTER TABLE SBI_DATA_SET ADD COLUMN NUM_ROWS BOOLEAN DEFAULT FALSE";
-			stmt.execute(sql);
-			sql =  "UPDATE SBI_DATA_SET SET NUM_ROWS=FALSE";
-			stmt.executeUpdate(sql);
-			sql =  "ALTER TABLE SBI_DATA_SET ADD COLUMN LANGUAGE_SCRIPT VARCHAR(50) DEFAULT NULL";
-			stmt.execute(sql);
-			sql =  "UPDATE SBI_DATA_SET SET LANGUAGE_SCRIPT = 'groovy' where OBJECT_TYPE = 'SbiScriptDataSet'";
-			stmt.executeUpdate(sql);
+			fixDomains(conn);
+			fixDatasets(conn);
 			conn.commit();
-			conn.close();
 		} catch (Exception e) {
 			logger.error("Error while changing database", e);	
 		} finally {
 			logger.debug("OUT");
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				logger.error("Error closing connection to export database", e);
+			}
 		}
+	}
+	
+	/*
+	 * Adjust domains: in some SpagoBI 2.0.0 installations there were a hibernate dialect with name 'HQL' instead of 'HSQL'.
+	 * Furthermore, hibernate dialects' value code was 'HSQL', 'MySQL', 'ORACLE' ... instead of the actual hibernate dialect java class
+	 * 
+	 * @param conn The jdbc connection to export database
+	 * @throws Exception
+	 */
+	private void fixDomains(Connection conn) throws Exception {
+		logger.debug("IN");
+		Statement stmt = conn.createStatement();
+		String sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'HSQL', VALUE_NM = 'HSQL' WHERE VALUE_CD = 'HQL' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = '-1' WHERE VALUE_CD = 'DEFAULT' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'org.hibernate.dialect.OracleDialect' WHERE VALUE_CD = 'ORACLE' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'org.hibernate.dialect.Oracle9Dialect' WHERE VALUE_CD = 'ORACLE 9i/10g' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'org.hibernate.dialect.SQLServerDialect' WHERE VALUE_CD = 'SQLSERVER' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'org.hibernate.dialect.HSQLDialect' WHERE VALUE_CD = 'HSQL' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'org.hibernate.dialect.MySQLInnoDBDialect' WHERE VALUE_CD = 'MYSQL' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'org.hibernate.dialect.PostgreSQLDialect' WHERE VALUE_CD = 'POSTGRESQL' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		sql =  "UPDATE SBI_DOMAINS SET VALUE_CD = 'org.hibernate.dialect.IngresDialect' WHERE VALUE_CD = 'INGRES' AND DOMAIN_CD = 'DIALECT_HIB'";
+		stmt.executeUpdate(sql);
+		logger.debug("OUT");
+	}
+	
+	/*
+	 * Add columns NUM_ROWS and LANGUAGE_SCRIPT to export database.
+	 * 
+	 * @param conn The jdbc connection to export database
+	 * @throws Exception
+	 */
+	private void fixDatasets(Connection conn) throws Exception {
+		logger.debug("IN");
+		Statement stmt = conn.createStatement();
+		String sql =  "ALTER TABLE SBI_DATA_SET ADD COLUMN NUM_ROWS BOOLEAN DEFAULT FALSE";
+		stmt.execute(sql);
+		sql =  "UPDATE SBI_DATA_SET SET NUM_ROWS=FALSE";
+		stmt.executeUpdate(sql);
+		sql =  "ALTER TABLE SBI_DATA_SET ADD COLUMN LANGUAGE_SCRIPT VARCHAR(50) DEFAULT NULL";
+		stmt.execute(sql);
+		sql =  "UPDATE SBI_DATA_SET SET LANGUAGE_SCRIPT = 'groovy' where OBJECT_TYPE = 'SbiScriptDataSet'";
+		stmt.executeUpdate(sql);
+		logger.debug("OUT");
 	}
 
 }

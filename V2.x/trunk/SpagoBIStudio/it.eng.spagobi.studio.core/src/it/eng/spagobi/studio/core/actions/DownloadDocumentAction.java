cc484fce000ad5a468dd175822a4a50fbe2960a9
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-05-07 13:27:40 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@7585 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/actions/DownloadDocumentAction.java
+++ V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/actions/DownloadDocumentAction.java
@@ -11,19 +11,16 @@ public class DownloadDocumentAction implements IViewActionDelegate {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
 	public void init(IViewPart view) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void run(IAction action) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
 		// TODO Auto-generated method stub
 		

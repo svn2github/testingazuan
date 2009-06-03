dc8a833bb333cb2674cd85da95ad437cd7278bc7
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-06-03 13:29:19 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@7744 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/mapcatalogue/bo/dao/hibernate/AbstractHibernateDAO.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/mapcatalogue/bo/dao/hibernate/AbstractHibernateDAO.java
@@ -21,10 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.mapcatalogue.bo.dao.hibernate;
 
-import it.eng.spagobi.commons.constants.SpagoBIConstants;
 import it.eng.spagobi.commons.utilities.HibernateUtil;
-import it.eng.spagobi.commons.utilities.SpagoBITracer;
-import it.eng.spagobi.security.ExoUserProfileImpl;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Session;

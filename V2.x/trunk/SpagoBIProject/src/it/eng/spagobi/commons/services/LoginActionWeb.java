9122624d5ba8f436c6979203d0304a5ea08d67e6
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-06-17 21:34:34 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@8007 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/commons/services/LoginActionWeb.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/commons/services/LoginActionWeb.java
@@ -1,143 +1,172 @@
 package it.eng.spagobi.commons.services;
 
-import it.eng.spago.base.Constants;
-import it.eng.spago.base.RequestContainer;
-import it.eng.spago.base.SessionContainer;
-import it.eng.spago.base.SourceBean;
-import it.eng.spago.configuration.ConfigSingleton;
-import it.eng.spago.dispatching.action.AbstractHttpAction;
-import it.eng.spago.error.EMFErrorSeverity;
-import it.eng.spago.error.EMFUserError;
-import it.eng.spago.security.IEngUserProfile;
-import it.eng.spagobi.commons.bo.UserProfile;
-import it.eng.spagobi.commons.constants.SpagoBIConstants;
-import it.eng.spagobi.commons.utilities.UserUtilities;
-import it.eng.spagobi.commons.utilities.messages.MessageBuilder;
-import it.eng.spagobi.services.security.exceptions.SecurityException;
-import it.eng.spagobi.services.security.service.ISecurityServiceSupplier;
-import it.eng.spagobi.services.security.service.SecurityServiceSupplierFactory;
-import it.eng.spagobi.wapp.util.MenuUtilities;
-
-import java.io.OutputStream;
+import java.io.IOException;
 import java.util.Locale;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
-public class LoginActionWeb extends AbstractHttpAction {
+import it.eng.spago.base.Constants;
+import it.eng.spago.base.SourceBean;
+import it.eng.spago.configuration.ConfigSingleton;
+import it.eng.spago.security.IEngUserProfile;
+import it.eng.spagobi.analiticalmodel.document.x.SpagoBIServiceExceptionHandler;
+import it.eng.spagobi.commons.bo.UserProfile;
+import it.eng.spagobi.commons.constants.SpagoBIConstants;
+import it.eng.spagobi.commons.utilities.StringUtilities;
+import it.eng.spagobi.commons.utilities.UserUtilities;
+import it.eng.spagobi.commons.utilities.messages.MessageBuilder;
+import it.eng.spagobi.services.security.service.ISecurityServiceSupplier;
+import it.eng.spagobi.services.security.service.SecurityServiceSupplierFactory;
+import it.eng.spagobi.utilities.assertion.Assert;
+import it.eng.spagobi.utilities.exceptions.SpagoBIServiceException;
+import it.eng.spagobi.utilities.service.AbstractBaseHttpAction;
+import it.eng.spagobi.utilities.service.JSONSuccess;
+import it.eng.spagobi.wapp.services.ChangeTheme;
+import it.eng.spagobi.wapp.util.MenuUtilities;
 
+public class LoginActionWeb extends AbstractBaseHttpAction {
+	
+	public static final String SERVICE_NAME = "LOGIN_SERVICE";
+	
+	// configuration parameter
+	public static String SSO_ACTIVE = "SPAGOBI_SSO.ACTIVE";
+	
+	// request parameters
+	public static String CALLBACK = "callback";
+	
+	public static String USER_ID = "user";
+	public static String PASSWORD = "password";
+	
+	public static String THEME = ChangeTheme.THEME_NAME;	
+	public static String BACK_URL = SpagoBIConstants.BACK_URL;
+	
+	// logger component
 	static Logger logger = Logger.getLogger(LoginActionWeb.class);
+	
 	IEngUserProfile profile = null;
 
 	
 	
-	public void service(SourceBean request, SourceBean response) throws Exception {
+	public void service(SourceBean request, SourceBean response) throws SpagoBIServiceException {
+		String callback = null;
+		String theme;
+		String backUrl;
+		SourceBean settingSB;
+		boolean isSSOActive;
+		String usr = null;
+		String pwd;
+		
+		
 		logger.debug("IN");
 
-		ServletOutputStream os=getHttpResponse().getOutputStream();
-		freezeHttpResponse();
-
-		ConfigSingleton serverConfig = ConfigSingleton.getInstance();
-		SourceBean validateSB = (SourceBean) serverConfig.getAttribute("SPAGOBI_SSO.ACTIVE");
-		String activeStr = (String) validateSB.getCharacters();
-		boolean activeSoo=false;
-		if (activeStr != null && activeStr.equalsIgnoreCase("true")) {
-			activeSoo=true;
-		}
-		RequestContainer reqCont = RequestContainer.getRequestContainer();
-		SessionContainer sessCont = reqCont.getSessionContainer();
-		SessionContainer permSess = sessCont.getPermanentContainer();
-
-		HttpServletRequest servletRequest=getHttpRequest();
-		HttpSession httpSession=servletRequest.getSession();
-
-		if (request.getAttribute("MESSAGE") != null && ((String)request.getAttribute("MESSAGE")).equalsIgnoreCase("START_LOGIN")){
-			response.setAttribute(SpagoBIConstants.PUBLISHER_NAME, "login");
-			logger.debug("OUT");
-			return;
-		}
-
-		String userId=null;
-		if (!activeSoo){
-			userId = (String)request.getAttribute("userID");
-			logger.debug("userID="+userId);
-			if (userId == null) {
-				logger.error("User identifier not found. Cannot build user profile object");
-				throw new SecurityException("User identifier not found.");
-			}			
-		}else{
-			userId=UserUtilities.getUserId(this.getHttpRequest());
-		}
-
-		ISecurityServiceSupplier supplier=SecurityServiceSupplierFactory.createISecurityServiceSupplier();
-		// If SSO is not active, check username and password, i.e. performs the authentication;
-		// instead, if SSO is active, the authentication mechanism is provided by the SSO itself, so SpagoBI does not make 
-		// any authentication, just creates the user profile object and puts it into Spago permanent container
-		if (!activeSoo) {
-			String pwd=(String)request.getAttribute("password");       
-			try {
-				Object ris=supplier.checkAuthentication(userId, pwd);
-				if (ris==null){
-					logger.error("pwd uncorrect");
-					os.print("KO");
-					os.flush();
-					return;
-				}
-			} catch (Exception e) {
-				logger.error("Reading user information... ERROR");
-				throw new SecurityException("Reading user information... ERROR",e);
-			}
-		}
-
-		try {
-			profile=UserUtilities.getUserProfile(userId);
-			if (profile == null){		            	
-				logger.error("user not created");
-				EMFUserError emfu = new EMFUserError(EMFErrorSeverity.ERROR, 501);
-				return;
-			}
-
-			// put user profile into session
-			permSess.setAttribute(IEngUserProfile.ENG_USER_PROFILE, profile);
-			// updates locale information on permanent container for Spago messages mechanism
-			Locale locale = MessageBuilder.getBrowserLocaleFromSpago();
-			if (locale != null) {
-				permSess.setAttribute(Constants.USER_LANGUAGE, locale.getLanguage());
-				permSess.setAttribute(Constants.USER_COUNTRY, locale.getCountry());
-			}
-		} catch (Exception e) {
-			logger.error("Reading user information... ERROR");
-			throw new SecurityException("Reading user information... ERROR",e);
-		}
-
-		//String username = (String) profile.getUserUniqueIdentifier();
-		String username = (String) ((UserProfile)profile).getUserId();
-		if (!UserUtilities.userFunctionalityRootExists(username)) {
-			logger.debug("funcitonality root not yet exists for "+username);	
-			//UserUtilities.createUserFunctionalityRoot(profile);
-		}
-		else{
-			logger.debug("funcitonality root already exists for "+username);					
-		}
-
-		MenuUtilities.getMenuItems(request, response, profile);
-		// fill response attributes
-		if(userId.equals("chiron")) {
-			response.setAttribute(SpagoBIConstants.PUBLISHER_NAME, "chiron");
-		} else {
-			response.setAttribute(SpagoBIConstants.PUBLISHER_NAME, "home");
-		}
-
+		try {		
+			setSpagoBIRequestContainer( request );
+			setSpagoBIResponseContainer( response );
 		
-		//login succesfull
-		os.print("OK");
-		os.flush();
-		logger.debug("OUT");		
+			Assert.assertNotNull(ConfigSingleton.getInstance(), "Impossible to load SpagoBI configuration file");
+			
+			callback = getAttributeAsString( CALLBACK );
+			logger.debug("Parameter [" + CALLBACK + "] is equals to [" + callback + "]");
+			
+			usr = getAttributeAsString( USER_ID );
+			logger.debug("Parameter [" + USER_ID + "] is equals to [" + usr + "]");
+			
+			pwd = getAttributeAsString( PASSWORD );  
+			logger.debug("Parameter [" + PASSWORD + "] is equals to [" + (StringUtilities.isEmpty(pwd)? "null": "*******") + "]"); // do not log pwd !
+			
+			theme = getAttributeAsString( THEME );
+			logger.debug("Parameter [" + THEME + "] is equals to [" + theme + "]");
+			
+			backUrl= getAttributeAsString( BACK_URL );
+			logger.debug("Parameter [" + BACK_URL + "] is equals to [" + backUrl + "]");
+			
+			
+			
+			
+			settingSB = (SourceBean)ConfigSingleton.getInstance().getAttribute( SSO_ACTIVE );
+			Assert.assertNotNull(settingSB, "Impossible to read from static configuration the block [" + SSO_ACTIVE + "]");
+			Assert.assertTrue(!StringUtilities.isEmpty(settingSB.getCharacters()), "Impossible to read from static configuration the value associated to block [" + SSO_ACTIVE + "]");
+			logger.debug("Configuration parameter [" + SSO_ACTIVE + "] is equals to [" + settingSB.getCharacters() + "]");
+			isSSOActive = "TRUE".equalsIgnoreCase(settingSB.getCharacters());
+			logger.info("SSO is " + (isSSOActive?"enabled" : "disabled"));
+			
+						
+			// If SSO is not active, check username and password, i.e. performs the authentication;
+			// instead, if SSO is active, the authentication mechanism is provided by the SSO itself, so SpagoBI does not make 
+			// any authentication, just creates the user profile object and puts it into Spago permanent container
+			if(!isSSOActive) {
+				ISecurityServiceSupplier supplier = SecurityServiceSupplierFactory.createISecurityServiceSupplier();
+								
+				Assert.assertNotNull(usr, "User identifier not spicified");				
+				Assert.assertNotNull(usr, "Password not spicified");
+				
+				if( supplier.checkAuthentication(usr, pwd) == null) {
+					logger.warn("An attempt to authenticate with wrong credential has made [" + usr + "/" + pwd +"]");
+					throw new SpagoBIServiceException(SERVICE_NAME, "An attempt to authenticate with wrong credential has made [" + usr + "/" + pwd +"]");
+				}
+				
+			} else {
+				usr = UserUtilities.getUserId(this.getHttpRequest());
+				Assert.assertNotNull(usr, "User identifier not found. Cannot build user profile object");
+				
+			}
+			
+			logger.info("User ["+ usr + "] has been autheticated succesfully");
+			
+			profile = UserUtilities.getUserProfile( usr );
+			Assert.assertNotNull(profile, "Impossible to load profile for the user [" + usr + "]");
 
+			
+			logger.info("User ["+ usr + "] profile has been loaded succesfully");
+			
+			// Propagate user profile
+			getSessionContainer().getPermanentContainer().setAttribute(IEngUserProfile.ENG_USER_PROFILE, profile);
+			
+			// Propagate THEME  if present
+			if (theme != null && theme.length() > 0){
+				getSessionContainer().getPermanentContainer().setAttribute(SpagoBIConstants.THEME, theme);
+			}
+			
+			// Propagate BACK URL if present
+			if (!StringUtilities.isEmpty( backUrl )){
+				getHttpSession().setAttribute(SpagoBIConstants.BACK_URL, backUrl);		
+			}
+						
+			// Propagate locale
+			Locale locale = MessageBuilder.getBrowserLocaleFromSpago();
+			logger.debug("User ["+ usr + "] loacale has been set to [" + locale.getLanguage() + "/" + locale.getCountry() + "]");
+			if (locale != null) {
+				getSessionContainer().getPermanentContainer().setAttribute(Constants.USER_LANGUAGE, locale.getLanguage());
+				getSessionContainer().getPermanentContainer().setAttribute(Constants.USER_COUNTRY, locale.getCountry());
+			}
+			
+			String username = (String)((UserProfile)profile).getUserId();
+			if (!UserUtilities.userFunctionalityRootExists(username)) {
+				logger.debug("Funcitonality root not yet exists for " + username);	
+				UserUtilities.createUserFunctionalityRoot(profile);
+			} else{
+				logger.debug("Funcitonality root already exists for " + username);					
+			}
+
+			MenuUtilities.getMenuItems(request, response, profile);	
+			try {
+				JSONObject results = new JSONObject();
+				results.put("username", username);
+				results.put("userid", profile.getUserUniqueIdentifier());
+				writeBackToClient( new JSONSuccess( results, callback ) );
+			} catch (IOException e) {
+				throw new SpagoBIServiceException("Impossible to write back the responce to the client", e);
+			}
+		} catch(Throwable t) {
+			SpagoBIServiceExceptionHandler.getInstance().getWrappedException(SERVICE_NAME, t);
+		} finally {
+			logger.info("User ["+ usr + "] has been logged in succesfully");
+			logger.debug("OUT");
+		}
 	}
-	}
+}
+
+
 
 

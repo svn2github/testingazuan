0318ced1d59aea4bcf04627f5b91565919d258ee
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-06-30 12:45:46 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@8233 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/behaviour/QuerableBehaviour.java
+++ V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/tools/dataset/common/behaviour/QuerableBehaviour.java
@@ -21,15 +21,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  **/
 package it.eng.spagobi.tools.dataset.common.behaviour;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import it.eng.spago.base.SourceBeanException;
 import it.eng.spago.error.EMFErrorSeverity;
 import it.eng.spago.error.EMFInternalError;
 import it.eng.spago.error.EMFUserError;
+import it.eng.spago.security.IEngUserProfile;
 import it.eng.spagobi.commons.utilities.StringUtilities;
 import it.eng.spagobi.tools.dataset.bo.DataSetParameterItem;
 import it.eng.spagobi.tools.dataset.bo.DataSetParametersList;
@@ -37,6 +33,13 @@ import it.eng.spagobi.tools.dataset.bo.IDataSet;
 import it.eng.spagobi.tools.dataset.bo.JDBCDataSet;
 import it.eng.spagobi.tools.dataset.bo.ScriptDataSet;
 import it.eng.spagobi.tools.dataset.common.query.IQueryTransformer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
@@ -54,7 +57,7 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 	}
 
 	public String getStatement() throws EMFInternalError, EMFUserError{
-
+		logger.debug("IN");
 		String statement;
 
 		IDataSet dataSet=getTargetDataSet();
@@ -70,13 +73,30 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 				statement = (String)dataSet.getQuery();
 
 		if(statement != null) {
-			try {
-				statement = StringUtilities.substituteProfileAttributesInString(statement, getTargetDataSet().getUserProfile() );
+			// if script substitute profile attributes in a strict way
+			if (dataSet instanceof ScriptDataSet) {
+				try{
+					HashMap attributes = getAllProfileAttributes(getTargetDataSet().getUserProfile()); // to be cancelled, now substitutution inline
+					statement=substituteProfileAttributes(statement, attributes);
+				}
+				catch (EMFInternalError e) {
+					logger.error("Errore nella valorizzazione degli attributi i profilo del dataset Script",e);
+					throw(e);
+				}
+
+
+			}			
+			else if (dataSet instanceof JDBCDataSet) {
+
+				try {
+					statement = StringUtilities.substituteProfileAttributesInString(statement, getTargetDataSet().getUserProfile() );
+				}
+				catch (Exception e) {
+					EMFUserError userError = new EMFUserError(EMFErrorSeverity.ERROR, 9213);
+					logger.error("Errore nella valorizzazione degli attributi di profilodel dataset JDBC",e);
+					throw userError;}
 			}
-			catch (Exception e) {
-				EMFUserError userError = new EMFUserError(EMFErrorSeverity.ERROR, 9213);
-				logger.error("Errore nella valorizzazione dei parametri",e);
-				throw userError;}
+
 
 			//check if there are parameters filled
 			if( getTargetDataSet().getParamsMap() != null && !getTargetDataSet().getParamsMap().isEmpty()){
@@ -97,12 +117,37 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 				statement = (String)queryTransformer.transformQuery( statement );
 			}
 		}
-
+		logger.debug("OUT");
 		return statement;
 	}
 
 
+	private String substituteProfileAttributes(String script, HashMap attributes) throws EMFInternalError{
+		logger.debug("IN");
+		String cleanScript=new String(script);
+		int indexSubstitution=0;
+		int profileAttributeStartIndex = script.indexOf("${",indexSubstitution);
+
+		while (profileAttributeStartIndex != -1) {
+			int profileAttributeEndIndex=script.indexOf("}",profileAttributeStartIndex);
+			String attributeName = script.substring(profileAttributeStartIndex + 2, profileAttributeEndIndex).trim();
+			Object attributeValueObj = attributes.get(attributeName);
+			if(attributeValueObj==null)
+			{
+				logger.error("Profile attribute "+attributeName+" not found");
+				attributeValueObj="undefined";
+			}
+			cleanScript=cleanScript.replaceAll("\\$\\{"+attributeName+"\\}", attributeValueObj.toString());
+			indexSubstitution=profileAttributeEndIndex;
+			profileAttributeStartIndex = script.indexOf("${",indexSubstitution);
+		}
+		logger.debug("OUT");
+		return cleanScript;	
+	}
+
+
 	public Map getParTypeMap(IDataSet dataSet) throws SourceBeanException{
+		logger.debug("IN");
 		// recover parameters from dataset and their type
 		Map parTypeMap=new HashMap();
 		String parametersXML=dataSet.getParameters();	
@@ -117,8 +162,37 @@ public class QuerableBehaviour extends AbstractDataSetBehaviour {
 			String type = dsDet.getType();
 			parTypeMap.put(name, type);
 		}
+		logger.debug("OUT");
 		return parTypeMap;
 
+	}
+
+	/**
+	 * Gets the all profile attributes. (Also present in GeneralUtilities) TODO: centralization of two methods
+	 * 
+	 * @param profile the profile
+	 * 
+	 * @return the all profile attributes
+	 * 
+	 * @throws EMFInternalError the EMF internal error
+	 */
+	public static HashMap getAllProfileAttributes(IEngUserProfile profile) throws EMFInternalError {
+		logger.debug("IN");
+		if (profile == null)
+			throw new EMFInternalError(EMFErrorSeverity.ERROR,
+			"getAllProfileAttributes method invoked with null input profile object");
+		HashMap profileattrs = new HashMap();
+		Collection profileattrsNames = profile.getUserAttributeNames();
+		if (profileattrsNames == null || profileattrsNames.size() == 0)
+			return profileattrs;
+		Iterator it = profileattrsNames.iterator();
+		while (it.hasNext()) {
+			Object profileattrName = it.next();
+			Object profileattrValue = profile.getUserAttribute(profileattrName.toString());
+			profileattrs.put(profileattrName, profileattrValue);
+		}
+		logger.debug("OUT");
+		return profileattrs;
 	}
 
 	public IQueryTransformer getQueryTransformer() {

1e5e78afabdb332263d90f04d69962849b7fd166
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-07-02 15:15:18 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@8312 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIQbeEngine/WebContent/js/spagobi/commons/exceptionHandler.js
+++ V2.x/trunk/SpagoBIQbeEngine/WebContent/js/spagobi/commons/exceptionHandler.js
@@ -17,15 +17,35 @@ it.eng.spagobi.engines.qbe.exceptionhandler.module = function(){
 		
 		
         handleFailure : function(response, options) {
-        	var str = "";
-        	for(p in response) {
-        		str += p + " = " + response[p] + "\n";
-        	}
-        	
+
         	var content = Ext.util.JSON.decode( response.responseText );
+        	
+        	var errMessage = ''
+            	if(response !== undefined) {
+            		
+            		if(response.responseText !== undefined) {
+            			var content = Ext.util.JSON.decode( response.responseText );
+            			if (content.errors !== undefined) {
+        					for (var count = 0; count < content.errors.length; count++) {
+        						var anError = content.errors[count];
+			        			if (anError.localizedMessage !== undefined && anError.localizedMessage !== '') {
+			        				errMessage += anError.localizedMessage;
+			        			} else if (anError.message !== undefined && anError.message !== '') {
+			        				errMessage += anError.message;
+			        			}
+			        			if (count < content.errors.length - 1) {
+			        				errMessage += '<br/>';
+			        			}
+        					}
+        				}
+            		} else {
+            			errMessage = 'Generic error';
+            		}
+            	}
+        	
         	Ext.MessageBox.show({
            		title: 'Service Error',
-           		msg: content.cause,
+           		msg: errMessage,
            		buttons: Ext.MessageBox.OK,
            		//fn: showResult,
            		icon: Ext.MessageBox.ERROR

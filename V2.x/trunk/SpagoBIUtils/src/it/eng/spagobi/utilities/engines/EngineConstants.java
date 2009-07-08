07d2b3188e98b09d03b53aad82b6a6f271597b6b
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-07-08 08:55:47 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@8377 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/utilities/engines/EngineConstants.java
+++ V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/utilities/engines/EngineConstants.java
@@ -32,7 +32,6 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 **/
 package it.eng.spagobi.utilities.engines;
 
-import it.eng.spago.security.IEngUserProfile;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
@@ -55,5 +54,6 @@ public class EngineConstants {
 	public static final String ENV_CONTENT_SERVICE_PROXY = "CONTENT_SERVICE_PROXY";
 	public static final String ENV_AUDIT_SERVICE_PROXY = "AUDIT_SERVICE_PROXY";
 	public static final String ENV_LOCALE = "LOCALE";
-	public static final String ENV_EVENT_SERVICE_PROXY = "EVENT_SERVICE_PROXY";	
+	public static final String ENV_EVENT_SERVICE_PROXY = "EVENT_SERVICE_PROXY";
+	public static final String ENV_USER_PROFILE = "ENV_USER_PROFILE";	
 }

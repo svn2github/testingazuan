5a4112fc52468677301e67fe267c373b1c8439bb
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-07-22 07:11:11 +0000

    Can insert many charts into report
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@8477 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/trunk/SpagoBIJasperReportEngine/JavaSource/it/eng/spagobi/engines/jasperreport/ScriptletChart.java
@@ -0,0 +1,175 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2008 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ **/
+
+package it.eng.spagobi.engines.jasperreport;
+
+
+
+
+import it.eng.spagobi.services.proxy.DocumentExecuteServiceProxy;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+import net.sf.jasperreports.engine.JRDefaultScriptlet;
+import net.sf.jasperreports.engine.JRScriptletException;
+import net.sf.jasperreports.engine.fill.JRFillVariable;
+
+/**
+ * @author Giulio Gavardi
+ *         giulio.gavardi@eng.it
+ */
+
+public class ScriptletChart extends JRDefaultScriptlet {
+
+	private static transient Logger logger=Logger.getLogger(ScriptletChart.class);
+
+
+	public void afterReportInit() throws JRScriptletException {
+
+		logger.debug("IN");
+
+
+		try {
+			// Creazione GET method
+
+			HashMap parametersMap=(HashMap)this.getParameterValue("REPORT_PARAMETERS_MAP");
+
+
+			String userId=(String)parametersMap.get("SBI_USERID");
+
+			logger.debug("user id is "+userId);
+
+			HttpSession session=(HttpSession)parametersMap.get("SBI_HTTP_SESSION");
+
+			HashMap chartParameters=new HashMap();
+
+			// Get all defined variables wich start with prefix sbichart,
+			// each is an image to fill
+			Map allVariables = this.variablesMap;
+			if(allVariables==null)allVariables=new HashMap();
+			logger.debug("Running all variables");
+			for (Iterator iterator = allVariables.keySet().iterator(); iterator.hasNext();) {
+				String varName = (String) iterator.next();
+				logger.debug("Processing variable "+varName);
+				if(varName.startsWith("sbichart_")){
+					JRFillVariable variable=(JRFillVariable)allVariables.get(varName);
+					if(variable.getValue()!=null){
+//						the realVarName is the name of the target variable!
+						String areaValue=varName.substring(9);
+						String chartLabel="";
+						String chartArea="";						
+						// call a utility function that parse the variable, in the form var1=val1;var2=val2
+						String varVal=(String)variable.getValue();
+						Map nameValuePars=parseVariable(varVal);
+
+						// check if there are the main parameters defined: they are, 
+						// chart_label : indicating the label of the chart that has to be called.
+						// chart_area
+						if(nameValuePars.get("chart_label")!=null){
+							String labelValue=(String) nameValuePars.get("chart_label");
+							logger.debug("execute chart with lable "+labelValue);
+
+							// Set other parameters
+							for (Iterator iterator2 = nameValuePars.keySet().iterator(); iterator2.hasNext();) {
+								String namePar = (String) iterator2.next();
+								if(!namePar.equalsIgnoreCase("chart_label")){
+									Object value=nameValuePars.get(namePar);
+									chartParameters.put(namePar, value);
+								}
+							}
+
+							DocumentExecuteServiceProxy proxy=new DocumentExecuteServiceProxy(userId,session);
+							logger.debug("Calling Service");
+							byte[] image=proxy.executeChart(labelValue, chartParameters);
+							logger.debug("Back from Service");
+
+							InputStream is=new ByteArrayInputStream(image);
+
+							logger.debug("Input Stream filled, Setting variable");
+							if(variablesMap.keySet().contains(areaValue)){
+								this.setVariableValue(areaValue, is);
+							}
+							else{
+								logger.error("variable where to set image chart "+areaValue+ " not defined");
+							}
+
+							is.close();
+						}
+						else{
+							logger.warn("chart_label not specified");							
+						}
+					}
+					else{
+						logger.warn("no value associated to the sbichart_ variable");
+					}
+				}
+
+			}
+
+			logger.debug("OUT");
+		} 
+		catch (Exception e) {
+			logger.error("Error in scriptlet",e);
+			throw new JRScriptletException(e);
+		}
+	}
+
+	/**
+	 *  varValue: parses the variable value in the form att1=value1; att2=value2;
+	 */
+
+	protected Map parseVariable(String varValue){
+		HashMap toReturn=new HashMap<String, String>();
+		StringTokenizer tokenizer=new StringTokenizer(varValue,";");
+		while(tokenizer.hasMoreTokens()){
+			String token=tokenizer.nextToken();
+			int indexEqual=token.indexOf("=");
+			String namePar=token.substring(0,indexEqual);
+			String valuePar=token.substring(indexEqual+1);
+			toReturn.put(namePar, valuePar);
+		}
+		return toReturn;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

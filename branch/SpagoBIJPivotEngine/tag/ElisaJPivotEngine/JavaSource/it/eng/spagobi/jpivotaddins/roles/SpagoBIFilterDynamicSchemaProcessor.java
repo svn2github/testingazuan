8bf340b8e2565053e26f381656efb39b5ebd8dec
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-09-09 10:04:40 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@8703 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- branch/SpagoBIJPivotEngine/tag/ElisaJPivotEngine/JavaSource/it/eng/spagobi/jpivotaddins/roles/SpagoBIFilterDynamicSchemaProcessor.java
+++ branch/SpagoBIJPivotEngine/tag/ElisaJPivotEngine/JavaSource/it/eng/spagobi/jpivotaddins/roles/SpagoBIFilterDynamicSchemaProcessor.java
@@ -11,7 +11,10 @@ public class SpagoBIFilterDynamicSchemaProcessor extends
 	protected String filter(String schemaUrl, Util.PropertyList connectInfo, InputStream stream) throws Exception {
 		
 		String originalSchema = super.filter(schemaUrl, connectInfo, stream);
-		String modifiedSchema = originalSchema.replaceAll("\\$\\{filter\\}", connectInfo.get("filter"));
+		String modifiedSchema = originalSchema;
+		if (connectInfo.get("filter") != null) {
+			modifiedSchema = originalSchema.replaceAll("\\$\\{filter\\}", connectInfo.get("filter"));
+		}
 		return modifiedSchema;
     }
 	

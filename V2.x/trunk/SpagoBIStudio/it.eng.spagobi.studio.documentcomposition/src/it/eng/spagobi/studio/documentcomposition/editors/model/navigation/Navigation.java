14672022f29307c9d068a8b6b95235ddbfeb349c
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-10-15 12:20:04 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@9234 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/editors/model/navigation/Navigation.java
+++ V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/editors/model/navigation/Navigation.java
@@ -3,6 +3,15 @@ package it.eng.spagobi.studio.documentcomposition.editors.model.navigation;
 public class Navigation {
 	
 	private String navigationNameText;
+	private String masterDocNameText;
+
+	public String getMasterDocNameText() {
+		return masterDocNameText;
+	}
+
+	public void setMasterDocNameText(String masterDocNameText) {
+		this.masterDocNameText = masterDocNameText;
+	}
 
 	public String getNavigationNameText() {
 		return navigationNameText;

0463ecf517da63127c89611c01f438fdb227f29a
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-10-22 15:19:30 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@9369 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/editors/model/documentcomposition/metadata/MetadataDocument.java
+++ V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/editors/model/documentcomposition/metadata/MetadataDocument.java
@@ -1,22 +1,89 @@
 package it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.metadata;
 
+import it.eng.spagobi.studio.core.util.ParametersMetadata;
+
 import java.util.Vector;
 
 public class MetadataDocument {
+	
+	
+	private Integer id;
 	private String name;
-	private Vector<MetadataParameter> parameters;
+	private String label;
+	private String description;
+	private String type;
+	private String engine;
+	private String dataSet;
+	private String dataSource;
+	private String templateName;
+	private String state;
+	private Vector<MetadataParameter> metadataParameters;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Vector<MetadataParameter> getParameters() {
-		return parameters;
+	public String getLabel() {
+		return label;
 	}
-	public void setParameters(Vector<MetadataParameter> parameters) {
-		this.parameters= parameters;
+	public void setLabel(String label) {
+		this.label = label;
 	}
-
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	public String getDataSet() {
+		return dataSet;
+	}
+	public void setDataSet(String dataSet) {
+		this.dataSet = dataSet;
+	}
+	public String getDataSource() {
+		return dataSource;
+	}
+	public void setDataSource(String dataSource) {
+		this.dataSource = dataSource;
+	}
+	public String getTemplateName() {
+		return templateName;
+	}
+	public void setTemplateName(String templateName) {
+		this.templateName = templateName;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Vector<MetadataParameter> getMetadataParameters() {
+		return metadataParameters;
+	}
+	public void setMetadataParameters(Vector<MetadataParameter> metadataParameters) {
+		this.metadataParameters = metadataParameters;
+	}
 
 }
--- V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/editors/model/documentcomposition/metadata/MetadataParameter.java
+++ V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/editors/model/documentcomposition/metadata/MetadataParameter.java
@@ -1,14 +1,46 @@
 package it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.metadata;
 
 public class MetadataParameter {
-	private String label;
 
-	public String getLabel() {
+    private java.lang.Integer id;
+
+    private java.lang.String label;
+
+    private java.lang.String type;
+
+    private java.lang.String urlName;
+
+	public java.lang.Integer getId() {
+		return id;
+	}
+
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+
+	public java.lang.String getLabel() {
 		return label;
 	}
 
-	public void setLabel(String label) {
+	public void setLabel(java.lang.String label) {
 		this.label = label;
 	}
 
+	public java.lang.String getType() {
+		return type;
+	}
+
+	public void setType(java.lang.String type) {
+		this.type = type;
+	}
+
+	public java.lang.String getUrlName() {
+		return urlName;
+	}
+
+	public void setUrlName(java.lang.String urlName) {
+		this.urlName = urlName;
+	}
+
+    
 }

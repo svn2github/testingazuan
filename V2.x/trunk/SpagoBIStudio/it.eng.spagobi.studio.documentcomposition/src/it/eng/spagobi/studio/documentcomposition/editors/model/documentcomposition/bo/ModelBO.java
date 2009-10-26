131f52f256d21f4c0750ff576f32e5104fddba44
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-10-26 16:58:15 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@9434 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/editors/model/documentcomposition/bo/ModelBO.java
+++ V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/editors/model/documentcomposition/bo/ModelBO.java
@@ -1,5 +1,8 @@
 package it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.bo;
 
+import java.util.Iterator;
+import java.util.Vector;
+
 import it.eng.spagobi.studio.documentcomposition.Activator;
 import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.Document;
 import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.DocumentComposition;
@@ -7,6 +10,7 @@ import it.eng.spagobi.studio.documentcomposition.editors.model.documentcompositi
 import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.Parameter;
 import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.Refresh;
 import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.Style;
+import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.metadata.MetadataDocument;
 import it.eng.spagobi.studio.documentcomposition.util.XmlTemplateGenerator;
 
 import org.eclipse.core.resources.IFile;
@@ -22,11 +26,56 @@ public class ModelBO {
 		return documentComposition;
 	}
 
-	public void saveModel(DocumentComposition documentComposition){
-		Activator.getDefault().setDocumentComposition(documentComposition);
-	}
+		public void saveModel(DocumentComposition documentComposition){
+			Activator.getDefault().setDocumentComposition(documentComposition);
+		}
 
 	public DocumentComposition getModel(){
 		return Activator.getDefault().getDocumentComposition();
 	}
+
+	
+	/** update the model with a new document!
+	 * 
+	 */
+	public void updateModelWithNewDocument(MetadataDocument _metadataDocument, Style style){
+		ModelBO bo=new ModelBO();
+		DocumentComposition documentComposition=bo.getModel();
+		DocumentsConfiguration documentsConfiguration=documentComposition.getDocumentsConfiguration();
+		if(documentsConfiguration==null)documentsConfiguration=new DocumentsConfiguration();
+		Vector<Document> documents=documentsConfiguration.getDocuments();
+		if(documents==null)documents=new Vector<Document>();
+		Document newDocument=new Document();
+		newDocument.setSbiObjLabel(_metadataDocument.getLabel());
+		newDocument.setLocalFileName(_metadataDocument.getLocalFileName());
+		newDocument.setStyle(style);
+		documents.add(newDocument);
+		bo.saveModel(documentComposition);
+	}
+
+	/** update the model with a new document!
+	 * 
+	 */
+	public void updateModelModifyDocument(MetadataDocument _metadataDocument, Style style){
+		ModelBO bo=new ModelBO();
+		DocumentComposition documentComposition=bo.getModel();
+		DocumentsConfiguration documentsConfiguration=documentComposition.getDocumentsConfiguration();
+		Vector<Document> documents=documentsConfiguration.getDocuments();
+		if(documents!=null){
+			for (Iterator iterator = documents.iterator(); iterator.hasNext();) {
+				Document document = (Document) iterator.next();
+				// Modify the current document!
+				if(document.getSbiObjLabel().equals(_metadataDocument.getLabel())){
+					document.setStyle(style);
+				}
+
+			}
+			bo.saveModel(documentComposition);
+		}
+	}
+
+
+
+
+
 }

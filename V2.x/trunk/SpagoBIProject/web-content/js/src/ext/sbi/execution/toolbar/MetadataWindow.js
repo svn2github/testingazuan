199844ab20ea9cceb30e55e731cd39915970eff4
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2009-12-09 09:54:48 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@9962 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/web-content/js/src/ext/sbi/execution/toolbar/MetadataWindow.js
+++ V2.x/trunk/SpagoBIProject/web-content/js/src/ext/sbi/execution/toolbar/MetadataWindow.js
@@ -41,7 +41,7 @@
   * 
   * Authors
   * 
-  * - Davide Zerbetto (davide.zerbetto@eng.it)
+  * - Chiara Chiarelli (chiara.chiarelli@eng.it)
   */
 
 Ext.ns("Sbi.execution.toolbar");
@@ -56,10 +56,25 @@ Sbi.execution.toolbar.MetadataWindow = function(config) {
 		, baseParams: params
 	}) + '&MESSAGEDET=METADATA_SELECT&OBJECT_ID=' + config.OBJECT_ID;
 	
+	this.services['getValuesForMetadataGridService'] = Sbi.config.serviceRegistry.getServiceUrl({
+		serviceName: 'GET_METADATA_ACTION'
+		, baseParams: params
+	})+ '&OBJECT_ID=' + config.OBJECT_ID;
+	
 	this.buddy = undefined;
 	
-	var c = Ext.apply({}, config, {
-		id:'win_metadata',
+	/*this.initMetadata();
+	
+	 var store = this.createGridSourceStore();
+	    store.load();
+	    alert(this.gridStore.toSource());
+	    this.shortTextStore = this.gridStore.shorttext;
+	    this.longTextStore = this.gridStore.longtext;
+	    var temp = this.createGrid();
+	    this.shortTextGrid.setSource(temp);*/
+	
+		var c = Ext.apply({}, config, {
+		id:'win_metadata'
 		bodyCfg: {
 			tag:'div',
 			cls:'x-panel-body',
@@ -72,17 +87,29 @@ Sbi.execution.toolbar.MetadataWindow = function(config) {
   				style: {overflow:'auto'}  
 			}]
 		},
-		layout:'fit',
-		width:650,
-		height:400,
-		//closeAction:'hide',
-		plain: true,
-		title: LN('sbi.execution.metadata')
+		//,items: [this.shortTextPanel,this.longTextPanel]
+		/*,buttons: [
+		          {
+		            id: 'save'
+		        	  ,text: LN('sbi.execution.notes.savenotes') 
+		        	  ,scope: this
+		        	  ,handler: this.saveMetadata
+		        	  ,disabled: false
+		          }
+		]*/
+		,layout:'fit'
+		,width:650
+		,height:410
+		,plain: true
+		,autoScroll:true
+		,title: LN('sbi.execution.metadata')
 	});   
+	
+	
 	
 	// constructor
     Sbi.execution.toolbar.MetadataWindow.superclass.constructor.call(this, c);
-    
+	
     if (this.buddy === undefined) {
     	this.buddy = new Sbi.commons.ComponentBuddy({
     		buddy : this
@@ -91,4 +118,146 @@ Sbi.execution.toolbar.MetadataWindow = function(config) {
     
 };
 
-Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {});
+Ext.extend(Sbi.execution.toolbar.MetadataWindow, Ext.Window, {
+	services:null
+   ,gridStore:null
+   ,shortTextStore:null
+   ,longTextStore:null
+   ,shortTextPanel: null
+   ,shortTextGrid:null
+   ,longTextPanel: null
+   ,longTextTabPanel: null
+   ,longTextEditor1:null
+   ,longTextEditor2:null
+   
+   ,initMetadata: function(){
+	    
+	   
+	    
+	    this.shortTextGrid = new Ext.grid.PropertyGrid({
+	        width: 640,
+	        autoHeight: true,
+	        cm: new Ext.grid.ColumnModel([
+				{header: "Ticker", width: 100, sortable: true},
+				{header: "Company Name", width: 540, sortable: true}
+			 ]),
+			columns: ["Ticker","Company Name"],
+	        source: {},
+	        viewConfig : {
+	            forceFit: true,
+	            scrollOffset: 2 // the grid will never have scrollbars
+	        }
+	    });
+	     
+	    
+	    this.shortTextPanel = new Ext.Panel({
+	        title: 'Short Text Metadata',
+	        collapsible: true,
+	        collapsed : true,
+	        items: [this.shortTextGrid],
+	        autoWidth : true,
+			autoHeight : true
+	    });
+		  
+	var tab1 =  new Ext.Panel({
+			title: 'Objective'
+	});
+	
+	var tab2 =  new Ext.Panel({
+			title: 'Long Description'
+	});
+	
+	
+	tab1.on("show", function() {
+		this.longTextEditor1 = new Ext.form.HtmlEditor({
+		         frame: true
+		        ,value: 'cc'
+		      	,width: '100%'
+		        ,disabled: false
+			   ,height: 258
+			    ,enableSourceEdit: false      
+		  }); 
+		tab1.add(this.longTextEditor1);
+	}, this);
+	
+	tab2.on("show", function() {
+		
+		this.longTextEditor2 = new Ext.form.HtmlEditor({
+		         frame: true
+		        ,value: 'cc'
+		      	,width: '100%'
+		        ,disabled: false
+			   ,height: 258
+			    ,enableSourceEdit: false      
+		  }); 
+		  
+		tab2.add(this.longTextEditor2);
+	}, this);
+	  
+	this.longTextTabPanel = new Ext.TabPanel({
+		 hideMode: 'offsets'	
+	       , resizeTabs: true, 
+	        minTabWidth: 115,
+	        tabWidth: 135,
+	        enableTabScroll: true,
+	        activeTab: 1,
+		    autoScroll:true,
+		    
+		    items: [tab1,tab2]
+		});
+		
+	this.longTextPanel = new Ext.Panel({
+	        title: 'Long Text Metadata',
+	        collapsible: true,
+	        collapsed : false,
+	        items: [this.longTextTabPanel]
+	    });
+
+	}
+	
+	, createGridSourceStore: function() {
+
+		var createGridStoreUrl = this.services['getValuesForMetadataGridService'];	
+		var store = new Ext.data.JsonStore({
+			url: createGridStoreUrl
+		});
+		store.on('loadexception', function(store, options, response, e) {
+			var msg = '';
+			var content = Ext.util.JSON.decode( response.responseText );		
+  			if(content !== undefined) {
+  				this.gridStore = content;
+  				msg += content.serviceName + ' : ' + content.message;
+  			} else {
+  				msg += 'Server response is empty';
+  			}
+	
+			Sbi.exception.ExceptionHandler.showErrorMessage(msg, response.statusText);
+		});
+		return store;
+	}
+	
+	,saveMetadata: function(){
+	
+	}
+	
+	,createGrid: function(){
+		var toReturn;
+		if(this.shortTextStore){
+		alert(this.shortTextStore.toSource());
+			var tempStr = '{ ';
+		      for(var i=0;i<this.shortTextStore.length;i++){
+			      if(i!=0){
+			      	tempStr += ' , ';
+			      }
+		      	  tempStr += this.shortTextStore[i].meta_name +': '+this.shortTextStore[i].meta_content;
+		      }
+		      tempStr += ' }';
+		      alert(tempStr);
+		      toReturn = eval(tempStr);
+		}else{
+			toReturn = {};
+		}
+	    return toReturn;
+	 }
+
+});

56cd4a18ccab7e2a34e517081e1632cb3584b029
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-01-20 18:25:49 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@10338 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V2.x/branches/SpagoBIQbeEngineInail/SpagoBIQbeEngine/WebContent/js/spagobi/locale/widgets/lookupfield/en.js
@@ -0,0 +1,6 @@
+Ext.ns("Sbi.locale");
+Sbi.locale.ln = Sbi.locale.ln || new Array();
+
+Sbi.locale.ln['sbi.lookup.Confirm'] = 'Confirm';
+Sbi.locale.ln['sbi.lookup.Annulla'] = 'Cancel';
+Sbi.locale.ln['sbi.lookup.Select']= 'Select Value ...';
---
+++ V2.x/branches/SpagoBIQbeEngineInail/SpagoBIQbeEngine/WebContent/js/spagobi/locale/widgets/lookupfield/it.js
@@ -0,0 +1,6 @@
+Ext.ns("Sbi.locale");
+Sbi.locale.ln = Sbi.locale.ln || new Array();
+
+Sbi.locale.ln['sbi.lookup.Confirm'] = 'Conferma';
+Sbi.locale.ln['sbi.lookup.Annulla'] = 'Annulla';
+Sbi.locale.ln['sbi.lookup.Select']= 'Seleziona un valore ...';

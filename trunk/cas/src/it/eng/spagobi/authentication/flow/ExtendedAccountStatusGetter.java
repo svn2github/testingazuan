392276905c0effafc48b7eab600e4c09271134b7
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-01-25 09:52:37 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@10361 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- trunk/cas/src/it/eng/spagobi/authentication/flow/ExtendedAccountStatusGetter.java
+++ trunk/cas/src/it/eng/spagobi/authentication/flow/ExtendedAccountStatusGetter.java
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.authentication.flow;
 
-
+  
 import it.eng.spago.base.SourceBean;
 import it.eng.spago.base.SourceBeanAttribute;
 import it.eng.spagobi.authentication.utility.AuthenticationUtility;

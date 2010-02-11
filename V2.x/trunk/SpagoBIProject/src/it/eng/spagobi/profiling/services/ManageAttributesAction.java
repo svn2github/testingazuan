944b0a9d8b587ad509e7a723eea52cf8857afdf2
chiarelli <chiarelli@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-02-11 15:33:17 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@10568 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/profiling/services/ManageAttributesAction.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/profiling/services/ManageAttributesAction.java
@@ -21,7 +21,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.profiling.services;
 
+import it.eng.spago.error.EMFErrorSeverity;
 import it.eng.spago.error.EMFUserError;
+import it.eng.spago.validation.EMFValidationError;
 import it.eng.spagobi.analiticalmodel.document.x.AbstractSpagoBIAction;
 import it.eng.spagobi.chiron.serializer.SerializationException;
 import it.eng.spagobi.chiron.serializer.SerializerFactory;
@@ -40,6 +42,7 @@ import java.util.ArrayList;
 import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.validator.GenericValidator;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -50,13 +53,16 @@ public class ManageAttributesAction extends AbstractSpagoBIAction{
 	private final String MESSAGE_DET = "MESSAGE_DET";
 	// type of service
 	private final String ATTR_LIST = "ATTR_LIST";
-	private final String ATTR_INSERT = "ATTR_INSERT";
 	private final String ATTR_DELETE = "ATTR_DELETE";
 	
 	private final String SAMPLES = "samples";
 	private final String ID = "id";
 	private final String NAME = "name";
 	private final String DESCRIPTION = "description";
+	private final int nameMaxLenght = 250;
+	private final int descriptionMaxLenght = 500;
+	private static final String ALPHANUMERIC_STRING_REGEXP_NOSPACE="^([a-zA-Z0-9\\-\\_])*$";
+	
 	/**
 	 * 
 	 */
@@ -98,9 +104,22 @@ public class ManageAttributesAction extends AbstractSpagoBIAction{
 						}
 						if(!samples.isNull(NAME)){
 							name = samples.getString(NAME);
+							if (!GenericValidator.isBlankOrNull(name) && 
+									!GenericValidator.matchRegexp(name, ALPHANUMERIC_STRING_REGEXP_NOSPACE)&&
+										!GenericValidator.maxLength(name, nameMaxLenght)){
+								logger.error("Either the field name is blank or it exceeds maxlength or it is not alfanumeric");
+								new EMFValidationError(EMFErrorSeverity.ERROR, description, "9000","");
+								
+							}
 						}
 						if(!samples.isNull(DESCRIPTION)){
 							description = samples.getString(DESCRIPTION);
+							if (!GenericValidator.isBlankOrNull(description) && 
+									!GenericValidator.matchRegexp(description, ALPHANUMERIC_STRING_REGEXP_NOSPACE) &&
+											!GenericValidator.maxLength(description, descriptionMaxLenght)){
+								logger.error("Either the field description is blank or it exceeds maxlength or it is not alfanumeric");
+								new EMFValidationError(EMFErrorSeverity.ERROR, description, "9000","");
+							}
 						}
 						
 						SbiAttribute attribute = new SbiAttribute();
@@ -118,7 +137,7 @@ public class ManageAttributesAction extends AbstractSpagoBIAction{
 						logger.debug("Attribute updated");
 						JSONObject attributesResponseSuccessJSON = new JSONObject();
 						attributesResponseSuccessJSON.put("success", true);
-						attributesResponseSuccessJSON.put("message", "Added User 1");
+						attributesResponseSuccessJSON.put("message", "");
 						attributesResponseSuccessJSON.put("data", "[]");
 						writeBackToClient( new JSONSuccess(attributesResponseSuccessJSON) );		
 						
@@ -135,55 +154,7 @@ public class ManageAttributesAction extends AbstractSpagoBIAction{
 				throw new SpagoBIServiceException(SERVICE_NAME,
 						"Exception occurred while retrieving attributes", e);
 			}
-		} /*else if (serviceType != null	&& serviceType.equalsIgnoreCase(ATTR_INSERT)) {
-		
-			String name = null;
-			String description =null;
-			try {
-				BufferedReader b =httpRequest.getReader();
-				if(b!=null){
-					String respJsonObject = b.readLine();
-					if(respJsonObject!=null){
-						JSONObject responseJSON = deserialize(respJsonObject);
-						JSONObject samples = responseJSON.getJSONObject(SAMPLES);
-					
-						name = samples.getString(NAME);
-						description = samples.getString(DESCRIPTION);
-					}
-				}
-				
-			} catch (IOException e1) {
-				logger.error("IO Exception",e1);
-				e1.printStackTrace();
-			} catch (SerializationException e) {
-				logger.error("Deserialization Exception",e);
-				e.printStackTrace();
-			} catch (JSONException e) {
-				logger.error("JSONException",e);
-				e.printStackTrace();
-			}
-
-			if (name != null) {
-				SbiAttribute attribute = new SbiAttribute();
-				attribute.setDescription(description);
-				attribute.setAttributeName(name);
-				
-				try {
-					attrDao.saveSbiAttribute(attribute);
-					logger.debug("New attribute inserted");
-					writeBackToClient( new JSONSuccess("Operation succeded") );
-
-				} catch (Throwable e) {
-					logger.error(e.getMessage(), e);
-					throw new SpagoBIServiceException(SERVICE_NAME,
-							"Exception occurred while saving new attribute",
-							e);
-				}
-			}else{
-				logger.error("Missing attribute name");
-				throw new SpagoBIServiceException(SERVICE_NAME,	"Please enter attribute name");
-			}
-		}*/ else if (serviceType != null	&& serviceType.equalsIgnoreCase(ATTR_DELETE)) {
+		} else if (serviceType != null	&& serviceType.equalsIgnoreCase(ATTR_DELETE)) {
 			
 			String idStr = null;
 			try {
@@ -213,7 +184,7 @@ public class ManageAttributesAction extends AbstractSpagoBIAction{
 					logger.debug("Attribute deleted");
 					JSONObject attributesResponseSuccessJSON = new JSONObject();
 					attributesResponseSuccessJSON.put("success", true);
-					attributesResponseSuccessJSON.put("message", "Destroyed User 1");
+					attributesResponseSuccessJSON.put("message", "");
 					attributesResponseSuccessJSON.put("data", "[]");
 					writeBackToClient( new JSONSuccess(attributesResponseSuccessJSON) );
 	
@@ -238,6 +209,8 @@ public class ManageAttributesAction extends AbstractSpagoBIAction{
 		writeBackToClient(new JSONSuccess(attributesResponseJSON));
 
 	}
+	
+	
 	
 	/**
 	 * Creates a json array with children attributes informations

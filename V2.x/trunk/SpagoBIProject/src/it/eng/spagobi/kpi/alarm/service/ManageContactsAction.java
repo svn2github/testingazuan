805dd32c2695c3d75b30d3cde9a0f58bb59ff4b3
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-02-22 11:12:56 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@10751 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/kpi/alarm/service/ManageContactsAction.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/kpi/alarm/service/ManageContactsAction.java
@@ -21,10 +21,22 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.kpi.alarm.service;
 
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 
+import it.eng.spago.error.EMFUserError;
 import it.eng.spagobi.analiticalmodel.document.x.AbstractSpagoBIAction;
+import it.eng.spagobi.commons.bo.Domain;
+import it.eng.spagobi.commons.dao.DAOFactory;
+import it.eng.spagobi.commons.dao.IRoleDAO;
+import it.eng.spagobi.commons.utilities.messages.MessageBuilder;
+import it.eng.spagobi.kpi.alarm.dao.ISbiAlarmContactDAO;
 import it.eng.spagobi.profiling.services.ManageRolesAction;
+import it.eng.spagobi.utilities.exceptions.SpagoBIServiceException;
 
 public class ManageContactsAction extends AbstractSpagoBIAction{
 	/**
@@ -38,9 +50,29 @@ public class ManageContactsAction extends AbstractSpagoBIAction{
 	private final String MESSAGE_DET = "MESSAGE_DET";
 	// type of service
 	private final String CONTACT_DETAIL = "CONTACT_DETAIL";
+	private final String CONTACTS_LIST = "CONTACTS_LIST";
 	@Override
 	public void doService() {
-		// TODO Auto-generated method stub
+		logger.debug("IN");
+		ISbiAlarmContactDAO contactDao;
+		try {
+			contactDao = DAOFactory.getAlarmContactDAO();
+		} catch (EMFUserError e1) {
+			logger.error(e1.getMessage(), e1);
+			throw new SpagoBIServiceException(SERVICE_NAME,	"Error occurred");
+		}
+		HttpServletRequest httpRequest = getHttpRequest();
+		MessageBuilder m = new MessageBuilder();
+		Locale locale = m.getLocale(httpRequest);
+
+		String serviceType = this.getAttributeAsString(MESSAGE_DET);
+		logger.debug("Service type "+serviceType);
+		if(serviceType != null && serviceType.equals(CONTACTS_LIST)){
+			
+		}else if(serviceType != null && serviceType.equals(CONTACT_DETAIL)){
+			
+		}
+		logger.debug("OUT");
 		
 	}
 

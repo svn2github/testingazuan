3ffd0ceb2923e98585a70d6a8808e74a4bff109b
giachino <giachino@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-03-01 13:21:10 +0000

    Modifies about export in CSV format: - modified default separator ';' (instead of ','). - modified extension file for CSV (.txt) so that the user can defines specific fields separator each time.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@10885 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIBirtReportEngine/src/it/eng/spagobi/engines/birt/BirtReportServlet.java
+++ V2.x/trunk/SpagoBIBirtReportEngine/src/it/eng/spagobi/engines/birt/BirtReportServlet.java
@@ -716,7 +716,8 @@ public class BirtReportServlet extends HttpServlet {
 	    OutputStream responseOut = response.getOutputStream();
 
 	    extractionOptions.setOutputFormat("csv");
-	    extractionOptions.setSeparator(",");
+	    //extractionOptions.setSeparator(",");
+	    extractionOptions.setSeparator(";");
 	    
 		
 	    //check if there is only a result set and generate one CSV file
@@ -727,7 +728,8 @@ public class BirtReportServlet extends HttpServlet {
 	    	
 	    	//Set the HTTP response
 			response.setContentType("text/csv");
-			response.setHeader("Content-disposition", "inline; filename=reportcsv.csv");
+			//response.setHeader("Content-disposition", "inline; filename=reportcsv.csv");
+			response.setHeader("Content-disposition", "inline; filename=reportcsv.txt");
 	    	
 	    	IResultSetItem resultItem = (IResultSetItem)resultSetList.get(0);
 			
@@ -772,7 +774,8 @@ public class BirtReportServlet extends HttpServlet {
 					logger.debug("Extraction successfull "+dispName);
 					 
 					// Add ZIP entry to ZIP output stream
-					outZip.putNextEntry(new ZipEntry("reportcsv"+i+".csv"));
+					//outZip.putNextEntry(new ZipEntry("reportcsv"+i+".csv"));
+					outZip.putNextEntry(new ZipEntry("reportcsv"+i+".txt"));
 					
 					//convert temp outputStream to InputStream
 					tempIn = new ByteArrayInputStream (((ByteArrayOutputStream) tempOut).toByteArray());

deb286102d245aef771356e0f86cdc1a237d9ed3
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-03-18 17:35:30 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@10999 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/utilities/engines/commonj/CmdExecWork.java
+++ V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/utilities/engines/commonj/CmdExecWork.java
@@ -3,6 +3,9 @@ package it.eng.spagobi.utilities.engines.commonj;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
 
 import sun.java2d.pipe.SpanIterator;
 
@@ -12,7 +15,13 @@ public class CmdExecWork extends SpagoBIWork {
 
 	String command;
 	String commandEnvironment;
-	String[] cmdParameters;
+
+	/** parameters  passed to command*/
+	Vector<String> cmdParameters;
+	/** to be added to classpath*/
+	Vector<String> classpathParameters;
+
+
 
 	public boolean isDaemon() {
 		// TODO Auto-generated method stub
@@ -27,6 +36,79 @@ public class CmdExecWork extends SpagoBIWork {
 		// TODO Auto-generated method stub
 
 	}
+
+
+	/** this method executes command followed by command parameters taken from template
+	 *  and by sbi parameters
+	 *  and add classpath variables followed by -cp
+	 * 
+	 * @param cmd
+	 * @param envFile
+	 * @return
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+
+	public int execCommand(String cmd, File envFile) throws InterruptedException, IOException {
+
+		File directoryExec = new File(commandEnvironment);
+
+		// build command
+		String commandToLaunch = command;
+
+		// add -cp 
+		if(classpathParameters.size()>0){
+			command += " -cp ";
+			for (Iterator iterator = classpathParameters.iterator(); iterator.hasNext();) {
+				String add = (String) iterator.next();
+				command += add;
+				if(iterator.hasNext()){
+					command += ";";
+				}
+			}
+		}
+
+		command += " ";
+
+		// add command parameters
+		for (Iterator iterator = cmdParameters.iterator(); iterator.hasNext();) {
+			String par = (String) iterator.next();			
+			command += par + " ";
+		}
+
+
+		// select sbi driver from MAP and add values!
+		for (Iterator iterator = analyticalParameters.iterator(); iterator.hasNext();) {
+			String url= (String) iterator.next();
+			if (sbiParameters.get(url) != null){
+				Object value = sbiParameters.get(url);
+				command += " " + url + "=" + value +" "; 
+			}
+		}
+
+
+		ProcessBuilder pb = new ProcessBuilder(command);
+		Map<String, String> env = pb.environment();
+
+		//env.put("Var1", "myValue");
+		//env.remove("OTHERVAR");
+		//env.put("VAR2", env.get("VAR1") + "suffix");
+
+		pb.directory(envFile);
+
+		//Process p = pb.start();
+
+		Process p = Runtime.getRuntime().exec(command, null, envFile);
+		
+		int exitValue = p.exitValue();
+		//p.exitValue();
+
+		return exitValue;
+
+	}
+
+
+	/*	
 
 	public int execCommand(String cmd, File envFile) throws InterruptedException, IOException {
 		int exitValue;
@@ -47,6 +129,7 @@ public class CmdExecWork extends SpagoBIWork {
 
 	}
 
+	 */
 
 	public String getCommand() {
 		return command;
@@ -64,12 +147,20 @@ public class CmdExecWork extends SpagoBIWork {
 		this.commandEnvironment = commandEnvironment;
 	}
 
-	public String[] getCmdParameters() {
+	public Vector<String> getCmdParameters() {
 		return cmdParameters;
 	}
 
-	public void setCmdParameters(String[] parameters) {
-		this.cmdParameters = parameters;
+	public void setCmdParameters(Vector<String> cmdParameters) {
+		this.cmdParameters = cmdParameters;
+	}
+
+	public Vector<String> getClasspathParameters() {
+		return classpathParameters;
+	}
+
+	public void setClasspathParameters(Vector<String> classpathParameters) {
+		this.classpathParameters = classpathParameters;
 	}
 
 
--- V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/utilities/engines/commonj/SpagoBIWork.java
+++ V2.x/trunk/SpagoBIUtils/src/it/eng/spagobi/utilities/engines/commonj/SpagoBIWork.java
@@ -2,15 +2,21 @@ package it.eng.spagobi.utilities.engines.commonj;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 import commonj.work.Work;
 
 public class SpagoBIWork implements Work{
 
+	String pid;
+
 	boolean running=false;
 
 	Map sbiParameters=new HashMap();
-	
+
+	// names of those in sbiParameters that are analytical drivers
+	Vector<String> analyticalParameters;
+
 	public boolean isDaemon() {
 		// TODO Auto-generated method stub
 		return false;
@@ -46,6 +52,26 @@ public class SpagoBIWork implements Work{
 		this.sbiParameters = sbiParameters;
 	}
 
+
+
+	public Vector<String> getAnalyticalParameters() {
+		return analyticalParameters;
+	}
+
+
+	public void setAnalyticalParameters(Vector<String> analyticalParameters) {
+		this.analyticalParameters = analyticalParameters;
+	}
+
+
+	public String getPid() {
+		return pid;
+	}
+
+
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
 
 
 

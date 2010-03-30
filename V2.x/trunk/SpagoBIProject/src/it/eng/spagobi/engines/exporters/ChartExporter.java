9359d8dc8a76370b69eef86802d5e7c26182f380
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-03-30 15:25:26 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@11067 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/exporters/ChartExporter.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/engines/exporters/ChartExporter.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 import com.lowagie.text.Cell;
 import com.lowagie.text.Document;
 import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
 import com.lowagie.text.Table;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -24,72 +25,85 @@ import com.lowagie.text.pdf.PdfWriter;
 public class ChartExporter
 {
 
-    private static transient Logger logger = Logger.getLogger("ChartExporter");
-    private static int MAX_NUM_IMG = 5;
-    private static String HORIZONTAL_ORIENTATION = "horizontal";
+	private static transient Logger logger = Logger.getLogger("ChartExporter");
+	private static int MAX_NUM_IMG = 5;
+	private static String HORIZONTAL_ORIENTATION = "horizontal";
 
+	public static final int MAX_WIDTH = 600;
+	public static final int MAX_HEIGHT = 1200;
 
-    public File getChartPDF(String uuid, boolean multichart, String orientation)
-        throws Exception
-    {
-        logger.debug("IN");
-  
-        File tmpFile;
+	public File getChartPDF(String uuid, boolean multichart, String orientation)
+	throws Exception
+	{
+		logger.debug("IN");
 
-        try{
-	        tmpFile = null;
-	        String dir = System.getProperty("java.io.tmpdir");
-	        String path = (new StringBuilder(String.valueOf(dir))).append("/").append(uuid).append(".png").toString();
-	        File dirF = new File(dir);
-	        tmpFile = File.createTempFile("tempPDFExport", ".pdf", dirF);
-	        Document pdfDocument = new Document();
-	        PdfWriter docWriter = PdfWriter.getInstance(pdfDocument, new FileOutputStream(tmpFile));
-	        pdfDocument.open();
-	        if(multichart)
-	        {
-	            List images = new ArrayList();
-	            for(int i = 0; i < MAX_NUM_IMG; i++)
-	            {
-	                String imgName = (new StringBuilder(String.valueOf(path.substring(0, path.indexOf(".png"))))).append(i).append(".png").toString();
-	                Image png = Image.getInstance(imgName);
-	                if(png == null)
-	                {
-	                    break;
-	                }
-	                images.add(png);
-	            }
-	
-	            Table table = new Table(images.size());
-	            for(int i = 0; i < images.size(); i++)
-	            {
-	                Image png = (Image)images.get(i);
-	                if(HORIZONTAL_ORIENTATION.equalsIgnoreCase(orientation))
-	                {
-	                    Cell pngCell = new Cell(png);
-	                    pngCell.setBorder(0);
-	                    table.setBorder(0);
-	                    table.addCell(pngCell);
-	                } else
-	                {
-	                    png.setAlignment(5);
-	                    pdfDocument.add(png);
-	                }
-	            }
-	
-	            pdfDocument.add(table);
-	        } else
-	        {
-	            Image jpg = Image.getInstance(path);
-	            pdfDocument.add(jpg);
-	        }
-	        pdfDocument.close();
-	        docWriter.close();
-	        
-	        logger.debug("OUT");
-	        
-	        return tmpFile;
-	        
-        } catch(Throwable e) {
+		File tmpFile;
+
+		try{
+			tmpFile = null;
+			String dir = System.getProperty("java.io.tmpdir");
+			String path = (new StringBuilder(String.valueOf(dir))).append("/").append(uuid).append(".png").toString();
+			File dirF = new File(dir);
+			tmpFile = File.createTempFile("tempPDFExport", ".pdf", dirF);
+			Document pdfDocument = new Document();
+			PdfWriter docWriter = PdfWriter.getInstance(pdfDocument, new FileOutputStream(tmpFile));
+			//pdfDocument.open();
+			if(multichart)
+			{
+				pdfDocument.open();
+
+				List images = new ArrayList();
+				for(int i = 0; i < MAX_NUM_IMG; i++)
+				{
+					String imgName = (new StringBuilder(String.valueOf(path.substring(0, path.indexOf(".png"))))).append(i).append(".png").toString();
+					Image png = Image.getInstance(imgName);
+					if(png == null)
+					{
+						break;
+					}
+					images.add(png);
+				}
+
+				Table table = new Table(images.size());
+				for(int i = 0; i < images.size(); i++)
+				{
+					Image png = (Image)images.get(i);
+					if(HORIZONTAL_ORIENTATION.equalsIgnoreCase(orientation))
+					{
+						Cell pngCell = new Cell(png);
+						pngCell.setBorder(0);
+						table.setBorder(0);
+						table.addCell(pngCell);
+					} else
+					{
+						png.setAlignment(5);
+						pdfDocument.add(png);
+					}
+				}
+
+				pdfDocument.add(table);
+			} else
+			{
+				Image jpg = Image.getInstance(path);
+				float height = jpg.getHeight();
+				float width = jpg.getWidth();
+
+				// if in need to change layout
+				if(width > MAX_WIDTH || height > MAX_HEIGHT){
+					changeLayout(pdfDocument, jpg, width, height);
+				}
+
+				pdfDocument.open();
+				pdfDocument.add(jpg);
+			}
+			pdfDocument.close();
+			docWriter.close();
+
+			logger.debug("OUT");
+
+			return tmpFile;
+
+		} catch(Throwable e) {
 			logger.error("An exception has occured", e);
 			throw new Exception(e);
 		} finally {
@@ -97,5 +111,47 @@ public class ChartExporter
 			//tmpFile.delete();
 
 		}
-    }
+	}
+
+	/** check if the image has to be turned or resize
+	 * 
+	 * @param document
+	 * @param jpg
+	 * @return if the image has been turned
+	 */
+
+	public void changeLayout(Document pdfDocument, Image jpg, float width, float height){
+		logger.debug("IN");
+		// if width is more than 600 and height is not more it suffices to turn the image of the chart
+		if(width > MAX_WIDTH && ! (height > MAX_WIDTH) && !(width > MAX_HEIGHT)){
+			pdfDocument.setPageSize(PageSize.LETTER.rotate());
+			logger.debug("pdf rotation");
+		} 
+		else{
+			// otherwise the chart needs to be scaled (if width > height also turn!)
+			if(width > height){
+				pdfDocument.setPageSize(PageSize.LETTER.rotate());
+				logger.debug("pdf rotation");
+			}
+			// calculate wich size is to reduce most:
+			float percentageToReduceHeight = 0;
+			float percentageToReduceWidth = 0;
+			if(height > MAX_HEIGHT) {
+				percentageToReduceHeight = (height * 100 ) / MAX_HEIGHT;
+			}
+			if(width > MAX_WIDTH) {
+				percentageToReduceWidth = (width * 100 ) / MAX_WIDTH;
+			}
+			float percToReduce = percentageToReduceHeight > percentageToReduceWidth ? percentageToReduceHeight : percentageToReduceWidth;
+			float percToScale = percToReduce - 100;
+			//						float newHeight = height - ((height/100)*percToReduce);
+//			float newWidth = width - ((width/100)*percToReduce);
+			logger.debug("pdf scale of percentage "+percToScale);
+			jpg.scalePercent(percToScale);
+		}
+		logger.debug("OUT");
+	}
+
+
+
 }

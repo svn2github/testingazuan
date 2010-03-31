45e0ed713cb218bcb2d127c79735a17e9ac6037b
bernabei <bernabei@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-03-31 09:38:14 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@11078 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBICommonJEngine/src/it/eng/spagobi/engines/commonj/exception/CommonjEngineException.java
+++ V2.x/trunk/SpagoBICommonJEngine/src/it/eng/spagobi/engines/commonj/exception/CommonjEngineException.java
@@ -1,5 +1,5 @@
 /**
-Copyright (c) 2005-2008, Engineering Ingegneria Informatica s.p.a.
+Copyright (c) 2005-2010, Engineering Ingegneria Informatica s.p.a.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
--- V2.x/trunk/SpagoBICommonJEngine/src/it/eng/spagobi/engines/commonj/runtime/WorkDeploymentDescriptor.java
+++ V2.x/trunk/SpagoBICommonJEngine/src/it/eng/spagobi/engines/commonj/runtime/WorkDeploymentDescriptor.java
@@ -1,17 +1,17 @@
 /**
-Copyright (c) 2005-2008, Engineering Ingegneria Informatica s.p.a.
+Copyright (c) 2005-2010, Engineering Ingegneria Informatica s.p.a.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice, this list of 
+ * Redistributions of source code must retain the above copyright notice, this list of 
       conditions and the following disclaimer.
-      
-    * Redistributions in binary form must reproduce the above copyright notice, this list of 
+
+ * Redistributions in binary form must reproduce the above copyright notice, this list of 
       conditions and the following disclaimer in the documentation and/or other materials 
       provided with the distribution.
-      
-    * Neither the name of the Engineering Ingegneria Informatica s.p.a. nor the names of its contributors may
+
+ * Neither the name of the Engineering Ingegneria Informatica s.p.a. nor the names of its contributors may
       be used to endorse or promote products derived from this software without specific
       prior written permission.
 
@@ -28,7 +28,7 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
-**/
+ **/
 package it.eng.spagobi.engines.commonj.runtime;
 
 import java.io.File;

7c16e3ee329410e62926ea18f28f6fc6a297c55f
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-04-08 12:28:57 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@11241 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/views/DocumentParametersView.java
+++ V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.documentcomposition/src/it/eng/spagobi/studio/documentcomposition/views/DocumentParametersView.java
@@ -20,23 +20,39 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  **/
 package it.eng.spagobi.studio.documentcomposition.views;
 
+import it.eng.spagobi.studio.core.log.SpagoBILogger;
+import it.eng.spagobi.studio.documentcomposition.Activator;
+import it.eng.spagobi.studio.documentcomposition.editors.DocumentCompositionEditor;
+import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.Document;
 import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.DocumentComposition;
+import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.Parameter;
+import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.Parameters;
 import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.metadata.MetadataDocument;
 import it.eng.spagobi.studio.documentcomposition.editors.model.documentcomposition.metadata.MetadataParameter;
+import it.eng.spagobi.studio.documentcomposition.util.DocCompUtilities;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Vector;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TableEditor;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.PartInitException;
@@ -54,6 +70,7 @@ public class DocumentParametersView extends ViewPart {
 	public static final int LABEL=1;
 	public static final int TYPE=2;
 	public static final int URLNAME=3;
+	public static final int DEFAULT_VALUE=4;
 
 
 	public void init(IViewSite site) throws PartInitException {
@@ -95,7 +112,7 @@ public class DocumentParametersView extends ViewPart {
 		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
 		data.heightHint = 200;
 		table.setLayoutData(data);
-		String[] titles = {"    Id    ","          Label          ", "          Type          ","          UrlName          "};
+		String[] titles = {"    Id    ","          Label          ", "          Type          ","          UrlName          ", "      Default Value     "};
 		for (int i=0; i<titles.length; i++) {
 			TableColumn column = new TableColumn (table, SWT.NONE);
 			column.setText (titles [i]);
@@ -103,27 +120,105 @@ public class DocumentParametersView extends ViewPart {
 		for (int i=0; i<titles.length; i++) {
 			table.getColumn (i).pack ();
 		}	
-		client.pack();
+
+		final TableEditor editor = new TableEditor(table);
+		// Add selection listener
+		table.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				// Clean up any previous editor control
+				Control oldEditor = editor.getEditor();
+				if (oldEditor != null) oldEditor.dispose();
+
+				// Identify the selected row
+				final TableItem item = (TableItem)e.item;
+				if (item == null) return;
+
+				// The control that will be the editor must be a child of the Table
+				Text newEditor = new Text(table, SWT.NONE);
+				newEditor.setText(item.getText(DEFAULT_VALUE));
+				newEditor.addModifyListener(new ModifyListener() {
+					public void modifyText(ModifyEvent me) {
+						Text text = (Text)editor.getEditor();
+						try{
+							editor.getItem().setText(DEFAULT_VALUE, text.getText());
+							String documentLabel = item.getData().toString();
+							String urlName = item.getText(URLNAME);
+							DocumentComposition docComposition=Activator.getDefault().getDocumentComposition();
+							Vector<Parameter> vectPars = retrieveParametersVectorFromDocumentLabel(docComposition, documentLabel);
+							String defaultVal = editor.getItem().getText(DEFAULT_VALUE);
+							
+							// get the parameter to insert the value
+							for (Iterator iterator = vectPars.iterator(); iterator.hasNext();) {
+								Parameter parameter = (Parameter) iterator.next();
+								if(parameter.getSbiParLabel().equals(urlName)){
+									parameter.setDefaultVal(defaultVal);
+								}
+
+							}
+							IEditorPart editorPart=DocCompUtilities.getEditorReference(DocCompUtilities.DOCUMENT_COMPOSITION_EDITOR_ID);
+							if(editorPart!=null) ((DocumentCompositionEditor)editorPart).setIsDirty(true);				
+						}
+						catch (Exception e) {
+							SpagoBILogger.warningLog("error in modifying default value");
+						}
+
+					}
+				});
+							newEditor.selectAll();
+							newEditor.setFocus();
+							editor.setEditor(newEditor, item, DEFAULT_VALUE);
+			}
+		});
 
 
-		toolkit.paintBordersFor(client);
-		section.setClient(client);
-		viewSelectedProperties();
-		setVisible(false);
+
+				client.pack();
+
+
+				toolkit.paintBordersFor(client);
+				section.setClient(client);
+				viewSelectedProperties();
+				setVisible(false);
 	}
 
 	public void reloadParametersProperties(MetadataDocument metadataDocument){
 		table.removeAll();
 		if(metadataDocument!=null){		
+
+			DocumentComposition docComposition=Activator.getDefault().getDocumentComposition();
+			// check if I can find a default value
+			Map<String, String> defaults = new HashMap<String, String>();
+			try{
+				String label = metadataDocument.getLabel();
+				Vector<Parameter> vectPars = retrieveParametersVectorFromDocumentLabel(docComposition, label);
+				// fill the map with default values
+				for (Iterator iterator = vectPars.iterator(); iterator.hasNext();) {
+					Parameter parameter = (Parameter) iterator.next();
+					defaults.put(parameter.getSbiParLabel(), parameter.getDefaultVal());
+				}
+
+			}
+			catch (Exception e) {
+				// exception proof cause next to release!
+				SpagoBILogger.errorLog("Error in recovering default value", null);
+			}
+
 			Vector<MetadataParameter> parameters=metadataDocument.getMetadataParameters();
 			if(parameters!=null){
 				for (Iterator iterator = parameters.iterator(); iterator.hasNext();) {
 					MetadataParameter metadataParameter = (MetadataParameter) iterator.next();
 					TableItem item = new TableItem (table, SWT.NONE);
+					// put as data the label f the document
+					item.setData(metadataDocument.getLabel());
 					item.setText (ID, metadataParameter.getId()!=null ? metadataParameter.getId().toString() : "");
 					item.setText (LABEL, metadataParameter.getLabel()!=null ? metadataParameter.getLabel() : "");
 					item.setText (TYPE, metadataParameter.getType()!=null ? metadataParameter.getType() : "");
 					item.setText (URLNAME, metadataParameter.getUrlName()!=null ? metadataParameter.getUrlName() : "");
+
+					// try searching for a default value
+					String def = defaults.get(metadataParameter.getUrlName());
+					item.setText (DEFAULT_VALUE, def != null ? def : "");
+
 				}
 			}
 		}
@@ -131,6 +226,22 @@ public class DocumentParametersView extends ViewPart {
 		client.redraw();
 		setVisible(true);
 	}
+
+	public Vector<Parameter> retrieveParametersVectorFromDocumentLabel(DocumentComposition docComposition, String label) throws Exception{
+		// get the document
+		Vector<Document> documents = docComposition.getDocumentsConfiguration().getDocuments();
+		Document actualDoc = null;
+		for (Iterator iterator = documents.iterator(); iterator.hasNext();) {
+			Document doc = (Document) iterator.next();
+			if (doc.getSbiObjLabel().equals(label)){
+				actualDoc = doc;						
+			}
+		}
+		Parameters parameters = actualDoc.getParameters();
+		Vector<Parameter> vectPars = parameters.getParameter();
+		return vectPars;
+	}
+
 
 
 	@Override

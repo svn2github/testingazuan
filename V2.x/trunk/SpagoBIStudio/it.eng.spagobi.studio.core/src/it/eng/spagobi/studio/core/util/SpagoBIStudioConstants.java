e081283b76ef0f78dba774ba6efaca7797ae5d58
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-04-12 11:46:01 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@11279 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/util/SpagoBIStudioConstants.java
+++ V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/util/SpagoBIStudioConstants.java
@@ -32,17 +32,17 @@ public class SpagoBIStudioConstants {
 	public final static String GEO_ENGINE_EXTENSION="sbigeo";
 
 	public final static String DASHBOARD_ENGINE_LABEL="DashboardInternalEng";
-	public final static String CHART_ENGINE_LABEL="ChartInternalEng";
+	public final static String CHART_ENGINE_LABEL="ChartEngine";
 	public final static String JASPER_REPORT_ENGINE_LABEL="JasperReportEngine";
 	public final static String BIRT_REPORT_ENGINE_LABEL="BirtEngine";
-	public final static String OLAP_ENGINE_LABEL="JPIVOT";
-	public final static String ETL_ENGINE_LABEL="TALEND";
+	public final static String OLAP_ENGINE_LABEL="JPivotEngine";
+	public final static String ETL_ENGINE_LABEL="TalendEngine";
 	public final static String DOSSIER_ENGINE_LABEL="Dossier";
-	public final static String DATA_MINING_ENGINE_LABEL="Weka";
-	public final static String QBE_ENGINE_LABEL="Qbe";
+	public final static String DATA_MINING_ENGINE_LABEL="WekaEngine";
+	public final static String QBE_ENGINE_LABEL="QbeEngine";
 	public final static String OFFICE_DOCUMENT_ENGINE_LABEL="OfficeInternalEng";
 	public final static String GEO_ENGINE_LABEL="GeoEngine";
-	public final static String DOCUMENT_COMPOSITION_ENGINE_LABEL="DocumentComposition";
+	public final static String DOCUMENT_COMPOSITION_ENGINE_LABEL="DocumentCompositionInternalEng";
 	
 	public final static int BIOBJECT_LABEL_LIMIT=30;
 	public final static int BIOBJECT_NAME_LIMIT=40;

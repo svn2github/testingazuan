f7f6ffa203d906bac014624a94d063328ac5b071
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-04-20 07:43:13 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@11308 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.chart/src/it/eng/spagobi/studio/chart/editors/model/chart/ChartModel.java
+++ V2.x/trunk/SpagoBIStudio/it.eng.spagobi.studio.chart/src/it/eng/spagobi/studio/chart/editors/model/chart/ChartModel.java
@@ -325,7 +325,7 @@ public class ChartModel {
 
 		SpagoBILogger.infoLog("Series personalization: labels, colors, draws");		
 		if(seriesDrawPersonalization){
-			drawPersonalization="<SERIES_DRAWS ";
+			drawPersonalization="<SERIES_DRAW ";
 			for (Iterator iterator = seriesPersonalizationHashMap.keySet().iterator(); iterator.hasNext();) {
 				String serName = (String) iterator.next();
 				SeriePersonalization serPers=seriesPersonalizationHashMap.get(serName);

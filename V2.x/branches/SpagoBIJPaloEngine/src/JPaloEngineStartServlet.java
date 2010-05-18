be74c354e8ff3631c832a27b873972e94e8b2381
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-05-18 08:34:01 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@11446 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/branches/SpagoBIJPaloEngine/src/JPaloEngineStartServlet.java
+++ V2.x/branches/SpagoBIJPaloEngine/src/JPaloEngineStartServlet.java
@@ -1,11 +1,22 @@
 
 
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
+import sun.misc.BASE64Decoder;
+
+import it.eng.spago.base.SourceBean;
+import it.eng.spago.security.IEngUserProfile;
+import it.eng.spagobi.services.content.bo.Content;
+import it.eng.spagobi.services.proxy.ContentServiceProxy;
 import it.eng.spagobi.utilities.engines.AbstractEngineStartServlet;
 import it.eng.spagobi.utilities.engines.EngineStartServletIOManager;
 import it.eng.spagobi.utilities.engines.SpagoBIEngineException;
@@ -17,6 +28,7 @@ import it.eng.spagobi.utilities.engines.SpagoBIEngineException;
 public class JPaloEngineStartServlet extends AbstractEngineStartServlet {
 	
 	private static final String PALO_BASE_URL = "SpagoBIJPaloEngine.html";
+	private static String DOCUMENT_ID="document";
 	
 	/**
      * Logger component
@@ -33,21 +45,39 @@ public class JPaloEngineStartServlet extends AbstractEngineStartServlet {
 
    
     public void doService( EngineStartServletIOManager servletIOManager ) throws SpagoBIEngineException {
-    	//JPaloEngineTemplate template;
+    	JPaloEngineTemplate template = null;
     	String jpaloUrl;
 
     	logger.debug("IN");
 		
 		try {		
-		
+			HttpSession session = servletIOManager.getHttpSession();
+			IEngUserProfile profile = (IEngUserProfile) session.getAttribute(IEngUserProfile.ENG_USER_PROFILE);
+			ContentServiceProxy contentProxy = new ContentServiceProxy((String)profile.getUserUniqueIdentifier(),session);
+			String documentId = (String) servletIOManager.getRequest().getParameter(DOCUMENT_ID);
+			Content templateContent = contentProxy.readTemplate(documentId,new HashMap());
+
+			byte[] byteContent = null;
+			try {
+				BASE64Decoder bASE64Decoder = new BASE64Decoder();
+				byteContent = bASE64Decoder.decodeBuffer(templateContent.getContent());
+				System.out.println("metodo 1::"+byteContent.length);
+				String xmlSourceBean = new String(byteContent);
+				System.out.println(xmlSourceBean);
+				SourceBean sb =SourceBean.fromXMLString(xmlSourceBean);
+				template = new JPaloEngineTemplate(sb);
+				System.out.println(template.getViewName());
+			}catch (Throwable t){
+				logger.warn("Error on decompile",t); 
+			}
 			//template = new JPaloEngineTemplate( servletIOManager.getTemplateAsSourceBean() );			
 			//http://localhost:8888/com.tensegrity.wpalo.SpagoBIJPaloEngine/SpagoBIJPaloEngine.html?options=user="admin";pass="ISMvKXpXpadDiUoOSoAfww==";openview="Sales"
 	    	jpaloUrl = PALO_BASE_URL+"?options=";	
 
 			jpaloUrl += "user=\"admin\";pass=\"ISMvKXpXpadDiUoOSoAfww==\"";
 			jpaloUrl += ";openview=\"";
-			//jpaloUrl += template.getViewName();
-			jpaloUrl += "Sales";
+			jpaloUrl += template.getViewName();
+			//jpaloUrl += "Sales";
 			jpaloUrl += "\"";
 			jpaloUrl += ",hidenavigator";
 			jpaloUrl += ",hideviewtabs";

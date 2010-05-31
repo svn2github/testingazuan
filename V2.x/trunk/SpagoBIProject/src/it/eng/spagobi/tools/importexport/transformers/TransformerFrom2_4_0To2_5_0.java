014630413e0ff9bcff4008d677856886ddc86ceb
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-05-31 10:45:53 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@11564 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/importexport/transformers/TransformerFrom2_4_0To2_5_0.java
+++ V2.x/trunk/SpagoBIProject/src/it/eng/spagobi/tools/importexport/transformers/TransformerFrom2_4_0To2_5_0.java
@@ -62,10 +62,10 @@ public class TransformerFrom2_4_0To2_5_0 implements ITransformer {
 		Connection conn = null;
 		try {
 			conn = TransformersUtilities.getConnectionToDatabase(pathImpTmpFolder, archiveName);
+			fixDatasets(conn);
 			fixKpiModel(conn);
 			fixModelInstance(conn);
 			fixThresholdValue(conn);
-			fixDatasets(conn);
 			conn.commit();
 		} catch (Exception e) {
 			logger.error("Error while changing database", e);	
@@ -90,21 +90,26 @@ public class TransformerFrom2_4_0To2_5_0 implements ITransformer {
 	 */
 	private void fixKpiModel(Connection conn) throws Exception {
 		logger.debug("IN");
-		
-		Statement stmt = conn.createStatement();
 
+		Statement stmt = conn.createStatement();
+		String sql =  "";
 		// ALTER TABLE SBI_OBJECT_NOTES ADD COLUMN OWNER VARCHAR(50);
-		String sql =  "ALTER TABLE SBI_KPI_MODEL ADD COLUMN KPI_MODEL_LBL VARCHAR(1000)";
-		stmt.execute(sql);
+		try{
+			sql =  "ALTER TABLE SBI_KPI_MODEL ADD COLUMN KPI_MODEL_LBL VARCHAR(1000)";
+			stmt.execute(sql);
+		}
+		catch (Exception e) {
+			logger.error("Error adding column: if add column fails may mean that column already esists; means you ar enot using an exact version spagobi DB",e);	
+		}
 		//sql =  "UPDATE SBI_KPI_MODEL SET KPI_MODEL_LBL=KPI_MODEL_CD WHERE KPI_MODEL_LBL IS NULL";
-		sql =  "UPDATE SBI_KPI_MODEL SET KPI_MODEL_LBL=KPI_MODEL_CD";
-		stmt.executeUpdate(sql);
+		//sql =  "UPDATE SBI_KPI_MODEL SET KPI_MODEL_LBL=KPI_MODEL_CD";
+		//stmt.executeUpdate(sql);
 
 		logger.debug("OUT");
 	}
-	
-	
-	
+
+
+
 	/*
 	 * Adjust ModelInstance Table
 	 * 
@@ -113,17 +118,24 @@ public class TransformerFrom2_4_0To2_5_0 implements ITransformer {
 	 */
 	private void fixModelInstance(Connection conn) throws Exception {
 		logger.debug("IN");
-		
+
 		Statement stmt = conn.createStatement();
 
 		// ALTER TABLE SBI_DATA_SET ADD COLUMN DS_METADATA VARCHAR(2000) DEFAULT NULL;
-		String sql =  "ALTER TABLE SBI_KPI_MODEL_INST ADD COLUMN MODELUUID VARCHAR(400) DEFAULT NULL;";
-		stmt.execute(sql);
+		String sql =  "";
+		try{
+			sql = "ALTER TABLE SBI_KPI_MODEL_INST ADD COLUMN MODELUUID VARCHAR(400) DEFAULT NULL;";
+			stmt.execute(sql);
+		}
+		catch (Exception e) {
+			logger.error("Error adding column: if add column fails may mean that column already esists; means you ar enot using an exact version spagobi DB",e);	
+		}
+
 		sql =  "UPDATE SBI_KPI_MODEL_INST SET MODELUUID=NULL";
 		stmt.executeUpdate(sql);
 		logger.debug("OUT");
 	}
-	
+
 	/*
 	 * Adjust DataSet Table
 	 * 
@@ -134,13 +146,20 @@ public class TransformerFrom2_4_0To2_5_0 implements ITransformer {
 		logger.debug("IN");
 		try {
 			Statement stmt = conn.createStatement();
-	
+
 			// ALTER TABLE SBI_DATA_SET ADD COLUMN DS_METADATA VARCHAR(2000) DEFAULT NULL;
-			String sql =  "ALTER TABLE SBI_DATA_SET ADD COLUMN DS_METADATA VARCHAR(2000) DEFAULT NULL;";
-			stmt.execute(sql);
+			String sql =  "";
+			try{
+				sql = "ALTER TABLE SBI_DATA_SET ADD COLUMN DS_METADATA VARCHAR(2000) DEFAULT NULL;";
+				stmt.execute(sql);
+			}
+			catch (Exception e) {
+				logger.error("Error adding column: if add column fails may mean that column already esists; means you ar enot using an exact version spagobi DB",e);	
+			}
+
 			sql =  "UPDATE SBI_DATA_SET SET DS_METADATA=NULL";
 			stmt.executeUpdate(sql);
-			
+
 		} catch (SQLException e) {
 			logger.error("Already existent column");
 			e.printStackTrace();
@@ -159,21 +178,28 @@ public class TransformerFrom2_4_0To2_5_0 implements ITransformer {
 	private void fixThresholdValue(Connection conn) throws Exception {
 		logger.debug("IN");
 		Statement stmt = conn.createStatement();
-		
-		String sql =  "ALTER TABLE SBI_THRESHOLD_VALUE ADD COLUMN MIN_CLOSED BOOLEAN DEFAULT FALSE";
-		stmt.execute(sql);
-		sql =  "UPDATE SBI_THRESHOLD_VALUE SET MIN_CLOSED=FALSE";
-		stmt.executeUpdate(sql);
 
-		sql =  "ALTER TABLE SBI_THRESHOLD_VALUE ADD COLUMN MAX_CLOSED BOOLEAN DEFAULT FALSE";
-		stmt.execute(sql);
-		sql =  "UPDATE SBI_THRESHOLD_VALUE SET MAX_CLOSED=FALSE";
-		stmt.executeUpdate(sql);
+		String sql =  "";
+		try{
+			sql = "ALTER TABLE SBI_THRESHOLD_VALUE ADD COLUMN MIN_CLOSED BOOLEAN DEFAULT FALSE";
+			stmt.execute(sql);
 
-		sql =  "ALTER TABLE SBI_THRESHOLD_VALUE ADD COLUMN TH_VALUE BOOLEAN DEFAULT NULL";
-		stmt.execute(sql);
-		sql =  "UPDATE SBI_THRESHOLD_VALUE SET TH_VALUE=FALSE";
-		stmt.executeUpdate(sql);
+			sql =  "UPDATE SBI_THRESHOLD_VALUE SET MIN_CLOSED=FALSE";
+			stmt.executeUpdate(sql);
+
+			sql =  "ALTER TABLE SBI_THRESHOLD_VALUE ADD COLUMN MAX_CLOSED BOOLEAN DEFAULT FALSE";
+			stmt.execute(sql);
+			sql =  "UPDATE SBI_THRESHOLD_VALUE SET MAX_CLOSED=FALSE";
+			stmt.executeUpdate(sql);
+
+			sql =  "ALTER TABLE SBI_THRESHOLD_VALUE ADD COLUMN TH_VALUE BOOLEAN DEFAULT NULL";
+			stmt.execute(sql);
+			sql =  "UPDATE SBI_THRESHOLD_VALUE SET TH_VALUE=FALSE";
+			stmt.executeUpdate(sql);
+		}
+		catch (Exception e) {
+			logger.error("Error adding column: if add column fails may mean that column already esists; means you ar enot using an exact version spagobi DB",e);	
+		}
 
 		logger.debug("OUT");
 	}

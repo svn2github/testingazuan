715ca750b5823802b95982db8f7780bc2c8f197e
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-07-16 14:15:48 +0000

    Bug fix: saving calculated fields into a multi-datamart document now works
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@11885 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V2.x/trunk/SpagoBIQbeEngine/src/it/eng/spagobi/engines/qbe/services/SaveTreeAction.java
+++ V2.x/trunk/SpagoBIQbeEngine/src/it/eng/spagobi/engines/qbe/services/SaveTreeAction.java
@@ -21,7 +21,11 @@
 package it.eng.spagobi.engines.qbe.services;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
@@ -29,6 +33,8 @@ import org.json.JSONObject;
 import it.eng.qbe.dao.DAOFactory;
 import it.eng.qbe.dao.ICalculatedFieldsDAO;
 import it.eng.qbe.model.structure.DataMartCalculatedField;
+import it.eng.qbe.model.structure.DataMartEntity;
+import it.eng.qbe.model.structure.DataMartModelStructure;
 import it.eng.qbe.query.serializer.SerializationConstants;
 import it.eng.spago.base.SourceBean;
 import it.eng.spagobi.utilities.assertion.Assert;
@@ -79,9 +85,21 @@ public class SaveTreeAction extends AbstractQbeEngineAction {
 			calculatedFields = getDatamartModel().getDataMartModelStructure().getCalculatedFields();
 			Assert.assertNotNull(calculatedFields, "Calculated field map cannot be null in order to execute " + this.getActionName() + " service");
 			
-			logger.debug("Saving calculated fields...");
-			calculatedFieldsDAO.saveCalculatedFields(getDatamartModel().getName(), calculatedFields);
-			logger.debug("Calculated fileds saved succesfully");
+			List datamartsName = getDatamartModel().getDataSource().getDatamartNames();
+			if (datamartsName.size() == 1) {
+				String datamartName = (String) datamartsName.get(0);
+				logger.debug("Saving calculated fields into datamart [" + datamartName + "] ...");
+				calculatedFieldsDAO.saveCalculatedFields(datamartName, calculatedFields);
+				logger.debug("Calculated fileds saved succesfully into datamart [" + datamartName + "].");
+			} else {
+				for (int i = 0; i < datamartsName.size(); i++) {
+					String datamartName = (String) datamartsName.get(i);
+					Map datamartCalcultedField = getCalculatedFieldsForDatamart(getDatamartModel().getDataMartModelStructure(), calculatedFields, datamartName);
+					logger.debug("Saving calculated fields into datamart [" + datamartName + "]...");
+					calculatedFieldsDAO.saveCalculatedFields(datamartName, datamartCalcultedField);
+					logger.debug("Calculated fileds saved succesfully into datamart [" + datamartName + "].");
+				}
+			}
 			
 			try {
 				writeBackToClient( new JSONAcknowledge() );
@@ -95,6 +113,31 @@ public class SaveTreeAction extends AbstractQbeEngineAction {
 		} finally {
 			logger.debug("OUT");
 		}			
+	}
+
+
+	/**
+	 * The input map contains all the calculated fields defined into the entire datamart model structure. 
+	 * This method returns the calculated field defined for a single datamart (used in case of composite datasource, i.e. more than 1 datamart).
+	 * @param dataMartModelStructure The datamart model structure
+	 * @param calculatedFields All the calculated fields defined into the entire datamart model structure
+	 * @param datamartName The datamart for which the calculated fields should be retrieved
+	 * @return the calculated field defined for the specified datamart 
+	 */
+	private Map getCalculatedFieldsForDatamart(DataMartModelStructure dataMartModelStructure, Map calculatedFields, String datamartName) {
+		Map toReturn = new HashMap();
+		Set keys = calculatedFields.keySet();
+		Iterator keysIt = keys.iterator();
+		while (keysIt.hasNext()) {
+			String entityUniqueName = (String) keysIt.next();
+			DataMartEntity dataMartEntity = dataMartModelStructure.getEntity(entityUniqueName);
+			DataMartEntity dataMartRootEntity = dataMartEntity.getRoot();
+			List rootEntities = dataMartModelStructure.getRootEntities(datamartName);
+			if (rootEntities.contains(dataMartRootEntity)) {
+				toReturn.put(entityUniqueName, calculatedFields.get(entityUniqueName));
+			}
+		}
+		return toReturn;
 	}
 
 

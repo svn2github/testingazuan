401387b73442af4971e7d567259029938bdd8dd9
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-09-08 16:27:28 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@12308 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_2.x/Meta/trunk/SpagoBIMetaData/src/it/eng/spagobi/meta/model/physical/PhysicalPrimaryKey.java
+++ V_2.x/Meta/trunk/SpagoBIMetaData/src/it/eng/spagobi/meta/model/physical/PhysicalPrimaryKey.java
@@ -11,44 +11,28 @@ public interface PhysicalPrimaryKey {
 	 * @model
 	 */
 	public abstract String getPkName();
-
-	/**
-	 * @model
-	 */
 	public abstract void setPkName(String pkName);
 
 	public abstract String getTableName();
-
 	public abstract void setTableName(String tableName);
 
 	/**
 	 * @model
 	 */
 	public abstract PhysicalTable getTable();
-
-	/**
-	 * @model
-	 */
 	public abstract void setTable(PhysicalTable table);
 
 	public abstract List<String> getColumnNames();
-
 	public abstract void addColumnNames(List<String> columnNames);
-
 	public abstract void addColumnName(String columnName);
 
 	/**
 	 * @model
 	 */
 	public abstract List<PhysicalColumn> getColumns();
-	
-	/**
-	 * @model
-	 */
 	public abstract void setColumns(List<PhysicalColumn> columns);
 
 	public abstract void addColumns(List<PhysicalColumn> columns);
-
 	public abstract void addColumn(PhysicalColumn column);
 
 }
--- V_2.x/Meta/trunk/SpagoBIMetaData/src/it/eng/spagobi/meta/model/physical/PhysicalTable.java
+++ V_2.x/Meta/trunk/SpagoBIMetaData/src/it/eng/spagobi/meta/model/physical/PhysicalTable.java
@@ -11,27 +11,18 @@ public interface PhysicalTable {
 	 * @model
 	 */
 	public abstract String getName();
-
-	/**
-	 * @model
-	 */
 	public abstract void setName(String name);
 
 	/**
 	 * @model
 	 */
 	public abstract PhysicalModel getModel();
-
-	/**
-	 * @model
-	 */
 	public abstract void setModel(PhysicalModel model);
 
 	
 	public abstract String getCatalog();
 	public abstract void setCatalog(String catalog);
 
-	
 	public abstract String getSchema();
 	public abstract void setSchema(String schema);
 
@@ -39,71 +30,45 @@ public interface PhysicalTable {
 	 * @model
 	 */
 	public abstract String getComment();
-
-	/**
-	 * @model
-	 */
 	public abstract void setComment(String comment);
 
 	/**
 	 * @model
 	 */
 	public abstract String getType();
-
-	/**
-	 * @model
-	 */
 	public abstract void setType(String type);
 
 	/**
 	 * @model
 	 */
 	public abstract List<PhysicalColumn> getColumns();
-
-	/**
-	 * @model
-	 */
 	public abstract void setColumns(List<PhysicalColumn> columns);
 
-	
 	public abstract void addColumn(PhysicalColumn column);
-
 	public abstract PhysicalColumn getColumn(String columnName);
 
 	
 	/**
 	 * @model
 	 */
-	public abstract void setPrimaryKeys(List<PhysicalPrimaryKey> primaryKeys);
-	
-	/**
-	 * @model
-	 */
 	public abstract List<PhysicalPrimaryKey> getPrimaryKeys();
+	public abstract void setPrimaryKeys(List<PhysicalPrimaryKey> primaryKeys);
 	
 	public abstract void addPrimaryKey(PhysicalPrimaryKey primaryKey);
 
 	/**
 	 * @model
 	 */
-	public abstract void setForeignKeys(List<PhysicalForeignKey> foreignKeys);
-	
-	/**
-	 * @model
-	 */
 	public abstract List<PhysicalForeignKey> getForeignKeys();
+	public abstract void setForeignKeys(List<PhysicalForeignKey> foreignKeys);
 	
 	public abstract void addForeignKey(PhysicalForeignKey foreignKey);
 
 	/**
 	 * @model
 	 */
-	public abstract void setIncomingKeys(List<PhysicalForeignKey> foreignKeys);
-	
-	/**
-	 * @model
-	 */
 	public abstract List<PhysicalForeignKey> getIncomingKeys();
+	public abstract void setIncomingKeys(List<PhysicalForeignKey> foreignKeys);
 	
 	public abstract void addIncomingKey(PhysicalForeignKey foreignKey);
 

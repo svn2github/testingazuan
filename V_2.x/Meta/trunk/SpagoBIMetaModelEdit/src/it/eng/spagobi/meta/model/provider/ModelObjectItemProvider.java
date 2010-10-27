77e34db84380dcbb0fb5f9eabbecbe64c8510d7d
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-10-27 10:29:11 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@12825 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_2.x/Meta/trunk/SpagoBIMetaModelEdit/src/it/eng/spagobi/meta/model/provider/ModelObjectItemProvider.java
+++ V_2.x/Meta/trunk/SpagoBIMetaModelEdit/src/it/eng/spagobi/meta/model/provider/ModelObjectItemProvider.java
@@ -41,13 +41,15 @@ import org.eclipse.emf.edit.provider.ViewerNotification;
  * @generated
  */
 public class ModelObjectItemProvider
-	extends ItemProviderAdapter
+	extends 
+		ItemProviderAdapter
 	implements
 		IEditingDomainItemProvider,
 		IStructuredItemContentProvider,
 		ITreeItemContentProvider,
 		IItemLabelProvider,
 		IItemPropertySource {
+	
 	/**
 	 * This constructs an instance from a factory and a notifier.
 	 * <!-- begin-user-doc -->

b42bbedf38a60af979ac577c30d64cdfc7d0de7e
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2010-11-12 14:06:26 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@12945 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_2.x/Meta/trunk/SpagoBIMetaModelEdit/src/it/eng/spagobi/meta/model/business/commands/AddBusinessTableCommand.java
+++ V_2.x/Meta/trunk/SpagoBIMetaModelEdit/src/it/eng/spagobi/meta/model/business/commands/AddBusinessTableCommand.java
@@ -21,11 +21,16 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 package it.eng.spagobi.meta.model.business.commands;
 
+import it.eng.spagobi.meta.commons.IModelObjectFilter;
 import it.eng.spagobi.meta.initializer.BusinessModelInitializer;
 import it.eng.spagobi.meta.initializer.BusinessRelationshipDescriptor;
+import it.eng.spagobi.meta.model.ModelObject;
 import it.eng.spagobi.meta.model.business.BusinessColumn;
+import it.eng.spagobi.meta.model.business.BusinessModel;
 import it.eng.spagobi.meta.model.business.BusinessTable;
 import it.eng.spagobi.meta.model.physical.PhysicalColumn;
+import it.eng.spagobi.meta.model.physical.PhysicalModel;
+import it.eng.spagobi.meta.model.physical.PhysicalTable;
 import it.eng.spagobi.meta.model.provider.SpagoBIMetalModelEditPlugin;
 
 import java.util.ArrayList;
@@ -41,6 +46,7 @@ import org.eclipse.emf.edit.domain.EditingDomain;
  */
 public class AddBusinessTableCommand extends AbstractSpagoBIModelCommand {
 
+	private BusinessTable addedBusinessTable;
 	
 	public AddBusinessTableCommand(EditingDomain domain, CommandParameter parameter) {
 		super("Business Table", "Add business table", domain, parameter);
@@ -52,22 +58,50 @@ public class AddBusinessTableCommand extends AbstractSpagoBIModelCommand {
 	
 	@Override
 	public void execute() {
-		// do something here
-		
-		System.err.println("COMMAND [AddTableCommand] SUCCESFULLY EXECUTED: ");
-		
-		this.executed = true;
+		if (parameter.getValue() instanceof Collection){
+			//parameter is a collection of selected column list
+			
+			//Create Business Table from a Physical Table with column filter
+			BusinessModel businessModel = (BusinessModel)parameter.getOwner();
+			Collection<PhysicalColumn> selectedColumns = (Collection)parameter.getValue();
+			//getting PhysicalTable reference
+			PhysicalColumn physicalColum =((PhysicalColumn)(selectedColumns.toArray()[0]));
+			PhysicalTable physicalTable = physicalColum.getTable();
+			BusinessModelInitializer initializer = new BusinessModelInitializer();
+			addedBusinessTable = initializer.addTable(physicalTable, new PhysicalColumnFilter(selectedColumns), businessModel, true);	
+			
+			System.err.println("COMMAND [AddTableCommand] SUCCESFULLY EXECUTED: ");
+			
+			this.executed = true;
+		}
+		else if (parameter.getValue() instanceof String){
+			//parameter is a String with Physical Table name to import as Business Table
+			
+			String tableName = (String)parameter.getValue();
+			BusinessModel businessModel = (BusinessModel)parameter.getOwner();
+			PhysicalModel physicalModel = businessModel.getPhysicalModel();
+			PhysicalTable physicalTable = physicalModel.getTable(tableName);
+			BusinessModelInitializer initializer = new BusinessModelInitializer();
+			addedBusinessTable = initializer.addTable(physicalTable, businessModel, true);
+
+			System.err.println("COMMAND [AddTableCommand] SUCCESFULLY EXECUTED: ");
+			
+			this.executed = true;
+		}
+
 	}
 	
 	
 	@Override
 	public void undo() {
-		
+		BusinessModel businessModel = (BusinessModel)parameter.getOwner();
+		businessModel.getTables().remove(addedBusinessTable);
 	}
 
 	@Override
 	public void redo() {
-			
+		BusinessModel businessModel = (BusinessModel)parameter.getOwner();
+		businessModel.getTables().add(addedBusinessTable);
 	}
 	
 	@Override
@@ -75,6 +109,28 @@ public class AddBusinessTableCommand extends AbstractSpagoBIModelCommand {
 		return SpagoBIMetalModelEditPlugin.INSTANCE.getImage("full/obj16/BusinessTable");
 	}
 
+//-------------------------------------------------------------------------
+//	Inner Classes
+//-------------------------------------------------------------------------
 	
+	/*
+	 * Inner class that implements IModelObjectFilter
+	 */
+	private class PhysicalColumnFilter implements IModelObjectFilter{
+
+		Collection<PhysicalColumn> columnsTrue;
+		public PhysicalColumnFilter(Collection<PhysicalColumn> columnsToMantain){
+			columnsTrue = columnsToMantain;
+		}
+		@Override
+		public boolean filter(ModelObject o) {
+			if (columnsTrue.contains((PhysicalColumn)o))
+				return false;
+			else
+				return true;
+		}		
+	}
+
+//-------------------------------------------------------------------------
 
 }

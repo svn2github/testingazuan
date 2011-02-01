39e2af6ac501b4dd2838bed1d3d9c2eff5cbe4e7
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-02-01 14:58:18 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@13291 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_2.x/Meta/branches/TestEditor/src/it/eng/spagobi/meta/model/business/wizards/AddBusinessRelationshipWizardPage.java
+++ V_2.x/Meta/branches/TestEditor/src/it/eng/spagobi/meta/model/business/wizards/AddBusinessRelationshipWizardPage.java
@@ -22,19 +22,15 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Text;
 
 
+
 public class AddBusinessRelationshipWizardPage extends WizardPage {
-	
-	private Text relationshipNameInputField;
-	private Combo sourceTableCombo;
-	private Combo destinationTableCombo;
-	private List sourceColumnList;
-	private List destinationColumnList;
-	private List columnCorrelationList;
 	private int cardinality;
 	
 	java.util.List<BusinessColumn> sourceColumns;
@@ -48,6 +44,10 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
 	private BusinessModel model;
 	private BusinessColumnSet sourceTable;
 	private BusinessColumnSet destinationTable;
+	private Text relationshipNameInputField;
+	private Combo sourceTableCombo, destinationTableCombo;
+	private List sourceColumnList, columnCorrelationList, destinationColumnList ;
+	private boolean nameInserted;
 	
 	protected AddBusinessRelationshipWizardPage(String pageName, BusinessModel model, BusinessColumnSet sourceTable, BusinessColumnSet destinationTable) {
 		super(pageName);
@@ -66,16 +66,23 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
 
 	@Override
 	public void createControl(Composite parent) {
-
-		Composite mainComposite = new Composite(parent, SWT.NULL);
+ 		Composite mainComposite = new Composite (parent, SWT.NULL);
  		setControl(mainComposite);
- 		mainComposite.setLayout(new GridLayout(1, false));
+ 		mainComposite.setLayout(new FillLayout(SWT.HORIZONTAL));
  		
- 		initRelationshipNameGroup(mainComposite, SWT.NONE);
- 		initCorrelationGroup(mainComposite, SWT.NONE); 		
- 		initCardinalityGroup(mainComposite, SWT.NONE);
- 		initCorrelationSummaryGroup(mainComposite, SWT.NONE);
+ 		Composite innerComposite = new Composite(mainComposite, SWT.NONE);
+ 		innerComposite.setLayout(new GridLayout(1, false));
  		
+ 		//Create Relationship Name Group
+ 		initRelationshipNameGroup(innerComposite,SWT.NONE);
+ 		
+ 		//Create Columns Group
+ 		initCorrelationGroup(innerComposite,SWT.NONE);
+ 		
+ 		//Create Correlation Summary Group (cardinality + defined correlation)
+ 		initCorrelationSummaryGroup(innerComposite, SWT.NONE);
+ 		
+
  		if(selectTable(sourceTableCombo, sourceTable, true)) {
  			populateSourceColumnList(sourceTable);
  		}
@@ -102,19 +109,27 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
 	}
 	
 	private void initRelationshipNameGroup(Composite parent, int style) {
-		Composite composite_name = new Composite(parent,style);
+ 		Composite composite_name = new Composite(parent, style);
  		composite_name.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
- 		composite_name.setLayout(new GridLayout(2, false)); 		
+ 		composite_name.setLayout(new GridLayout(2, false));
+ 		
  		Label lblBusinessRelationshipName = new Label(composite_name, SWT.NONE);
  		lblBusinessRelationshipName.setText("Business Relationship Name");
  		relationshipNameInputField = new Text(composite_name, SWT.BORDER);
- 		relationshipNameInputField.setText("br name");
  		relationshipNameInputField.setToolTipText("Name to assign to the Business Relationship");
- 		relationshipNameInputField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+ 		relationshipNameInputField.setText("business relationship name");
+ 		relationshipNameInputField.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+ 		relationshipNameInputField.addModifyListener(new ModifyListener(){
+			public void modifyText(ModifyEvent me){
+				if (relationshipNameInputField.isFocusControl()){
+					nameInserted = true;
+				}
+			}
+		});
 	}
 	
 	private void initCorrelationGroup(Composite parent, int style) {
-		Composite columnCorrelationGroup = new Composite(parent, style);
+ 		Composite columnCorrelationGroup = new Composite(parent, style);
  		columnCorrelationGroup.setLayout(new FillLayout(SWT.HORIZONTAL));
  		columnCorrelationGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
  		initSourceGroup(columnCorrelationGroup, SWT.NONE);
@@ -122,19 +137,75 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
 	}
 	
 	public void initCardinalityGroup(Composite parent, int style){
-		Composite cardinalityGroup = new Composite(parent, style);
-		cardinalityGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
-		cardinalityGroup.setLayout(new GridLayout(3, false));
-		
-		Button btnAddRelationship = new Button(cardinalityGroup, SWT.NONE);
+ 		Composite cardinalityGroup = new Composite(parent, style);
+ 		cardinalityGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+ 		//cardinalityGroup.setSize(279, 52);
+ 		cardinalityGroup.setLayout(new GridLayout(4, false));
+ 		
+ 		Label lblCardinality = new Label(cardinalityGroup, SWT.NONE);
+ 		lblCardinality.setText("Cardinality");
+ 		lblCardinality.setLocation(0, 0);
+ 		
+ 		Composite compGroup = new Composite(cardinalityGroup, SWT.NONE);
+ 		compGroup.setLayout(new FillLayout(SWT.HORIZONTAL));
+ 		compGroup.setLayoutData(new GridData(SWT.LEFT, SWT.FILL, true, false, 1, 1));
+ 		//compGroup.setBounds(0, 0, 64, 64);
+ 		
+ 		Button button1to1 = new Button(compGroup, SWT.RADIO);
+ 		button1to1.setText("1 to 1");
+ 		button1to1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+					cardinality = ONE_TO_ONE;
+			}
+		});
+ 		
+ 		Button button1toN = new Button(compGroup, SWT.RADIO);
+ 		button1toN.setText("1 to N");
+ 		button1toN.setSelection(true);
+ 		button1toN.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				cardinality = ONE_TO_MANY;
+			}
+		});
+ 		
+ 		Button buttonNto1 = new Button(compGroup, SWT.RADIO);
+ 		buttonNto1.setText("N to 1");
+ 		buttonNto1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				cardinality = MANY_TO_ONE;
+			}
+		});
+ 		
+ 		Button buttonNtoN = new Button(compGroup, SWT.RADIO);
+ 		buttonNtoN.setText("N to N");
+ 		buttonNtoN.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				cardinality = MANY_TO_MANY;
+			}
+		});
+ 		
+ 		Button btnAddRelationship = new Button(cardinalityGroup, SWT.NONE);
+ 		btnAddRelationship.setText("Add Relationship");
 		btnAddRelationship.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				if ( (sourceColumnList.getSelection().length != 0) && (destinationColumnList.getSelection().length != 0) && (getBusinessRelationshipName().length() != 0) ){
 					setErrorMessage(null);
-					columnCorrelationList.add(getBusinessRelationshipName()+": "+ sourceTableCombo.getText() + "." + sourceColumnList.getSelection()[0] + " -> "+destinationTableCombo.getText()+"."+destinationColumnList.getSelection()[0]+" "+cardinalityToString(cardinality));
+					columnCorrelationList.add(sourceTableCombo.getText() + "." + sourceColumnList.getSelection()[0] + " -> "+destinationTableCombo.getText()+"."+destinationColumnList.getSelection()[0]);
 					sourceColumns.add( getSourceColumn() );
 					destinationColumns.add( getDestinationColumn() );
+					
+					//disable Input Name field
+					relationshipNameInputField.setEnabled(false);
+					
+					//disable Columns Combo
+					sourceTableCombo.setEnabled(false);
+					destinationTableCombo.setEnabled(false);
+					
 					checkPageComplete();
 				} else {
 					setErrorMessage("You must select a source column, a target column and specify a name");
@@ -142,90 +213,83 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
 					
 			}
 		});
-		btnAddRelationship.setText("Add Relationship"); 		
-		Label lblCardinality = new Label(cardinalityGroup, SWT.NONE);
-		lblCardinality.setText("Cardinality");
-		
-		Composite compCardinality = new Composite(cardinalityGroup, SWT.NONE);
-		compCardinality.setLayoutData(new GridData(SWT.LEFT, SWT.FILL, true, false, 1, 1));
-		compCardinality.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
-		Button btnTo = new Button(compCardinality, SWT.RADIO);
-		btnTo.addSelectionListener(new SelectionAdapter() {
+ 		
+ 		Button btnRemoveRelationship = new Button(cardinalityGroup, SWT.NONE);
+ 		btnRemoveRelationship.setText("Remove Relationship");
+ 		btnRemoveRelationship.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-					cardinality = ONE_TO_ONE;
+				int selectionIndex = columnCorrelationList.getSelectionIndex();
+				if (selectionIndex != -1){
+					//remove source and destination columns from correlation using same index
+					sourceColumns.remove(selectionIndex);
+					destinationColumns.remove(selectionIndex);
+					columnCorrelationList.remove(selectionIndex);
+				}
+				//check if correlation list is empty
+				if (columnCorrelationList.getItemCount() == 0){
+					//re-enable Input name field
+					relationshipNameInputField.setEnabled(true);
+					
+					//re-set default combo selection
+					sourceTableCombo.setEnabled(true);
+					destinationTableCombo.setEnabled(true);
+					selectTable(sourceTableCombo, sourceTable, true);
+					selectTable(destinationTableCombo, destinationTable, true);
+				}
 			}
 		});
-		btnTo.setText("1 to 1");
-		
-		Button btnToN = new Button(compCardinality, SWT.RADIO);
-		btnToN.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				cardinality = ONE_TO_MANY;
-			}
-		});
-		btnToN.setSelection(true);
-		btnToN.setText("1 to N");
-		
-		Button btnNTo = new Button(compCardinality, SWT.RADIO);
-		btnNTo.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				cardinality = MANY_TO_ONE;
-			}
-		});
-		btnNTo.setText("N to 1");
-		
-		Button btnNToN = new Button(compCardinality, SWT.RADIO);
-		btnNToN.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				cardinality = MANY_TO_MANY;
-			}
-		});
-		btnNToN.setText("N to N");
 	}
 	
 	private void initCorrelationSummaryGroup(Composite parent, int style) {
-		Group correlationSummaryGroup = new Group(parent, style);
- 		FillLayout fl_grpRelationship = new FillLayout(SWT.HORIZONTAL);
- 		fl_grpRelationship.marginWidth = 5;
- 		fl_grpRelationship.marginHeight = 5;
- 		correlationSummaryGroup.setLayout(fl_grpRelationship);
- 		correlationSummaryGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+ 		Group correlationSummaryGroup = new Group(parent, style);
  		correlationSummaryGroup.setText("Relationship");
+ 		correlationSummaryGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+ 		correlationSummaryGroup.setLayout(new FillLayout(SWT.HORIZONTAL));
  		
- 		Composite compCorrelationSummary = new Composite(correlationSummaryGroup, SWT.NONE);
- 		compCorrelationSummary.setLayout(new FillLayout(SWT.HORIZONTAL));
+ 		Composite compCorrelation = new Composite(correlationSummaryGroup, SWT.NONE);
+ 		compCorrelation.setLayout(new GridLayout(1, false));
  		
- 		columnCorrelationList = new List(compCorrelationSummary, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+ 		initCardinalityGroup(compCorrelation, SWT.NONE);
+ 		
+ 		columnCorrelationList = new List(compCorrelation, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+ 		columnCorrelationList.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
+ 		
 	}
 	
 	private void initSourceGroup(Composite parent, int style) {
-		Group sourceGroup = new Group(parent, style);
-		sourceGroup.setText("Source Business Table");
-		sourceGroup.setLayout(new GridLayout(1, false));
-		initSourceTableCombo(sourceGroup, SWT.READ_ONLY);
-		Label lblColumns = new Label(sourceGroup, SWT.NONE);
-		lblColumns.setText("Source Columns");
-		initSourceColumnList(sourceGroup, SWT.NONE);
+ 		Group grpSourceBusinessTable = new Group(parent, style);
+ 		grpSourceBusinessTable.setText("Source Business Table");
+ 		grpSourceBusinessTable.setLayout(new GridLayout(1, false));
+ 		
+ 		initSourceTableCombo(grpSourceBusinessTable, SWT.READ_ONLY);
+ 		
+ 		Label lblSourceColumns = new Label(grpSourceBusinessTable, SWT.NONE);
+ 		lblSourceColumns.setText("Source Columns");
+ 		//lblSourceColumns.setLocation(0, 0);
+ 		
+ 		initSourceColumnList(grpSourceBusinessTable, SWT.NONE);
 	}
 	
 	private void initDestinationGroup(Composite parent, int style) {
-		Group destinationGroup = new Group(parent, style);
-		destinationGroup.setText("Target Business Table");
-		destinationGroup.setLayout(new GridLayout(1, false));
-		initDestinationTableCombo(destinationGroup, SWT.READ_ONLY);
-		Label label = new Label(destinationGroup, SWT.NONE);
-		label.setText("Target Columns");
-		initDestinationColumnList(destinationGroup, SWT.NONE);
+		Group grpTargetBusinessTable = new Group(parent, style);
+ 		grpTargetBusinessTable.setText("Target Business Table");
+ 		grpTargetBusinessTable.setLayout(new GridLayout(1, false));
+ 		
+ 		initDestinationTableCombo(grpTargetBusinessTable, SWT.READ_ONLY);
+ 		
+ 		Label lblTargetColumns = new Label(grpTargetBusinessTable, SWT.NONE);
+ 		lblTargetColumns.setText("Target Columns");
+ 		lblTargetColumns.setLocation(0, 0);
+ 		
+ 		initDestinationColumnList(grpTargetBusinessTable, SWT.NONE);
 	}
 		
 	private void initSourceColumnList(Composite parent, int style) {
-		Composite compSourceColumnList = new Composite(parent, style);
+ 		Composite compSourceColumnList = new Composite(parent, style);
  		GridData gdCompSourceColumnList = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+ 		//compSourceColumnList.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+ 		//compSourceColumnList.setBounds(0, 0, 64, 64);
  		gdCompSourceColumnList.heightHint = 100;
  		compSourceColumnList.setLayoutData(gdCompSourceColumnList);
  		compSourceColumnList.setLayout(new FillLayout(SWT.HORIZONTAL));
@@ -234,17 +298,19 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
  		sourceColumnList.addSelectionListener(new SelectionAdapter() {
  			@Override
  			public void widgetSelected(SelectionEvent e) {
- 				if (destinationColumnList.getSelectionCount() > 0){
+ 				if ((destinationColumnList.getSelectionCount() > 0) && (relationshipNameInputField.isEnabled()) && (!nameInserted)){
  					//set suggested name
- 					relationshipNameInputField.setText("BR_"+sourceTableCombo.getText()+"_"+sourceColumnList.getSelection()[0]+"_"+destinationTableCombo.getText()+"_"+destinationColumnList.getSelection()[0]);
+ 					relationshipNameInputField.setText("BR_"+sourceTableCombo.getText()+"_"+destinationTableCombo.getText());
  				}
  			}
  		});
 	}
 	
 	private void initDestinationColumnList(Composite parent, int style) {
-		Composite compDestinationColumnList = new Composite(parent, style);
- 		GridData gdCompDestinationColumnList = new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1);
+ 		Composite compDestinationColumnList = new Composite(parent, style);
+ 		GridData gdCompDestinationColumnList = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+ 		//compDestinationColumnList.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+ 		//compDestinationColumnList.setBounds(0, 0, 64, 64);
  		gdCompDestinationColumnList.heightHint = 100;
  		compDestinationColumnList.setLayoutData(gdCompDestinationColumnList);
  		compDestinationColumnList.setLayout(new FillLayout(SWT.HORIZONTAL));
@@ -253,17 +319,18 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
  		destinationColumnList.addSelectionListener(new SelectionAdapter() {
  			@Override
  			public void widgetSelected(SelectionEvent e) {
- 				if (sourceColumnList.getSelectionCount() > 0){
+ 				if ((sourceColumnList.getSelectionCount() > 0) && (relationshipNameInputField.isEnabled()) && (!nameInserted)){
  					//set suggested name
- 					relationshipNameInputField.setText("BR_"+sourceTableCombo.getText()+"_"+sourceColumnList.getSelection()[0]+"_"+destinationTableCombo.getText()+"_"+destinationColumnList.getSelection()[0]);
+ 					relationshipNameInputField.setText("BR_"+sourceTableCombo.getText()+"_"+destinationTableCombo.getText());
  				}
  			}
  		});
 	}
 	
 	private void initSourceTableCombo(Composite parent, int style) {
-		sourceTableCombo = new Combo(parent, style);
+ 		sourceTableCombo = new Combo(parent, style);
  		sourceTableCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+ 		//sourceTableCombo.setBounds(0, 0, 91, 23);
  		populateSourceTableCombo();
  		sourceTableCombo.addSelectionListener(new SelectionAdapter() {
  			@Override
@@ -276,8 +343,9 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
 	}
 	
 	private void initDestinationTableCombo(Composite parent, int style) {
-		destinationTableCombo = new Combo(parent, style);
+ 		destinationTableCombo = new Combo(parent, style);
  		destinationTableCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+ 		//destinationTableCombo.setBounds(0, 0, 91, 23);
  		populateDestinationTableCombo();
  		destinationTableCombo.addSelectionListener(new SelectionAdapter() {
  			@Override
@@ -298,7 +366,6 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
 	}
 	
 	private void populateDestinationTableCombo(){
-		destinationTableCombo.removeAll();
 		EList<BusinessColumnSet> businessTables = model.getTables();
 		for(BusinessColumnSet table : businessTables){
 			destinationTableCombo.add(table.getName());
@@ -378,5 +445,4 @@ public class AddBusinessRelationshipWizardPage extends WizardPage {
 	public BusinessRelationshipDescriptor getRelationshipDescriptor() {
 		return new BusinessRelationshipDescriptor( getSourceTable(), getDestinationTable(), sourceColumns, destinationColumns, cardinality, getBusinessRelationshipName() );
 	}
-
 }

2788bf41b781d4cb53bb2ee392316dfe3a8a0ed5
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-02-27 16:37:19 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@13593 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V_2.x/Meta/trunk/it.eng.spagobi.meta.initializer/src/it/eng/spagobi/meta/initializer/properties/PhysicalModelDefaultPropertiesInitializer.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package it.eng.spagobi.meta.initializer.properties;
+
+import it.eng.spagobi.meta.model.ModelObject;
+import it.eng.spagobi.meta.model.physical.PhysicalColumn;
+import it.eng.spagobi.meta.model.physical.PhysicalForeignKey;
+import it.eng.spagobi.meta.model.physical.PhysicalModel;
+import it.eng.spagobi.meta.model.physical.PhysicalPrimaryKey;
+import it.eng.spagobi.meta.model.physical.PhysicalTable;
+
+/**
+ * @author Andrea Gioia (andrea.gioia@eng.it)
+ *
+ */
+public class PhysicalModelDefaultPropertiesInitializer implements IPropertiesInitializer {
+	public void addProperties(ModelObject o) {
+		
+		if(o instanceof PhysicalColumn) {
+			initColumnProperties((PhysicalColumn)o);
+		} else if(o instanceof PhysicalModel) {
+			initModelProperties((PhysicalModel)o);
+		} else if(o instanceof PhysicalForeignKey) {
+			initForeignKeyProperty((PhysicalForeignKey)o);
+		} else if(o instanceof PhysicalPrimaryKey) {
+			initPrimaryKeyProperties((PhysicalPrimaryKey)o);
+		} else if(o instanceof PhysicalTable) {
+			initTableProperties((PhysicalTable)o);
+		} else {
+			
+		}
+	}
+	
+	private void initModelProperties(PhysicalModel o) {
+		
+	}
+
+	private void initTableProperties(PhysicalTable o) {
+		
+	}
+	
+	private void initColumnProperties(PhysicalColumn o) {
+		
+	}
+	
+	private void initPrimaryKeyProperties(PhysicalPrimaryKey o) {
+		
+	}
+
+	private void initForeignKeyProperty(PhysicalForeignKey o) {
+	
+	}
+}

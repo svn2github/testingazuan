c734201e3da1efe045bc258b55418e82033cedf3
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-03-05 01:03:27 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@13608 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V_2.x/Meta/trunk/it.eng.spagobi.meta.oda/src/it/eng/spagobi/meta/oda/impl/Driver.java
@@ -0,0 +1,102 @@
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2011 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
+package it.eng.spagobi.meta.oda.impl;
+
+import org.eclipse.datatools.connectivity.oda.IConnection;
+import org.eclipse.datatools.connectivity.oda.IDriver;
+import org.eclipse.datatools.connectivity.oda.LogConfiguration;
+import org.eclipse.datatools.connectivity.oda.OdaException;
+import org.eclipse.datatools.connectivity.oda.util.manifest.DataTypeMapping;
+import org.eclipse.datatools.connectivity.oda.util.manifest.ExtensionManifest;
+import org.eclipse.datatools.connectivity.oda.util.manifest.ManifestExplorer;
+
+/**
+ * Implementation class of IDriver for an ODA runtime driver.
+ */
+public class Driver implements IDriver
+{
+    static String ODA_DATA_SOURCE_ID = "it.eng.spagobi.meta.oda";  //$NON-NLS-1$
+    
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IDriver#getConnection(java.lang.String)
+	 */
+	public IConnection getConnection( String dataSourceType ) throws OdaException
+	{
+        // assumes that this driver supports only one type of data source,
+        // ignores the specified dataSourceType
+        return new Connection();
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IDriver#setLogConfiguration(org.eclipse.datatools.connectivity.oda.LogConfiguration)
+	 */
+	public void setLogConfiguration( LogConfiguration logConfig ) throws OdaException
+	{
+		// do nothing; assumes simple driver has no logging
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IDriver#getMaxConnections()
+	 */
+	public int getMaxConnections() throws OdaException
+	{
+		return 0;	// no limit
+	}
+	
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IDriver#setAppContext(java.lang.Object)
+	 */
+	public void setAppContext( Object context ) throws OdaException
+	{
+	    // do nothing; assumes no support for pass-through context
+	}
+
+    /**
+     * Returns the object that represents this extension's manifest.
+     * @throws OdaException
+     */
+    static ExtensionManifest getManifest()
+        throws OdaException
+    {
+        return ManifestExplorer.getInstance()
+                .getExtensionManifest( ODA_DATA_SOURCE_ID );
+    }
+    
+    /**
+     * Returns the native data type name of the specified code, as
+     * defined in this data source extension's manifest.
+     * @param nativeTypeCode    the native data type code
+     * @return                  corresponding native data type name
+     * @throws OdaException     if lookup fails
+     */
+    static String getNativeDataTypeName( int nativeDataTypeCode ) 
+        throws OdaException
+    {
+        DataTypeMapping typeMapping = 
+                            getManifest().getDataSetType( null )
+                                .getDataTypeMapping( nativeDataTypeCode );
+        if( typeMapping != null )
+            return typeMapping.getNativeType();
+        return "Non-defined"; 
+    }
+
+}

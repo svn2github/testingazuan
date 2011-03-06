a6a3a2a19d3666b208321d6114b5e7b0919bee66
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-03-06 23:14:59 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@13633 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_2.x/Meta/trunk/it.eng.spagobi.meta.oda.ui/src/it/eng/spagobi/meta/oda/ui/impl/CustomDataSetWizardPage.java
+++ V_2.x/Meta/trunk/it.eng.spagobi.meta.oda.ui/src/it/eng/spagobi/meta/oda/ui/impl/CustomDataSetWizardPage.java
@@ -1,9 +1,24 @@
-/*
- *************************************************************************
- * Copyright (c) 2010 <<Your Company Name here>>
- *  
- *************************************************************************
- */
+/**
+
+SpagoBI - The Business Intelligence Free Platform
+
+Copyright (C) 2005-2011 Engineering Ingegneria Informatica S.p.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**/
 
 package it.eng.spagobi.meta.oda.ui.impl;
 
@@ -21,7 +36,6 @@ import org.eclipse.datatools.connectivity.oda.design.ResultSetColumns;
 import org.eclipse.datatools.connectivity.oda.design.ResultSetDefinition;
 import org.eclipse.datatools.connectivity.oda.design.ui.designsession.DesignSessionUtil;
 import org.eclipse.datatools.connectivity.oda.design.ui.wizards.DataSetWizardPage;
-import org.eclipse.datatools.connectivity.oda.design.util.DesignUtil;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;

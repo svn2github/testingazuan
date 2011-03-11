2eb2552622a0195786c44b85ad659f2ae8c82928
aghedin <aghedin@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-03-11 11:03:52 +0000

    Refactoring of the packages.. remove the object DatamartItem
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@13710 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_2.x/Meta/trunk/it.eng.spagobi.meta.datamartTree/src/it/eng/spagobi/meta/datamarttree/builder/DatamartSrtuctureBuilder.java
+++ V_2.x/Meta/trunk/it.eng.spagobi.meta.datamartTree/src/it/eng/spagobi/meta/datamarttree/builder/DatamartSrtuctureBuilder.java
@@ -1,17 +1,12 @@
 package it.eng.spagobi.meta.datamarttree.builder;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import it.eng.qbe.dao.DAOFactory;
 import it.eng.qbe.datasource.jpa.JPADataSource;
-import it.eng.qbe.model.structure.AbstractDataMartItem;
-import it.eng.qbe.model.structure.DataMartEntity;
-import it.eng.qbe.model.structure.DataMartField;
 import it.eng.qbe.model.structure.DataMartModelStructure;
 import it.eng.qbe.model.structure.builder.DataMartStructureBuilderFactory;
 import it.eng.qbe.model.structure.builder.IDataMartStructureBuilder;
-import it.eng.spagobi.meta.datamarttree.bo.DatamartItem;
 
 public class DatamartSrtuctureBuilder {
 
@@ -36,47 +31,10 @@ public class DatamartSrtuctureBuilder {
 
 		
 		return datamartModelStructure;
-		
-//		List<DatamartItem> datamartFieldEntries= new ArrayList<DatamartItem>();
-//
-//		//build the tree
-//		for(int i=0; i<rootEntries.size(); i++){
-//			datamartFieldEntries.add(toDatamartField(rootEntries.get(i)));
-//		}
-//		
-//		return datamartFieldEntries;
+
 	}
 	
-	/**
-	 * Build a DatamrtField for the visualization starting from a DataMartEntity
-	 * @param dataMartEntity the datamart entity to transform
-	 * @return the transformed DatamartField
-	 */
-	public static DatamartItem toDatamartField(DataMartEntity dataMartEntity){
-		DatamartItem dataMartChildField;
-		DatamartItem dataMartField = new DatamartItem(dataMartEntity.getName(), dataMartEntity.getPath(), dataMartEntity.getRole(), dataMartEntity.getType());
 
-		//get the fields and set them as children
-		List<DataMartField> dataMartEntityFields = dataMartEntity.getAllFields();
-		for(int i=0; i<dataMartEntityFields.size(); i++){
-			dataMartChildField = new DatamartItem(dataMartEntityFields.get(i).getName());
-			dataMartChildField.setParent(dataMartField);
-		}
-		
-		//get the fields and set them as children
-		List<DataMartField> dataMartEntityCalculatedFields = dataMartEntity.getCalculatedFields();
-		for(int i=0; i<dataMartEntityFields.size(); i++){
-			dataMartChildField = new DatamartItem(dataMartEntityFields.get(i).getName());
-			dataMartChildField.setParent(dataMartField);
-		}
-		
-		//get the subentities and build the sub tree
-		List<DataMartEntity> subEntities = dataMartEntity.getSubEntities();
-		for(int i=0; i<subEntities.size(); i++){
-			(toDatamartField(subEntities.get(i))).setParent(dataMartField);
-		}
-		return dataMartField;
-	}
 	
 	
 	

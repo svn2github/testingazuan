2405470587e67c3dab2e7a131b52bdcc41269e63
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-04-04 09:53:21 +0000

    deleted some unused imports
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@14230 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_2.x/Meta/trunk/it.eng.spagobi.meta.test/src/it/eng/spagobi/meta/test/generator/JpaMappingGeneratorTest.java
+++ V_2.x/Meta/trunk/it.eng.spagobi.meta.test/src/it/eng/spagobi/meta/test/generator/JpaMappingGeneratorTest.java
@@ -2,7 +2,6 @@ package it.eng.spagobi.meta.test.generator;
 
 import java.io.File;
 
-import it.eng.spagobi.meta.compiler.DataMartGenerator;
 import it.eng.spagobi.meta.generator.jpamapping.JpaMappingCodeGenerator;
 import it.eng.spagobi.meta.initializer.BusinessModelInitializer;
 import it.eng.spagobi.meta.initializer.PhysicalModelInitializer;
@@ -57,27 +56,4 @@ public class JpaMappingGeneratorTest {
         
         gen = new JpaMappingCodeGenerator();
 	}
-
-	@Test
-	public void testGenerate() {
-		String outputDir="C:/progetti/spagobi2.0/workspaceSpagoBIMeta/SpagoBIReverse/src/";
-		try {
-			gen.generate( businessModel, outputDir);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		DataMartGenerator generator=new DataMartGenerator(
-				new File("C:/progetti/spagobi2.0/workspaceSpagoBIMeta/SpagoBIReverse/src/"),
-				new File("C:/progetti/spagobi2.0/workspaceSpagoBIMeta/SpagoBIReverse/build/classes"),
-				new File("C:/ProgramFiles/eclipse-jee-helios-SR1-win32-SpagiBIMETA/plugins/"),
-				new File("C:/progetti/spagobi2.0/workspaceSpagoBIMeta/SpagoBIReverse/dist"),
-				"it/eng/spagobi/meta"
-				);
-		
-		generator.compile();
-		generator.jar();
-	}
-
-
 }

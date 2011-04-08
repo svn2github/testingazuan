3775109694c6359debe22e72ea444a9bbe22708e
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-04-08 12:44:58 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@14301 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_2.x/Meta/trunk/it.eng.spagobi.commons/src/it/eng/spagobi/commons/Activator.java
+++ V_2.x/Meta/trunk/it.eng.spagobi.commons/src/it/eng/spagobi/commons/Activator.java
@@ -1,5 +1,13 @@
 package it.eng.spagobi.commons;
 
+import java.io.File;
+import java.net.URL;
+
+import it.eng.spagobi.commons.exception.SpagoBIPluginException;
+import it.eng.spagobi.commons.resource.DefaultResourceLocator;
+
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -17,6 +25,17 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+
+		DefaultResourceLocator rs = new DefaultResourceLocator("it.eng.spagobi.commons");
+
+		String fileRelativePath = rs.getPropertyAsString("logback.configurationFile.dir");
+		System.out.println("fileRelativePath: "+fileRelativePath);
+		File logback = rs.getFile(fileRelativePath);
+		System.out.println("Absolute path: "+logback.getAbsolutePath());
+		
+		String logbackConfiguration = System.setProperty("logback.configurationFile", "c:\\logback.xml");
+		System.out.println("property: "+System.getProperty("logback.configurationFile"));
+
 	}
 
 	/*

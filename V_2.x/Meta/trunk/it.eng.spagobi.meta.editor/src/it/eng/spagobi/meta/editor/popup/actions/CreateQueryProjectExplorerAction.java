c5740de1b204ef8a30927c5c1ed7b5c98b656ae0
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-04-11 09:54:27 +0000

    added contextual menu for *.sbimodel file in Project Explorer
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@14325 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V_2.x/Meta/trunk/it.eng.spagobi.meta.editor/src/it/eng/spagobi/meta/editor/popup/actions/CreateQueryProjectExplorerAction.java
@@ -0,0 +1,44 @@
+package it.eng.spagobi.meta.editor.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class CreateQueryProjectExplorerAction implements IObjectActionDelegate {
+
+	private Shell shell;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public CreateQueryProjectExplorerAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		MessageDialog.openInformation(
+			shell,
+			"it.eng.spagobi.meta.editor",
+			"New Query was executed.");
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}

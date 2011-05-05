fc8167c60a8a34eded90d42e8d19e44ac5dadd4b
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-05-05 22:34:23 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@14884 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_3.x/Meta/trunk/it.eng.spagobi.meta.edit/src/it/eng/spagobi/meta/model/business/provider/BusinessViewItemProvider.java
+++ V_3.x/Meta/trunk/it.eng.spagobi.meta.edit/src/it/eng/spagobi/meta/model/business/provider/BusinessViewItemProvider.java
@@ -77,7 +77,7 @@ public class BusinessViewItemProvider
 		if (itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
-			addJoinRelationshipsPropertyDescriptor(object);
+			//addJoinRelationshipsPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}

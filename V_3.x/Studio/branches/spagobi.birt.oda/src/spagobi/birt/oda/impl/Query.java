299066d36258242b6e254da3b4698213a5c02a55
gioia <gioia@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2011-06-06 09:34:54 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@15323 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V_3.x/Studio/branches/spagobi.birt.oda/src/spagobi/birt/oda/impl/Query.java
@@ -0,0 +1,391 @@
+/*
+ *************************************************************************
+ * Copyright (c) 2008 <<Your Company Name here>>
+ *  
+ *************************************************************************
+ */
+
+package spagobi.birt.oda.impl;
+
+import it.eng.spagobi.sdk.datasets.bo.SDKDataSet;
+import it.eng.spagobi.sdk.datasets.bo.SDKDataStoreMetadata;
+import it.eng.spagobi.sdk.proxy.DataSetsSDKServiceProxy;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.eclipse.datatools.connectivity.oda.IParameterMetaData;
+import org.eclipse.datatools.connectivity.oda.IQuery;
+import org.eclipse.datatools.connectivity.oda.IResultSet;
+import org.eclipse.datatools.connectivity.oda.IResultSetMetaData;
+import org.eclipse.datatools.connectivity.oda.OdaException;
+import org.eclipse.datatools.connectivity.oda.SortSpec;
+import org.eclipse.datatools.connectivity.oda.spec.QuerySpecification;
+
+/**
+ * Implementation class of IQuery for an ODA runtime driver.
+ * <br>
+ * For demo purpose, the auto-generated method stubs have
+ * hard-coded implementation that returns a pre-defined set
+ * of meta-data and query results.
+ * A custom ODA driver is expected to implement own data source specific
+ * behavior in its place. 
+ */
+public class Query implements IQuery
+{
+	private static Logger logger = Logger.getLogger(Query.class);
+
+	private int m_maxRows;
+	private String queryString;
+	DataSetsSDKServiceProxy datasource;
+	SDKDataStoreMetadata datsStore;
+	
+	public Query() {
+		m_maxRows = 101;
+		logger.debug("ODA Query created");
+	}
+	public Query(DataSetsSDKServiceProxy proxy) {
+		this.datasource = proxy;
+		m_maxRows = 101;
+		logger.debug("ODA Query created");
+	}
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#prepare(java.lang.String)
+	 */
+	public void prepare( String queryText ) throws OdaException
+	{
+		logger.debug("Open dataset with label "+ queryText);
+		logger.debug("ODA Query prepare");
+		this.queryString = queryText;
+		
+
+		if(queryText!=null){
+
+			try {
+				SDKDataSet[] datasets = this.datasource.getDataSets();
+				for(int i =0; i<datasets.length; i++){
+					SDKDataSet datsSet = (SDKDataSet)datasets[i];
+					if(queryText.equals(datsSet.getLabel())){
+						SDKDataStoreMetadata sdkDataStoreMetadata= this.datasource.getDataStoreMetadata(datsSet);
+						datsStore = sdkDataStoreMetadata;
+					}
+				}
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		}
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setAppContext(java.lang.Object)
+	 */
+	public void setAppContext( Object context ) throws OdaException
+	{
+	    // do nothing; assumes no support for pass-through context
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#close()
+	 */
+	public void close() throws OdaException
+	{
+        // TODO Auto-generated method stub
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getMetaData()
+	 */
+	public IResultSetMetaData getMetaData() throws OdaException
+	{
+        /* TODO Auto-generated method stub
+         * Replace with implementation to return an instance 
+         * based on this prepared query.
+         */
+		return new ResultSetMetaData(datsStore);
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#executeQuery()
+	 */
+	public IResultSet executeQuery() throws OdaException
+	{
+		return new ResultSet( datsStore );
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setProperty(java.lang.String, java.lang.String)
+	 */
+	public void setProperty( String name, String value ) throws OdaException
+	{
+		// do nothing; assumes no data set query property
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setMaxRows(int)
+	 */
+	public void setMaxRows( int max ) throws OdaException
+	{
+	    m_maxRows = max;
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getMaxRows()
+	 */
+	public int getMaxRows() throws OdaException
+	{
+		return m_maxRows;
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#clearInParameters()
+	 */
+	public void clearInParameters() throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setInt(java.lang.String, int)
+	 */
+	public void setInt( String parameterName, int value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to named input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setInt(int, int)
+	 */
+	public void setInt( int parameterId, int value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setDouble(java.lang.String, double)
+	 */
+	public void setDouble( String parameterName, double value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to named input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setDouble(int, double)
+	 */
+	public void setDouble( int parameterId, double value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setBigDecimal(java.lang.String, java.math.BigDecimal)
+	 */
+	public void setBigDecimal( String parameterName, BigDecimal value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to named input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setBigDecimal(int, java.math.BigDecimal)
+	 */
+	public void setBigDecimal( int parameterId, BigDecimal value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setString(java.lang.String, java.lang.String)
+	 */
+	public void setString( String parameterName, String value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to named input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setString(int, java.lang.String)
+	 */
+	public void setString( int parameterId, String value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setDate(java.lang.String, java.sql.Date)
+	 */
+	public void setDate( String parameterName, Date value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to named input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setDate(int, java.sql.Date)
+	 */
+	public void setDate( int parameterId, Date value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setTime(java.lang.String, java.sql.Time)
+	 */
+	public void setTime( String parameterName, Time value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to named input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setTime(int, java.sql.Time)
+	 */
+	public void setTime( int parameterId, Time value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setTimestamp(java.lang.String, java.sql.Timestamp)
+	 */
+	public void setTimestamp( String parameterName, Timestamp value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to named input parameter
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setTimestamp(int, java.sql.Timestamp)
+	 */
+	public void setTimestamp( int parameterId, Timestamp value ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to input parameter
+	}
+
+    /* (non-Javadoc)
+     * @see org.eclipse.datatools.connectivity.oda.IQuery#setBoolean(java.lang.String, boolean)
+     */
+    public void setBoolean( String parameterName, boolean value )
+            throws OdaException
+    {
+        // TODO Auto-generated method stub
+        // only applies to named input parameter
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.datatools.connectivity.oda.IQuery#setBoolean(int, boolean)
+     */
+    public void setBoolean( int parameterId, boolean value )
+            throws OdaException
+    {
+        // TODO Auto-generated method stub       
+        // only applies to input parameter
+    }
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.datatools.connectivity.oda.IQuery#setNull(java.lang.String)
+     */
+    public void setNull( String parameterName ) throws OdaException
+    {
+        // TODO Auto-generated method stub
+        // only applies to named input parameter
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.datatools.connectivity.oda.IQuery#setNull(int)
+     */
+    public void setNull( int parameterId ) throws OdaException
+    {
+        // TODO Auto-generated method stub
+        // only applies to input parameter
+    }
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#findInParameter(java.lang.String)
+	 */
+	public int findInParameter( String parameterName ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to named input parameter
+		return 0;
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getParameterMetaData()
+	 */
+	public IParameterMetaData getParameterMetaData() throws OdaException
+	{
+        /* TODO Auto-generated method stub
+         * Replace with implementation to return an instance 
+         * based on this prepared query.
+         */
+		return new ParameterMetaData();
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setSortSpec(org.eclipse.datatools.connectivity.oda.SortSpec)
+	 */
+	public void setSortSpec( SortSpec sortBy ) throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to sorting, assumes not supported
+        throw new UnsupportedOperationException();
+	}
+
+	/*
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getSortSpec()
+	 */
+	public SortSpec getSortSpec() throws OdaException
+	{
+        // TODO Auto-generated method stub
+		// only applies to sorting
+		return null;
+	}
+	@Override
+	public void cancel() throws OdaException, UnsupportedOperationException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public String getEffectiveQueryText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public QuerySpecification getSpecification() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setObject(String arg0, Object arg1) throws OdaException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void setObject(int arg0, Object arg1) throws OdaException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void setSpecification(QuerySpecification arg0) throws OdaException,
+			UnsupportedOperationException {
+		// TODO Auto-generated method stub
+		
+	}
+    
+}

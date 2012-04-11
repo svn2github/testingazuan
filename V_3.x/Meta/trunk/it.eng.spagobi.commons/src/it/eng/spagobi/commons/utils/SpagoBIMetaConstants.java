60c9213c95f268183cbe0f18f76f3668cc2dfb4d
gavardi <gavardi@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-04-11 13:26:46 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@17941 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_3.x/Meta/trunk/it.eng.spagobi.commons/src/it/eng/spagobi/commons/utils/SpagoBIMetaConstants.java
+++ V_3.x/Meta/trunk/it.eng.spagobi.commons/src/it/eng/spagobi/commons/utils/SpagoBIMetaConstants.java
@@ -26,8 +26,8 @@ import org.eclipse.core.runtime.QualifiedName;
 public class SpagoBIMetaConstants {
 
 	
-	public static final String FOLDER_DATASET = "DataSet";
-
+	public static final String FOLDER_DATASET = "Business Queries";
+	
 	public static QualifiedName MODEL_NAME = new QualifiedName("it.eng.spagobi.meta.editor.modelId", "modelId");
 	public static QualifiedName MODEL_FILE_NAME = new QualifiedName("it.eng.spagobi.meta.editor.modelFileName", "modelFileName");
 	public static QualifiedName MODEL_FILE_NAME_FULL_PATH = new QualifiedName("it.eng.spagobi.meta.editor.modelFileNameFullPath", "modelFileNameFullPath");

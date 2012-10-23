168dea9c561da659be56a8aeeb4637a7594bde44
franceschini <franceschini@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2012-10-23 10:30:17 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@18910 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V_3.x/Server/tags/SpagoBI-3.6(20121023)/SpagoBIProject/web-content/js/src/ext/sbi/execution/ShortcutsPanel.js
@@ -0,0 +1,133 @@
+/** SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. **/
+ 
+  
+ 
+  
+ 
+/**
+  * Object name 
+  * 
+  * [description]
+  * 
+  * 
+  * Public Properties
+  * 
+  * [list]
+  * 
+  * 
+  * Public Methods
+  * 
+  *  [list]
+  * 
+  * 
+  * Public Events
+  * 
+  *  [list]
+  * 
+  * Authors
+  * 
+  * - Andrea Gioia (andrea.gioia@eng.it)
+  */
+
+Ext.ns("Sbi.execution");
+
+Sbi.execution.ShortcutsPanel = function(config, doc) {
+	
+	config = Ext.applyIf(config, {border: false});
+	
+	this.subobjectsPanel =  new Sbi.execution.SubobjectsPanel(config, doc);
+	this.snapshotsPanel =  new Sbi.execution.SnapshotsPanel(config, doc);
+	this.viewpointsPanel =  new Sbi.execution.ViewpointsPanel(config, doc);
+		
+	var c = Ext.apply({}, config, {
+		layout:'accordion',
+	    layoutConfig:{
+	          animate:true
+	    },
+	    border: false,
+	    items: this.getSortedPanels()
+	});
+	
+	// constructor
+    Sbi.execution.ShortcutsPanel.superclass.constructor.call(this, c);
+    
+    this.addEvents('subobjectexecutionrequest', 'snapshotexcutionrequest','viewpointexecutionrequest', 'applyviewpoint','subobjectshowmetadatarequest');
+    
+    this.viewpointsPanel.on('executionrequest', function(viewpoint) {
+    	this.fireEvent('viewpointexecutionrequest', viewpoint);
+    }, this);
+    
+    this.viewpointsPanel.on('applyviewpoint', function(viewpoint) {
+    	this.fireEvent('applyviewpoint', viewpoint);
+    }, this);
+    
+    this.subobjectsPanel.on('executionrequest', function(subObjectId) {
+    	this.fireEvent('subobjectexecutionrequest', subObjectId);
+    }, this);
+    
+    this.snapshotsPanel.on('executionrequest', function(snapshotId) {
+    	this.fireEvent('snapshotexcutionrequest', snapshotId);
+    }, this);
+    
+     this.subobjectsPanel.on('showmetadatarequest', function(subObjectId) {
+    	this.fireEvent('subobjectshowmetadatarequest', subObjectId);
+    }, this);
+    
+};
+
+Ext.extend(Sbi.execution.ShortcutsPanel, Ext.Panel, {
+	
+	viewpointsPanel: null
+    , subobjectsPanel: null
+    , snapshotsPanel: null		
+	
+	, synchronize: function( executionInstance ) {
+		this.synchronizeViewpoints(executionInstance);
+		this.synchronizeSubobjects(executionInstance);
+		this.synchronizeSnapshots(executionInstance);
+	}
+
+	, synchronizeViewpoints: function( executionInstance ) {
+		this.viewpointsPanel.synchronize( executionInstance );
+	}
+	
+	, synchronizeSubobjects: function( executionInstance ) {
+		this.subobjectsPanel.synchronize( executionInstance );
+	}
+	
+	//method use with checkbox , synchronizeSubobjectsAndOpenMetadata: function( id, meta, executionInstance ) {
+	, synchronizeSubobjectsAndOpenMetadata: function( id, executionInstance ) {
+		// synchronize subobjects
+		this.subobjectsPanel.synchronize( executionInstance );
+		// open the metadata windows if so chosen in saveWindow
+		//if(meta == true) {
+		this.subobjectsPanel.openMetadataWindowAfterSaving( id, executionInstance );
+
+		//}
+	}
+	, synchronizeSnapshots: function( executionInstance ) {
+		this.snapshotsPanel.synchronize( executionInstance );
+	}
+	
+	, getSortedPanels: function() {
+		var toReturn = new Array();
+		if(Sbi.settings && Sbi.settings.execution && Sbi.settings.execution.shortcutsPanel && Sbi.settings.execution.shortcutsPanel.panelsOrder) {
+			var viewpointsPosition = Sbi.settings.execution.shortcutsPanel.panelsOrder.viewpoints - 1;
+			var subobjectsPosition = Sbi.settings.execution.shortcutsPanel.panelsOrder.subobjects - 1;
+			var snapshotsPosition = Sbi.settings.execution.shortcutsPanel.panelsOrder.snapshots - 1;
+			toReturn[viewpointsPosition] = this.viewpointsPanel;
+			toReturn[subobjectsPosition] = this.subobjectsPanel;
+			toReturn[snapshotsPosition] = this.snapshotsPanel;
+		} else {
+			toReturn[0] = this.viewpointsPanel;
+			toReturn[1] = this.subobjectsPanel;
+			toReturn[2] = this.snapshotsPanel;
+		}
+		return toReturn;
+	}
+	
+});
---
+++ V_3.x/Server/tags/SpagoBI-3.6(20121023)/SpagoBIQbeEngine/src/it/eng/spagobi/engines/worksheet/widgets/ChartDefinition.java
@@ -0,0 +1,73 @@
+/* SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package it.eng.spagobi.engines.worksheet.widgets;
+
+import it.eng.spagobi.engines.worksheet.bo.Attribute;
+import it.eng.spagobi.engines.worksheet.bo.Field;
+import it.eng.spagobi.engines.worksheet.bo.Serie;
+import it.eng.spagobi.engines.worksheet.bo.SheetContent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * @author Davide Zerbetto (davide.zerbetto@eng.it)
+ */
+public class ChartDefinition extends SheetContent {
+	
+	private Attribute category = null;
+	private List<Serie> series = null;
+	private JSONObject config = null;
+	
+	public ChartDefinition() {}
+
+	public Attribute getCategory() {
+		return category;
+	}
+
+	public void setCategory(Attribute category) {
+		this.category = category;
+	}
+
+	public List<Serie> getSeries() {
+		return series;
+	}
+
+	public void setSeries(List<Serie> series) {
+		this.series = series;
+	}
+
+	public JSONObject getConfig() {
+		return config;
+	}
+
+	public void setConfig(JSONObject config) {
+		this.config = config;
+	}
+
+	@Override
+	public List<Attribute> getFiltersOnDomainValues() {
+		List<Attribute> toReturn = new ArrayList<Attribute>();
+		Attribute category = getCategory();
+		String values = category.getValues();
+		if (values != null && !values.equals(new JSONArray().toString())) {
+			toReturn.add(category);
+		}
+		return toReturn;
+	}
+
+	@Override
+	public List<Field> getAllFields() {
+		List<Field> toReturn = new ArrayList<Field>();
+		toReturn.add(getCategory());
+		toReturn.addAll(getSeries());
+		return toReturn;
+	}
+
+}

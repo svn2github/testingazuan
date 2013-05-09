26cf3f098293c9c854956a899c5c41e8bc8bfb45
aghedin <aghedin@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2013-05-09 15:22:39 +0000

    OPEN - issue SPAGOBI-1134: Visualization of the Data set in the QbE https://www.spagoworld.org/jira/browse/SPAGOBI-1134
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@19564 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ V_4.x/Server/branches/AdHocReportingFromDataset/QbeCore/src/dataset/it/eng/qbe/datasource/dataset/DataSetDataSource.java
@@ -0,0 +1,111 @@
+/* SpagoBI, the Open Source Business Intelligence suite
+
+ * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package it.eng.qbe.datasource.dataset;
+
+import it.eng.qbe.datasource.AbstractDataSource;
+import it.eng.qbe.datasource.ConnectionDescriptor;
+import it.eng.qbe.datasource.IDataSource;
+import it.eng.qbe.datasource.IPersistenceManager;
+import it.eng.qbe.datasource.configuration.CompositeDataSourceConfiguration;
+import it.eng.qbe.datasource.configuration.DataSetDataSourceConfiguration;
+import it.eng.qbe.datasource.configuration.IDataSourceConfiguration;
+import it.eng.qbe.datasource.jpa.JPADataSource;
+import it.eng.qbe.datasource.transaction.ITransaction;
+import it.eng.qbe.datasource.transaction.dataset.DataSetTransaction;
+import it.eng.qbe.model.accessmodality.AbstractModelAccessModality;
+import it.eng.qbe.model.structure.IModelStructure;
+import it.eng.qbe.model.structure.builder.IModelStructureBuilder;
+import it.eng.qbe.model.structure.builder.dataset.DataSetModelStructureBuilder;
+import it.eng.spagobi.tools.dataset.bo.IDataSet;
+import it.eng.spagobi.utilities.assertion.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+public class DataSetDataSource  extends AbstractDataSource implements IDataSource{
+	
+	
+	private List<IDataSet> datasets;
+	
+	
+	private static transient Logger logger = Logger.getLogger(JPADataSource.class);
+
+	protected DataSetDataSource(String dataSourceName, IDataSourceConfiguration configuration) {
+		logger.debug("Creating a new JPADataSource");
+		setName( dataSourceName );
+		dataMartModelAccessModality = new AbstractModelAccessModality();
+		this.configuration = configuration;
+		datasets= new ArrayList<IDataSet>();
+		
+//		// validate and set configuration
+		if(configuration instanceof DataSetDataSourceConfiguration){
+			datasets.add(((DataSetDataSourceConfiguration) configuration).getDataset());
+		} else if(configuration instanceof CompositeDataSourceConfiguration){
+			IDataSourceConfiguration subConf = ((CompositeDataSourceConfiguration)configuration).getSubConfigurations().get(0);
+			if(subConf instanceof DataSetDataSourceConfiguration){
+				datasets.add(((DataSetDataSourceConfiguration)subConf).getDataset());
+			} else {
+				Assert.assertUnreachable("Not suitable configuration to create a JPADataSource");
+			}
+		} else {
+			Assert.assertUnreachable("Not suitable configuration to create a JPADataSource");
+		}
+		logger.debug("Created a new JPADataSource");
+	}
+	
+	public DataSetDataSourceConfiguration getDataSetDataSourceConfiguration() {
+		return (DataSetDataSourceConfiguration)configuration;
+	}
+	
+
+	public void open() {
+
+	}
+	
+	public boolean isOpen() {
+		return true;
+	}
+	
+	public void close() {
+
+	}
+	
+	public ConnectionDescriptor getConnection() {
+		return null;
+	}
+	
+	
+
+	public IModelStructure getModelStructure() {
+		IModelStructureBuilder structureBuilder;
+		if(dataMartModelStructure == null) {			
+			structureBuilder = new DataSetModelStructureBuilder(this);
+			dataMartModelStructure = structureBuilder.build();
+		}
+		
+		return dataMartModelStructure;
+	}
+	
+
+	public ITransaction getTransaction(){
+		return new DataSetTransaction(this);
+	}
+
+	//TO-DO
+	public IPersistenceManager getPersistenceManager() {
+		return null;
+	}
+	
+
+	
+	public List<IDataSet> getRootEntities(){
+		return datasets;
+	}
+	
+}

4bdde59d2f1b60b27a8e4981c44659d4de8e74b8
zerbetto <zerbetto@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-01-21 16:28:14 +0000

    CLOSED - issue SPAGOBI-1565: Error in execution parameters' panel: clear parameters button does not clear parameters without default values https://spagobi.eng.it/jira/browse/SPAGOBI-1565
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@21780 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_4.x/Server/tags/SpagoBI-4.1.0(20131203)/SpagoBIProject/web-content/js/src/ext/sbi/execution/ParametersPanel.js
+++ V_4.x/Server/tags/SpagoBI-4.1.0(20131203)/SpagoBIProject/web-content/js/src/ext/sbi/execution/ParametersPanel.js
@@ -685,12 +685,13 @@ Ext.extend(Sbi.execution.ParametersPanel, Ext.FormPanel, {
 			Sbi.debug('[ParametersPanel.getDefaultValuesFormState] : default values for field [' + field.name + '] is [' + value + ']');
 			var description = this.concatenateDefaultValuesDescription(behindParameter.defaultValues);
 			Sbi.debug('[ParametersPanel.getDefaultValuesFormState] : default description for field [' + field.name + '] is [' + description + ']');
-			if (value != null) {
+			if (!field.isTransient) {
+				// in case the parameters is transient (i.e. it is single-value and therefore hidden), it is not considered
+				Sbi.debug('[ParametersPanel.getDefaultValuesFormState] : field [' + field.name + '] is not transient');
 				state[field.name] = value;
 				state[field.name + '_field_visible_description'] = description;
 			} else {
-				// in case the default value is not set, we don't set into the state because in that case the null value wuold 
-				// overwrite the value of a single-value driver)
+				Sbi.debug('[ParametersPanel.getDefaultValuesFormState] : field [' + field.name + '] is transient and therefore skipped');
 			}
 		}
 		

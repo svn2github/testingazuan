207e1901d201f9c26d89104e8c900eff930836d7
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-10-03 10:52:44 +0000

    fixed problem: catalog with null name throw exception that also prevent schema combo population
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@23713 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_5.x/Meta/trunk/it.eng.spagobi.meta.editor/src/it/eng/spagobi/meta/editor/multi/wizards/SelectionConnectionPage.java
+++ V_5.x/Meta/trunk/it.eng.spagobi.meta.editor/src/it/eng/spagobi/meta/editor/multi/wizards/SelectionConnectionPage.java
@@ -6,10 +6,8 @@
  License, v. 2.0. If a copy of the MPL was not distributed with this file,
  You can obtain one at http://mozilla.org/MPL/2.0/.
  
-**/
+ **/
 package it.eng.spagobi.meta.editor.multi.wizards;
-
-
 
 import it.eng.spagobi.commons.resource.IResourceLocator;
 import it.eng.spagobi.meta.editor.SpagoBIMetaEditorPlugin;
@@ -46,24 +44,23 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
 
-
 /**
  * @author cortella
- *
+ * 
  */
 public class SelectionConnectionPage extends WizardPage {
 
-	private static final IResourceLocator RL = SpagoBIMetaEditorPlugin.getInstance().getResourceLocator(); 
-	
+	private static final IResourceLocator RL = SpagoBIMetaEditorPlugin.getInstance().getResourceLocator();
+
 	private List connectionList;
-	private DSEBridge dseBridge;
+	private final DSEBridge dseBridge;
 	private IConnectionProfile[] profiles;
 	private Combo catalogCombo;
 	private Combo schemaCombo;
 	private Connection jdbcConnection;
 	private String selectedConnectionName;
 	private PhysicalTableSelectionPage physicalTableSelectionPageRef;
-	
+
 	/**
 	 * @param pageName
 	 */
@@ -71,58 +68,62 @@ public class SelectionConnectionPage extends WizardPage {
 		super(pageName);
 		setTitle(RL.getString("multi.editor.wizard.selectionconnection.title"));
 		setMessage(RL.getString("multi.editor.wizard.selectionconnection.message"));
-		ImageDescriptor image = ImageDescriptor.createFromURL( (URL)RL.getImage("it.eng.spagobi.meta.editor.business.wizards.inline.selectConnection") );
-	    if (image!=null) {
-	    	setImageDescriptor(image);
-	    }
+		ImageDescriptor image = ImageDescriptor.createFromURL((URL) RL.getImage("it.eng.spagobi.meta.editor.business.wizards.inline.selectConnection"));
+		if (image != null) {
+			setImageDescriptor(image);
+		}
 		dseBridge = new DSEBridge();
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets
+	 * .Composite)
 	 */
 	@Override
 	public void createControl(Composite parent) {
 		profiles = dseBridge.getConnectionProfiles();
-		
-		
+
 		Composite container = new Composite(parent, SWT.NULL);
 		setControl(container);
 		container.setLayout(new GridLayout(1, false));
-		
+
 		Group grpConnection = new Group(container, SWT.NONE);
 		grpConnection.setText(RL.getString("multi.editor.wizard.selectionconnection.label"));
 		grpConnection.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		FillLayout fl_grpConnection = new FillLayout(SWT.HORIZONTAL);
 		grpConnection.setLayout(fl_grpConnection);
-		
+
 		ListViewer listViewer = new ListViewer(grpConnection, SWT.BORDER | SWT.V_SCROLL);
 		connectionList = listViewer.getList();
 		connectionList.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				
-				
-				//reset combos
+
+				// reset combos
 				catalogCombo.removeAll();
 				catalogCombo.setEnabled(false);
 				schemaCombo.removeAll();
 				schemaCombo.setEnabled(false);
-				
+
 				selectedConnectionName = connectionList.getSelection()[0];
-				//ProgressMonitorDialog to show a progress bar for long operation
+				// ProgressMonitorDialog to show a progress bar for long
+				// operation
 				ProgressMonitorDialog dialog = new ProgressMonitorDialog(new Shell());
 				dialog.setCancelable(false);
-				
+
 				try {
-					dialog.run(true, false, new IRunnableWithProgress(){
-					    public void run(IProgressMonitor monitor) {
-					    	//Note: this is a non-UI Thread
-					        monitor.beginTask("Checking connection, please wait...", IProgressMonitor.UNKNOWN);
-							//doing task...
-					        jdbcConnection = dseBridge.connect(selectedConnectionName);
-					        monitor.done();				        
-					    }
+					dialog.run(true, false, new IRunnableWithProgress() {
+						@Override
+						public void run(IProgressMonitor monitor) {
+							// Note: this is a non-UI Thread
+							monitor.beginTask("Checking connection, please wait...", IProgressMonitor.UNKNOWN);
+							// doing task...
+							jdbcConnection = dseBridge.connect(selectedConnectionName);
+							monitor.done();
+						}
 					});
 				} catch (InvocationTargetException e1) {
 					e1.printStackTrace();
@@ -130,27 +131,23 @@ public class SelectionConnectionPage extends WizardPage {
 					e1.printStackTrace();
 				}
 
-				if (jdbcConnection != null){
+				if (jdbcConnection != null) {
 					setErrorMessage(null);
 					setPageComplete(true);
 					populateCatalogCombo(jdbcConnection);
-				}
-				else {
-					setPageComplete(false);	
-					ErrorDialog.openError(
-							null, 
-							"Connection failed", 
-							"Connection to database failed, please check your settings",
-							new org.eclipse.core.runtime.Status(IStatus.ERROR,"id","Connection to database failed")
-					);	
+				} else {
+					setPageComplete(false);
+					ErrorDialog.openError(null, "Connection failed", "Connection to database failed, please check your settings",
+							new org.eclipse.core.runtime.Status(IStatus.ERROR, "id", "Connection to database failed"));
 					setErrorMessage("Please select a valid connection to continue");
-				}			
+				}
 			}
 		});
 		listViewer.setContentProvider(new ArrayContentProvider());
 		listViewer.setLabelProvider(new LabelProvider() {
-			public String getText(Object element){
-				return ((IConnectionProfile)element).getName();
+			@Override
+			public String getText(Object element) {
+				return ((IConnectionProfile) element).getName();
 			}
 		});
 		listViewer.setInput(profiles);
@@ -158,14 +155,14 @@ public class SelectionConnectionPage extends WizardPage {
 		grpCatalogAndSchema.setText(RL.getString("multi.editor.wizard.selectionconnection.catalogschema.label"));
 		grpCatalogAndSchema.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
 		grpCatalogAndSchema.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
+
 		Composite composite = new Composite(grpCatalogAndSchema, SWT.NONE);
 		composite.setLayout(new GridLayout(4, false));
-		
+
 		Label lblCatalog = new Label(composite, SWT.NONE);
 		lblCatalog.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblCatalog.setText(RL.getString("multi.editor.wizard.selectionconnection.catalog.label"));
-		
+
 		catalogCombo = new Combo(composite, SWT.READ_ONLY);
 		catalogCombo.setEnabled(false);
 		catalogCombo.addSelectionListener(new SelectionAdapter() {
@@ -177,23 +174,23 @@ public class SelectionConnectionPage extends WizardPage {
 			}
 		});
 		catalogCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
+
 		Label lblSchema = new Label(composite, SWT.NONE);
 		lblSchema.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblSchema.setText(RL.getString("multi.editor.wizard.selectionconnection.schema.label"));
-		
+
 		schemaCombo = new Combo(composite, SWT.READ_ONLY);
 		schemaCombo.setEnabled(false);
 		schemaCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
 	}
-	
-	private void populateCatalogCombo(Connection connection){
+
+	private void populateCatalogCombo(Connection connection) {
 		String catalog;
 		DatabaseMetaData dbMeta;
 		ResultSet rs = null;
-		
-		try{
+
+		try {
 			catalog = connection.getCatalog();
 			if (catalog != null)
 				catalogCombo.add(catalog);
@@ -201,135 +198,138 @@ public class SelectionConnectionPage extends WizardPage {
 				dbMeta = connection.getMetaData();
 				rs = dbMeta.getCatalogs();
 				while (rs.next()) {
-					catalogCombo.add( rs.getString(1));
-				}				
+					String catalogName = rs.getString(1);
+					if (catalogName != null) {
+						catalogCombo.add(catalogName);
+					}
+				}
 			}
 			if (catalogCombo.getItemCount() > 0) {
-				if (catalogCombo.getItemCount() == 1){
-					//only one catalog found, automatically search schema
+				if (catalogCombo.getItemCount() == 1) {
+					// only one catalog found, automatically search schema
 					catalogCombo.select(0);
 					catalogCombo.setEnabled(true);
-					populateSchemaCombo(connection,null);
-				}
-				else{
-					//set first value as default selection
+					populateSchemaCombo(connection, null);
+				} else {
+					// set first value as default selection
 					catalogCombo.select(0);
 					catalogCombo.setEnabled(true);
 				}
-			}
-			else {
+			} else {
 				catalogCombo.setEnabled(false);
-				populateSchemaCombo(connection,null);				
+				populateSchemaCombo(connection, null);
 			}
-		}
-		catch(Throwable t) {
+		} catch (Throwable t) {
 			throw new RuntimeException("Impossible to check catalog", t);
 		}
 	}
-	
-	private void populateSchemaCombo(Connection connection, String catalog){
-		
+
+	private void populateSchemaCombo(Connection connection, String catalog) {
+
 		ResultSet rs;
 		DatabaseMetaData dbMeta;
-		
-		try{
+
+		try {
 			dbMeta = connection.getMetaData();
-			if (catalog == null){
+			if (catalog == null) {
 				rs = dbMeta.getSchemas();
-			}	
-			else {
+			} else {
 				rs = dbMeta.getSchemas();
-				//rs = dbMeta.getSchemas(catalog,null);				
+				// rs = dbMeta.getSchemas(catalog,null);
 			}
 			while (rs.next()) {
-				schemaCombo.add(rs.getString(1));
+				String schemaName = rs.getString(1);
+				if (schemaName != null) {
+					schemaCombo.add(rs.getString(1));
+				}
 			}
-			if (schemaCombo.getItemCount() > 0){
-				//set first value as default selection
+			if (schemaCombo.getItemCount() > 0) {
+				// set first value as default selection
 				schemaCombo.select(0);
 				schemaCombo.setEnabled(true);
-			}
-			else {
+			} else {
 				schemaCombo.setEnabled(false);
 			}
-			//release result set resources
-			//rs.close();
-			//close connection
-			//connection.close();
-		}
-		catch(Throwable t) {
+			// release result set resources
+			// rs.close();
+			// close connection
+			// connection.close();
+		} catch (Throwable t) {
 			throw new RuntimeException("Impossible to check schema", t);
-		}		
+		}
 	}
-	
+
 	public String getConnectionName() {
 		if (connectionList.getSelection().length > 0)
 			return connectionList.getSelection()[0];
 		else
 			return null;
 	}
-	
+
 	public String getCatalogName() {
-		if (catalogCombo.isEnabled()){
+		if (catalogCombo.isEnabled()) {
 			int selectionIndex = catalogCombo.getSelectionIndex();
 			return catalogCombo.getItem(selectionIndex);
-		}
-		else 
+		} else
 			return null;
 	}
-	
+
 	public String getSchemaName() {
-		if (schemaCombo.isEnabled()){
+		if (schemaCombo.isEnabled()) {
 			int selectionIndex = schemaCombo.getSelectionIndex();
 			return schemaCombo.getItem(selectionIndex);
-		}
-		else 
+		} else
 			return null;
-	}	
-	
-	public Connection getConnection() {
-		return dseBridge.connect( getConnectionName() );
 	}
-	
-	//must be invoked after getConnection
-	public String getConnectionDriver(){
+
+	public Connection getConnection() {
+		return dseBridge.connect(getConnectionName());
+	}
+
+	// must be invoked after getConnection
+	public String getConnectionDriver() {
 		return dseBridge.getConnectionDriver();
 	}
-	//must be invoked after getConnection
-	public String getConnectionUrl(){
+
+	// must be invoked after getConnection
+	public String getConnectionUrl() {
 		return dseBridge.getConnectionUrl();
 	}
-	//must be invoked after getConnection
-	public String getConnectionUsername(){
+
+	// must be invoked after getConnection
+	public String getConnectionUsername() {
 		return dseBridge.getConnectionUsername();
 	}
-	//must be invoked after getConnection
-	public String getConnectionPassword(){
+
+	// must be invoked after getConnection
+	public String getConnectionPassword() {
 		return dseBridge.getConnectionPassword();
 	}
-	//must be invoked after getConnection
-	public String getConnectionDatabaseName(){
+
+	// must be invoked after getConnection
+	public String getConnectionDatabaseName() {
 		return dseBridge.getConnectionDatabaseName();
 	}
-	
-	//Set next page data
+
+	// Set next page data
+	@Override
 	public IWizardPage getNextPage() {
-    	IWizardPage nextPage = super.getNextPage();
-    	String schemaName = getSchemaName();
-    	String catalogName = getCatalogName();
-    	if (getConnectionName() != null && (schemaName != null || catalogName != null) ){
-        	if (nextPage instanceof PhysicalTableSelectionPage) {
-        		physicalTableSelectionPageRef.addTableItems(getConnectionName(), getCatalogName(), getSchemaName());
-        	}
-    	}
-            return nextPage;
-        }
+		IWizardPage nextPage = super.getNextPage();
+		String schemaName = getSchemaName();
+		String catalogName = getCatalogName();
+		if (getConnectionName() != null && (schemaName != null || catalogName != null)) {
+			if (nextPage instanceof PhysicalTableSelectionPage) {
+				physicalTableSelectionPageRef.addTableItems(getConnectionName(), getCatalogName(), getSchemaName());
+			}
+		}
+		return nextPage;
+	}
 
 	/**
-	 * @param physicalTableSelectionPageRef the physicalTableSelectionPageRef to set
+	 * @param physicalTableSelectionPageRef
+	 *            the physicalTableSelectionPageRef to set
 	 */
-	public void setPhysicalTableSelectionPageRef(
-			PhysicalTableSelectionPage physicalTableSelectionPageRef) {
+	public void setPhysicalTableSelectionPageRef(PhysicalTableSelectionPage physicalTableSelectionPageRef) {
 		this.physicalTableSelectionPageRef = physicalTableSelectionPageRef;
 	}
 

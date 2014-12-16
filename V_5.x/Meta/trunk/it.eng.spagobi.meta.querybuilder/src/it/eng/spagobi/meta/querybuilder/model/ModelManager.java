cee00c01470c71a9607d48a88f0e2dc025d40618
mcortella <mcortella@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2014-12-16 17:19:15 +0000

    added support for SAP DB and SAP HANA (using generic sap db dialect) in SpagoBI Meta
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi@24335 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- V_5.x/Meta/trunk/it.eng.spagobi.meta.querybuilder/src/it/eng/spagobi/meta/querybuilder/model/ModelManager.java
+++ V_5.x/Meta/trunk/it.eng.spagobi.meta.querybuilder/src/it/eng/spagobi/meta/querybuilder/model/ModelManager.java
@@ -6,7 +6,7 @@
  License, v. 2.0. If a copy of the MPL was not distributed with this file,
  You can obtain one at http://mozilla.org/MPL/2.0/.
  
-**/
+ **/
 package it.eng.spagobi.meta.querybuilder.model;
 
 import it.eng.qbe.datasource.ConnectionDescriptor;
@@ -49,227 +49,229 @@ import org.xml.sax.SAXException;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
- *
+ * 
  */
 public class ModelManager {
-	
-	Object modelRefernce;	
+
+	Object modelRefernce;
 	Model model;
-	
+
 	File mappingsFolder;
 	org.eclipse.core.internal.resources.File modelFile;
-	
 
 	private static Logger logger = LoggerFactory.getLogger(ModelManager.class);
-	
+
 	public ModelManager(Model model) {
 		this.modelRefernce = model;
 		this.model = model;
 	}
-	
+
 	public ModelManager(File modelFile) {
 		this.modelRefernce = modelFile;
 	}
-	
+
 	public Model getModel() {
-		if(model == null) {
+		if (model == null) {
 			loadModel();
 		}
 		return model;
 	}
-	
+
 	public PhysicalModel getPhysicalModel() {
 		return getModel().getPhysicalModels().get(0);
 	}
-	
+
 	public BusinessModel getBusinessModel() {
 		return getModel().getBusinessModels().get(0);
 	}
-	
+
 	public void loadModel() {
-		if(modelRefernce instanceof File) {
-			File modelFile = (File)modelRefernce;
+		if (modelRefernce instanceof File) {
+			File modelFile = (File) modelRefernce;
 			IModelDAO modelDAO = new ModelDAOFileImpl(modelFile.getParentFile());
 			model = modelDAO.loadModel(modelFile.getName());
 		}
 	}
-	
+
 	public void validateModel() {
 		ModelValidator modelValidator = new ModelValidator();
-		if(modelValidator.validate(getModel()) == false) {
+		if (modelValidator.validate(getModel()) == false) {
 			String message = "Model [" + getModel().getName() + "] contains the following structural errors: ";
-			for(String diagnosticMessage : modelValidator.getDiagnosticMessages()) {
+			for (String diagnosticMessage : modelValidator.getDiagnosticMessages()) {
 				message += "\n - " + diagnosticMessage;
 			}
 			throw new SpagoBIPluginException(message);
 		}
-	}	
-	
-	
+	}
+
 	public String generateMapping(boolean appendTimestampToPersistenceUnitName) {
 		String persistenceUnitName = getBusinessModel().getName();
-		if(appendTimestampToPersistenceUnitName) {
+		if (appendTimestampToPersistenceUnitName) {
 			persistenceUnitName += "_" + System.currentTimeMillis();
 		}
-		
+
 		return generateMapping(persistenceUnitName);
 	}
-	
+
 	private String generateMapping(String persistenceUnitName) {
-		
+
 		logger.trace("IN");
 		JpaMappingJarGenerator generator = null;
 		try {
-			Assert.assertNotNull("Impossible to generate mapping. Mapping folder is not set", getModelMappingFolder() );
-			
-			
-			
+			Assert.assertNotNull("Impossible to generate mapping. Mapping folder is not set", getModelMappingFolder());
+
 			GeneratorDescriptor descriptor = GeneratorFactory.getGeneratorDescriptorById("it.eng.spagobi.meta.generator.jpamapping");
-			generator = (JpaMappingJarGenerator)descriptor.getGenerator();
+			generator = (JpaMappingJarGenerator) descriptor.getGenerator();
 			generator.setLibDir(new File("plugins"));
 			generator.setPersistenceUnitName(persistenceUnitName);
-			generator.setModelFile(getModelFile());			
+			generator.setModelFile(getModelFile());
 			generator.generate(getBusinessModel(), getMappingsFolder().toString());
-		} catch(Throwable t) {
-			throw new SpagoBIPluginException("Impossible to generate mapping for business model [" + getBusinessModel().getName() + "] into folder [" + getMappingsFolder() + "]", t);
+		} catch (Throwable t) {
+			throw new SpagoBIPluginException("Impossible to generate mapping for business model [" + getBusinessModel().getName() + "] into folder ["
+					+ getMappingsFolder() + "]", t);
 		} finally {
 			// finally block to hide technical folders created during generation
-			if(generator != null){
+			if (generator != null) {
 				logger.debug("hide techical folders");
 				generator.hideTechnicalResources();
 			}
 			logger.trace("OUT");
 		}
-		
+
 		return persistenceUnitName;
 	}
-	
+
 	public File getMappingsFolder() {
-		if(mappingsFolder == null && modelRefernce instanceof File) {
-			return ((File)modelRefernce).getParentFile();
+		if (mappingsFolder == null && modelRefernce instanceof File) {
+			return ((File) modelRefernce).getParentFile();
 		}
 		return mappingsFolder;
 	}
-	
+
 	public File getModelMappingFolder() {
 		File mappingsFolder = getMappingsFolder();
-		
-		return mappingsFolder != null? new File(mappingsFolder, getBusinessModel().getName() ): null;
+
+		return mappingsFolder != null ? new File(mappingsFolder, getBusinessModel().getName()) : null;
 	}
 
 	public void setMappingsFolder(File mappingsFolder) {
 		this.mappingsFolder = mappingsFolder;
 	}
-	
+
 	public IDataSource createDataSource() {
-		return createDataSource( discoverPersistenceUnitName() );
+		return createDataSource(discoverPersistenceUnitName());
 	}
-	
+
 	public IDataSource createDataSource(String persistenceUnitName) {
 		ConnectionDescriptor connectionDescriptor = getConnectionDescriptor();
-		connectionDescriptor.setName( getBusinessModel().getName() );
-		
-		Map<String,Object> dataSourceProperties = new HashMap<String,Object>();
+		connectionDescriptor.setName(getBusinessModel().getName());
+
+		Map<String, Object> dataSourceProperties = new HashMap<String, Object>();
 		dataSourceProperties.put("connection", connectionDescriptor);
-		
-		
+
 		File mappingDistFolder = new File(getModelMappingFolder(), "dist");
-		
+
 		List<String> persistenceUnitNames = new ArrayList<String>();
-		persistenceUnitNames.add( persistenceUnitName );
-		
+		persistenceUnitNames.add(persistenceUnitName);
+
 		return OdaStructureBuilder.getDataSourceSingleModel(persistenceUnitNames, dataSourceProperties, new File(mappingDistFolder, "datamart.jar"));
 	}
-	
+
 	private String discoverPersistenceUnitName() {
-		
+
 		File mappingSrcFolder = new File(getModelMappingFolder(), "src");
 		File mappingMetaInfFolder = new File(mappingSrcFolder, "META-INF");
-		File persistenceFile = new File(mappingMetaInfFolder,"persistence.xml");
-		
+		File persistenceFile = new File(mappingMetaInfFolder, "persistence.xml");
+
 		String persistenceName = null;
 		if (persistenceFile.exists()) {
 			DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-	        DocumentBuilder builder;
+			DocumentBuilder builder;
 			try {
 				builder = domFactory.newDocumentBuilder();
-		        Document document = builder.parse(persistenceFile);
-				
+				Document document = builder.parse(persistenceFile);
+
 				XPath xpath = XPathFactory.newInstance().newXPath();
-		        XPathExpression expr = xpath.compile("/persistence/persistence-unit/@name");
-		 
-		        Object result = expr.evaluate(document, XPathConstants.STRING);
-		        persistenceName = (String) result;
+				XPathExpression expr = xpath.compile("/persistence/persistence-unit/@name");
+
+				Object result = expr.evaluate(document, XPathConstants.STRING);
+				persistenceName = (String) result;
 			} catch (ParserConfigurationException e) {
-				logger.error("Impossible to retrieve Persistence Unit Name - ParserConfigurationException : [{}]",e);
+				logger.error("Impossible to retrieve Persistence Unit Name - ParserConfigurationException : [{}]", e);
 				e.printStackTrace();
 			} catch (SAXException e) {
-				logger.error("Impossible to retrieve Persistence Unit Name - SAXException : [{}]",e);
+				logger.error("Impossible to retrieve Persistence Unit Name - SAXException : [{}]", e);
 				e.printStackTrace();
 			} catch (IOException e) {
-				logger.error("Impossible to retrieve Persistence Unit Name - IOException : [{}]",e);
+				logger.error("Impossible to retrieve Persistence Unit Name - IOException : [{}]", e);
 				e.printStackTrace();
 			} catch (XPathExpressionException e) {
-				logger.error("Impossible to retrieve Persistence Unit Name - XPathExpressionException : [{}]",e);
+				logger.error("Impossible to retrieve Persistence Unit Name - XPathExpressionException : [{}]", e);
 				e.printStackTrace();
 			}
 		}
 		return persistenceName;
 	}
-	
+
 	private ConnectionDescriptor getConnectionDescriptor() {
-		
+
 		PhysicalModel physicalModel = getPhysicalModel();
-		
+
 		String databaseName = physicalModel.getDatabaseName();
 		logger.debug("Datasource database name is [{}]", databaseName);
-		
+
 		List<String> missingProperties = new ArrayList<String>();
-		
+
 		String connectionUrl = getProperty(physicalModel, "connection.url");
 		logger.debug("Datasource connection url is [{}]", connectionUrl);
-		if(connectionUrl == null) missingProperties.add("connection.url");
-		
+		if (connectionUrl == null)
+			missingProperties.add("connection.url");
+
 		String connectionUsername = getProperty(physicalModel, "connection.username");
-		logger.debug("Datasource connection username is [{}]",connectionUsername);
-		if(connectionUsername == null) missingProperties.add("connection.username");
-		
+		logger.debug("Datasource connection username is [{}]", connectionUsername);
+		if (connectionUsername == null)
+			missingProperties.add("connection.username");
+
 		String connectionPassword = getProperty(physicalModel, "connection.password");
-		logger.debug("Datasource connection password is [{}]",connectionPassword);
-		if(connectionPassword == null) missingProperties.add("connection.password");
-		
+		logger.debug("Datasource connection password is [{}]", connectionPassword);
+		if (connectionPassword == null)
+			missingProperties.add("connection.password");
+
 		String connectionDriver = getProperty(physicalModel, "connection.driver");
-		logger.debug("Datasource connection driver is [{}]",connectionDriver);
-		if(connectionDriver == null) missingProperties.add("connection.driver");
-		
-		if(missingProperties.size() > 0) {
-			throw new RuntimeException("Impossible to create connection descriptor because the following properties are not available in model [" + getBusinessModel().getName()+ "]" + missingProperties.toString());
+		logger.debug("Datasource connection driver is [{}]", connectionDriver);
+		if (connectionDriver == null)
+			missingProperties.add("connection.driver");
+
+		if (missingProperties.size() > 0) {
+			throw new RuntimeException("Impossible to create connection descriptor because the following properties are not available in model ["
+					+ getBusinessModel().getName() + "]" + missingProperties.toString());
 		}
-		
+
 		DatabaseConnectionManager dcm = new DatabaseConnectionManager();
 		String connectionDialect = dcm.getHibernateDialectClassName(databaseName);
 		logger.debug("Datasource connection dialect is [{}]", connectionDialect);
-		
-		if(connectionDialect == null) {
-			throw new RuntimeException("Impossible to create connection descriptor because it is not possible to find a suitable dialect for database [" + databaseName + "]");
+
+		if (connectionDialect == null) {
+			logger.error("Impossible to create connection descriptor because it is not possible to find a suitable dialect for database [" + databaseName + "]");
+			throw new RuntimeException("Impossible to create connection descriptor because it is not possible to find a suitable dialect for database ["
+					+ databaseName + "]");
 		}
-		
-		
-		//Create Connection
-		ConnectionDescriptor connectionDescriptor = new ConnectionDescriptor();			
-		connectionDescriptor.setName( getBusinessModel().getName());
-		connectionDescriptor.setDialect( connectionDialect );			
-		connectionDescriptor.setDriverClass( connectionDriver );			
-		connectionDescriptor.setPassword( connectionPassword );
-		connectionDescriptor.setUrl( connectionUrl);
-		connectionDescriptor.setUsername( connectionUsername );
-		
+
+		// Create Connection
+		ConnectionDescriptor connectionDescriptor = new ConnectionDescriptor();
+		connectionDescriptor.setName(getBusinessModel().getName());
+		connectionDescriptor.setDialect(connectionDialect);
+		connectionDescriptor.setDriverClass(connectionDriver);
+		connectionDescriptor.setPassword(connectionPassword);
+		connectionDescriptor.setUrl(connectionUrl);
+		connectionDescriptor.setUsername(connectionUsername);
+
 		return connectionDescriptor;
 	}
-	
+
 	private String getProperty(PhysicalModel physicalModel, String propertyName) {
 		ModelProperty property = physicalModel.getProperties().get(propertyName);
-		return property!= null? property.getValue(): null;
+		return property != null ? property.getValue() : null;
 	}
 
 	public org.eclipse.core.internal.resources.File getModelFile() {
@@ -279,6 +281,5 @@ public class ModelManager {
 	public void setModelFile(org.eclipse.core.internal.resources.File modelFile) {
 		this.modelFile = modelFile;
 	}
-	
-	
+
 }
--- V_5.x/Meta/trunk/it.eng.spagobi.meta.querybuilder/src/it/eng/spagobi/meta/querybuilder/ui/editor/DatabaseConnectionManager.java
+++ V_5.x/Meta/trunk/it.eng.spagobi.meta.querybuilder/src/it/eng/spagobi/meta/querybuilder/ui/editor/DatabaseConnectionManager.java
@@ -6,7 +6,7 @@
  License, v. 2.0. If a copy of the MPL was not distributed with this file,
  You can obtain one at http://mozilla.org/MPL/2.0/.
  
-**/
+ **/
 package it.eng.spagobi.meta.querybuilder.ui.editor;
 
 import java.util.HashMap;
@@ -15,13 +15,13 @@ import java.util.Set;
 
 /**
  * @author Andrea Gioia (andrea.gioia@eng.it)
- *
+ * 
  */
 public class DatabaseConnectionManager {
-	private Map<String, String> drivers;
-	private Map<String, String> dialects;
-	
-	public DatabaseConnectionManager(){
+	private final Map<String, String> drivers;
+	private final Map<String, String> dialects;
+
+	public DatabaseConnectionManager() {
 		drivers = new HashMap<String, String>();
 		drivers.put("oracle", "oracle.jdbc.OracleDriver");
 		drivers.put("postgres", "org.postgresql.Driver");
@@ -30,8 +30,9 @@ public class DatabaseConnectionManager {
 		drivers.put("teradata", "com.teradata.jdbc.TeraDriver");
 		drivers.put("mysql", "com.mysql.jdbc.Driver");
 		drivers.put("db2", "com.ibm.db2.jcc.DB2Driver");
+		drivers.put("hdb", "com.sap.db.jdbc.Driver");
+		drivers.put("sap", "com.sap.db.jdbc.Driver");
 
-		
 		dialects = new HashMap<String, String>();
 		dialects.put("oracle", "org.hibernate.dialect.OracleDialect");
 		dialects.put("postgres", "org.hibernate.dialect.PostgreSQLDialect");
@@ -40,58 +41,62 @@ public class DatabaseConnectionManager {
 		dialects.put("teradata", "org.hibernate.dialect.TeradataDialect");
 		dialects.put("mysql", "org.hibernate.dialect.MySQLDialect");
 		dialects.put("db2", "org.hibernate.dialect.DB2Dialect");
+		dialects.put("hdb", "org.hibernate.dialect.SAPDBDialect");
+		dialects.put("sap", "org.hibernate.dialect.SAPDBDialect");
 
 	};
-	
 
-	public  Set getSupportedDatabases() {
+	public Set getSupportedDatabases() {
 		return drivers.keySet();
 	}
-	
-	public  boolean isDatabaseSupported(String databaseName) {
+
+	public boolean isDatabaseSupported(String databaseName) {
 		return drivers.keySet().contains(databaseName);
 	}
-	
-	public  String getJDBCDriverClassName(String databaseName) {
+
+	public String getJDBCDriverClassName(String databaseName) {
 		return drivers.get(databaseName);
 	}
-	
-	public  boolean checkDriverClassAvailability(String databaseName) {
+
+	public boolean checkDriverClassAvailability(String databaseName) {
 		return false;
 	}
-	
-	public  String getHibernateDialectClassName(String dbname) {
+
+	public String getHibernateDialectClassName(String dbname) {
 		String dialect = null;
 		String driver = null;
-		
-		if(dbname.toLowerCase().contains("oracle")) {
+
+		if (dbname.toLowerCase().contains("oracle")) {
 			dialect = "org.hibernate.dialect.OracleDialect";
 			driver = "oracle.jdbc.OracleDriver";
-		} else if(dbname.toLowerCase().contains("postgres")) {
+		} else if (dbname.toLowerCase().contains("postgres")) {
 			dialect = "org.hibernate.dialect.PostgreSQLDialect";
 			driver = "org.postgresql.Driver";
-		} else if(dbname.toLowerCase().contains("ingres")) {
+		} else if (dbname.toLowerCase().contains("ingres")) {
 			dialect = "org.hibernate.dialect.IngresDialect";
 			driver = "com.ingres.jdbc.IngresDriver";
-		} else if(dbname.toLowerCase().contains("microsoft")) {
+		} else if (dbname.toLowerCase().contains("microsoft")) {
 			dialect = "org.hibernate.dialect.SQLServerDialect";
 			driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-			//driver = "net.sourceforge.jtds.jdbc.Driver";
-		} else if(dbname.toLowerCase().contains("hsql")) {
+			// driver = "net.sourceforge.jtds.jdbc.Driver";
+		} else if (dbname.toLowerCase().contains("hsql")) {
 			dialect = "org.hibernate.dialect.HSQLDialect";
-			//driver = "org.hsqldb.jdbc.JDBCDriver";
+			// driver = "org.hsqldb.jdbc.JDBCDriver";
 			driver = "org.hsqldb.jdbcDriver";
-		} else if(dbname.toLowerCase().contains("teradata")) {
+		} else if (dbname.toLowerCase().contains("teradata")) {
 			dialect = "org.hibernate.dialect.TeradataDialect";
 			driver = "com.teradata.jdbc.TeraDriver";
-		} else if(dbname.toLowerCase().contains("mysql")) {
+		} else if (dbname.toLowerCase().contains("mysql")) {
 			dialect = "org.hibernate.dialect.MySQLDialect";
 			driver = "com.mysql.jdbc.Driver";
-		} else if(dbname.toLowerCase().contains("db2")) {
+		} else if (dbname.toLowerCase().contains("db2")) {
 			dialect = "org.hibernate.dialect.DB2Dialect";
 			driver = "com.ibm.db2.jcc.DB2Driver";
-		} 
-		
+		} else if (dbname.toLowerCase().contains("sap") || dbname.toLowerCase().contains("hdb")) {
+			dialect = "org.hibernate.dialect.SAPDBDialect";
+			driver = "com.sap.db.jdbc.Driver";
+		}
+
 		return dialect;
 	}
 }
